1b3662090fd432b4118b2ab667990b8b5690959e
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2014-09-16 09:56:34 +0100

    tidied up the code for testing in repl

--- service-lane.lisp
+++ service-lane.lisp
@@ -1,15 +1,18 @@
 ;;
 
-(defun main (&optional stream)
+(defun solution (&optional stream)
   (format t "~a~%" (read-line stream)))
 
-;; (main) ; uncomment this when running on hacker-rank
+;; (solution) ; uncomment this when running on hacker-rank
 
 (defun repl-main ()
   (let ((path (if (search "chess" (machine-instance))
-                  "/Users/jacekpodkanski/Documents/hackerrank/"
-                  "/home/jacek/Programming/HackerRank/")))
-    (with-open-file (s (concatenate 'string path "service-lane.input1.txt"))
-      (main s))))
+                  "Documents/hackerrank/"
+                  "Programming/HackerRank/")))
+    (with-open-file (s (concatenate 'string
+                                    (directory-namestring (user-homedir-pathname))
+                                    path
+                                    "service-lane.input1.txt"))
+      (solution s))))
 
 (repl-main)
