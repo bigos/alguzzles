be26590382c423b6a333788b9365e2bb49ee10f4
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2014-09-16 13:33:12 +0100

    more proggress parsing inputs

--- service-lane.lisp
+++ service-lane.lisp
@@ -1,7 +1,21 @@
 ;;
 
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
 (defun solution (&optional stream)
-  (format t "~a~%" (read-line stream)))
+  (let* ((first-input) (n) (tests) (width))
+    (setf first-input (split-by-one-space (read-line stream)))
+    (setf n (parse-integer (elt first-input 0))
+          tests (parse-integer (elt first-input 1))
+          width (map 'list (lambda (x) (parse-integer x)) (split-by-one-space (read-line stream)))
+          )
+
+    (format t "n ~a tests ~a width ~s~%" n tests width)
+    ))
 
 ;; (solution) ; uncomment this when running on hacker-rank
 
