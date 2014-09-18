a2c1d7b836226adbc8e2e0b85a6e3b53210e4bef
J. Podkanski <ruby.object@googlemail.com>
2014-09-18 08:30:09 +0100

    parsed inputs

--- cut-the-sticks.lisp
+++ cut-the-sticks.lisp
@@ -1,5 +1,19 @@
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+
 (defun solution (&optional stream)
-  (let* ((a (string-to-char (read-line stream))))))
+  (let* ((sticks-no (parse-integer (read-line stream)))
+         (sticks (split-and-parse (read-line stream)))
+
+         )))
 
 ;; (solution) ; uncomment this when running on hacker-rank
 
