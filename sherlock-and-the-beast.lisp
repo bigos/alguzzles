a81ed22c4390763184720ea3929fd822e9b58fc7
Jacek Podkanski <ruby.object@googlemail.com>
2014-09-21 00:32:26 +0100

    added test cases showing slow execution

--- sherlock-and-the-beast.lisp
+++ sherlock-and-the-beast.lisp
@@ -1,32 +1,27 @@
-(defun binary-to-decent (bin len)
-  (let* ((nstr (substitute #\5
-                           #\1
-                           (substitute #\3
-                                       #\0
-                                       (format nil
-                                               (format nil "~~~d,'0b" len)
-                                               bin) )))
-         (count3 (count #\3 nstr))
-         (count5 (count #\5 nstr)))
+(defun decent-binary (bin format-str)
+  (let* ((nstr (format nil format-str bin))
+         (count3 (count #\0 nstr))
+         (count5 (count #\1 nstr)))
     (if (and (= (+ count3 count5)
                 (length nstr))
              (zerop (rem count3 5))
              (zerop (rem count5 3)))
-        (parse-integer nstr)
+        (parse-integer (substitute #\5 #\1 (substitute #\3 #\0 nstr)))
         nil)))
 
 (defun largest-decent (len)
-  (let ((res))
+  (let ((format-str (format nil "~~~D,'0b" len)))
     (loop for bin from (expt 2 len) downto 0
-       do (setf res (binary-to-decent bin len))
+       for res = (decent-binary bin format-str)
        until res
        finally (return res))))
 
 (defun solution (&optional stream)
+  (declare (optimize (debug 3)))
   (let* ((tc (parse-integer (read-line stream)))
          (nums (loop repeat tc collect (parse-integer (read-line stream)))))
     (loop for x in nums
-       with r = (largest-decent x)
+       for r = (largest-decent x)
        do (format t "~A~%" (if r r -1)))))
 
 ;; (solution) ; uncomment this when running on hacker-rank
@@ -38,7 +33,7 @@
     (with-open-file (s (concatenate 'string
                                     (directory-namestring (user-homedir-pathname))
                                     path
-                                    "sherlock-and-the-beast.input.1.txt"))
+                                    "sherlock-and-the-beast.input.11.txt"))
       (solution s))))
 
 (repl-main)
