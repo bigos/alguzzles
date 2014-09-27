c17580f963bdd90a3c87e94a2a7edac932142e43
J. Podkanski <ruby.object@googlemail.com>
2014-09-27 17:06:32 +0100

    trying old code

--- manasa-and-stones.lisp
+++ manasa-and-stones.lisp
@@ -1,42 +1,42 @@
+;;; this is going to be useful
 (defun expotential-divisors (n expot)
-  (declare (optimize (speed 3)))
-  (declare (type fixnum n expot))
-  (reverse (loop for x = 1 then (* x expot ) until (> x n) collect x)))
+  (loop for x = 1 then (* x expot ) until (> x n) collect x))
 
-(defun calc-base (n divisors ab)
-  (declare (optimize (speed 3)))
-  (declare (type fixnum n))
-  (loop for d in divisors
-     collect (if (zerop (floor (/ n d)))
-                 (car ab)
-                 (cdr ab))
-     do (if (>= n d) (setf n (rem n d)))))
+(defun calculi (n expot)
+  (declare (optimize (debug 3)))
+  (let ((divs (reverse (expotential-divisors n expot)))
+        (n1 n) (d) (r))
+    (loop
+       while divs
+       do
+         (progn
+           (setf d (floor (/ n1 (car divs))))
+           (setf r (if (>= n1 (car divs))
+                       (rem n1 (car divs))
+                       999))
+           (format t "~A ~A ~A ~A~%" n1 divs d r)
+           (setf divs (cdr divs))
+           (setf n1 r))
+       collect d)))
 
-(defun pad-min-len (core len a)
-  (declare (optimize (speed 3)))
-  (declare (type fixnum len))
-  (let* ((cl (length core))
-         (padl (if (< cl len)
-                   (- len cl)
-                   0)))
-    (list (* padl a) core)))
+(defun puzzle (a b)
+  (let ((term (expt 2 2)))
+    (loop for x from 0 to (1- term)
+       collect (format nil "~2,'0b" x)
+         )))
 
-;;; fixed but slow  ;;; substitute might be a problem
-(defun puzzle (a b l)
-  (declare (optimize (speed 3)))
-  (declare (type fixnum a b l))
-  (let* ((term (expt 2 l)))
-    (remove-duplicates
-     (loop for x from 0 to (- term 1)
-        collect (list '+ (pad-min-len
-                          (calc-base x (expotential-divisors (1+ x) 2) (cons a b))
-                          l a))))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun find-vals (n a b)
-  (let ((res (puzzle a b (1- n))))
-    (loop for x in res
-       do (format t "~a " x))
-    (terpri)))
+(defun find-me (n r values)
+  (format t "~A ~A ~% " n r)
+  (loop for v in values
+     do (progn (if (> n 0)
+                   (find-me (1- n)  (+ r v)  values))
+               )))
+
+(defun find-values (n a b)
+  (find-me n 0 (list a b))
+  (format t "~%"))
 
 (defun solution (&optional stream)
   (let* ((tests (parse-integer (read-line stream)))
@@ -44,8 +44,9 @@
                   collect (list (parse-integer (read-line stream))
                                 (parse-integer (read-line stream))
                                 (parse-integer (read-line stream))))))
+    (format t ">>> ~A~%" data)
     (loop for dataset in data
-       do (find-vals (nth 0 dataset)
+       do (find-values (nth 0 dataset)
                        (nth 1 dataset)
                        (nth 2 dataset)))))
 
@@ -59,20 +60,6 @@
                                     (directory-namestring (user-homedir-pathname))
                                     path
                                     "manasa-and-stones.input.1.txt"))
-      (solution s))
-    ;; (with-open-file (s (concatenate 'string
-    ;;                                 (directory-namestring (user-homedir-pathname))
-    ;;                                 path
-    ;;                                 "manasa-and-stones.input.2.txt"))
-    ;;   (solution s))
-    ))
+      (solution s))))
 
 (repl-main)
-
-;; (require :sb-sprof)
-;; (sb-sprof:with-profiling (:max-samples 10
-;;                                        :report :flat
-;;                                        :loop nil))
-;; (sb-sprof:start-profiling)
-;; (puzzle 10 200 23)
-;; (sb-sprof:report)
