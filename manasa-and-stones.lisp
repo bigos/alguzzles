d3dfd07f81b9e3d44eb82fa1ba9f0d1da10effc0
J. Podkanski <ruby.object@googlemail.com>
2014-09-27 17:02:21 +0100

    another dead end

--- manasa-and-stones.lisp
+++ manasa-and-stones.lisp
@@ -1,27 +1,26 @@
-;;; this is going to be useful
 (defun expotential-divisors (n expot)
   (declare (optimize (speed 3)))
   (declare (type fixnum n expot))
   (reverse (loop for x = 1 then (* x expot ) until (> x n) collect x)))
 
-(defun calc-base (n divisors)
+(defun calc-base (n divisors ab)
   (declare (optimize (speed 3)))
   (declare (type fixnum n))
   (loop for d in divisors
-     collect (floor (/ n d))
+     collect (if (zerop (floor (/ n d)))
+                 (car ab)
+                 (cdr ab))
      do (if (>= n d) (setf n (rem n d)))))
 
-(defun pad-min-len (core len)
+(defun pad-min-len (core len a)
   (declare (optimize (speed 3)))
-  (declare (type fixnum len)
-      )
+  (declare (type fixnum len))
   (let* ((cl (length core))
          (padl (if (< cl len)
                    (- len cl)
                    0)))
-    (concatenate 'list
-                 (loop repeat padl collect 0)
-                 core)))
+    (list (* padl a) core)))
+
 ;;; fixed but slow  ;;; substitute might be a problem
 (defun puzzle (a b l)
   (declare (optimize (speed 3)))
@@ -29,12 +28,9 @@
   (let* ((term (expt 2 l)))
     (remove-duplicates
      (loop for x from 0 to (- term 1)
-        collect (list '+
-                      (substitute a
-                                  0
-                                  (substitute b
-                                              1
-                                              (calc-base x (expotential-divisors (1+ x) 2)))))))))
+        collect (list '+ (pad-min-len
+                          (calc-base x (expotential-divisors (1+ x) 2) (cons a b))
+                          l a))))))
 
 (defun find-vals (n a b)
   (let ((res (puzzle a b (1- n))))
@@ -73,10 +69,10 @@
 
 (repl-main)
 
-(require :sb-sprof)
-(sb-sprof:with-profiling (:max-samples 10
-                                       :report :flat
-                                       :loop nil))
-(sb-sprof:start-profiling)
-(puzzle 10 200 23)
-(sb-sprof:report)
+;; (require :sb-sprof)
+;; (sb-sprof:with-profiling (:max-samples 10
+;;                                        :report :flat
+;;                                        :loop nil))
+;; (sb-sprof:start-profiling)
+;; (puzzle 10 200 23)
+;; (sb-sprof:report)
