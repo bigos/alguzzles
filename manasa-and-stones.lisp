d8ca48b51cbd4a091297d9cecfc0a43fc32d833c
J. Podkanski <ruby.object@googlemail.com>
2014-09-28 08:21:36 +0100

    reset

--- manasa-and-stones.lisp
+++ manasa-and-stones.lisp
@@ -1,42 +1,5 @@
-;;; this is going to be useful
-(defun expotential-divisors (n expot)
-  (loop for x = 1 then (* x expot ) until (> x n) collect x))
-
-(defun calculi (n expot)
-  (declare (optimize (debug 3)))
-  (let ((divs (reverse (expotential-divisors n expot)))
-        (n1 n) (d) (r))
-    (loop
-       while divs
-       do
-         (progn
-           (setf d (floor (/ n1 (car divs))))
-           (setf r (if (>= n1 (car divs))
-                       (rem n1 (car divs))
-                       999))
-           (format t "~A ~A ~A ~A~%" n1 divs d r)
-           (setf divs (cdr divs))
-           (setf n1 r))
-       collect d)))
-
-(defun puzzle (a b)
-  (let ((term (expt 2 2)))
-    (loop for x from 0 to (1- term)
-       collect (format nil "~2,'0b" x)
-         )))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defun find-me (n r values)
-  (format t "~A ~A ~% " n r)
-  (loop for v in values
-     do (progn (if (> n 0)
-                   (find-me (1- n)  (+ r v)  values))
-               )))
-
 (defun find-values (n a b)
-  (find-me n 0 (list a b))
-  (format t "~%"))
+  (format t "arguments: ~a ~a ~a~%" n a b))
 
 (defun solution (&optional stream)
   (let* ((tests (parse-integer (read-line stream)))
@@ -44,7 +7,6 @@
                   collect (list (parse-integer (read-line stream))
                                 (parse-integer (read-line stream))
                                 (parse-integer (read-line stream))))))
-    (format t ">>> ~A~%" data)
     (loop for dataset in data
        do (find-values (nth 0 dataset)
                        (nth 1 dataset)
