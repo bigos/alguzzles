c0bbfcb8d85cb2f83264c566f9ff62686509d7e9
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2014-10-07 09:11:39 +0100

    perhaps proggress

--- sherlock-and-gcd.lisp
+++ sherlock-and-gcd.lisp
@@ -1,37 +1,3 @@
-;; Sherlock is stuck. He has an array A1,A2,⋯,AN. He wants to know if there exists a subset, B={Ai1,Ai2,…,Aik}
-;; where 1≤i1<i2<…<ik≤N, of this array which follows the property
-
-;; B is non-empty subset.
-;; There exists no integer x(x>1) which divides all elements of B. Note that x may or may not be an element of A.
-
-;; Input Format
-;; First line contains T, the number of testcases. Each testcase consists of N in one line. The next line contains N integers denoting the array A.
-
-;; Output
-;; Print YES or NO, if there exists any such subset or not, respectively.
-
-;; Constraints
-;; 1≤T≤10
-;; 1≤N≤100
-;; 1≤Ai≤10^5 ∀1≤i≤N
-
-;; Sample input
-
-;; 2
-;; 3
-;; 1 2 3
-;; 2
-;; 2 4
-
-;; Sample output
-
-;; YES
-;; NO
-
-;; Explanation
-;; In first testcase, S={1},S={1,2},S={1,3},S={2,3} and S={1,2,3} are all the possible subsets which satisfy the given condition.
-;; In second testcase, no non-empty subset exists which satisfies the given condition.
-
 (defun split-by-one-space (string)
   (loop for i = 0 then (1+ j)
      as j = (position #\Space string :start i)
@@ -48,12 +14,15 @@
       (cond ((< n 3) nil)
             ((eq n 3) (apply '< nums))
             (T (progn
-                 (loop repeat 4
+                 (loop repeat (- (length nums) 2)
                     for x = 0 then (+ x 1)
-                    for y = (+ x 2) then (+ y 1)
-                    until (>= y (length nums))
-                    do (format t "~A ~A~%" x y))
-                 nil)))
+                    for y = (+ x 3) then (+ y 1)
+                    for z = (subseq nums x y)
+                    for res = (apply '< z)
+                    do (format t "~A ~A~%" nums z)
+                    until res
+                    finally (return res))
+                 )))
       ))
 
 (defun puzzle (data)
