30630c587c630bf90e21807f0f965914954cf59f
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2014-10-14 13:15:42 +0100

    new puzzle

---
+++ sherlock-and-squares/solution.lisp
@@ -0,0 +1,33 @@
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+
+(defun solution (&optional stream)
+  (let* ((testcases (parse-integer (read-line stream)))
+         (data (make-array (list testcases 2)
+                           :initial-contents
+                           (loop repeat testcases
+                              collect (split-and-parse (read-line stream))))))
+    (format t "~A ~s~%" testcases data)))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun repl-main ()
+  (let ((path (if(search "chess" (machine-instance))
+                 "Documents/hackerrank/"
+                 "Programming/hackerrank/"))
+        (puzzle "sherlock-and-squares"))
+    (with-open-file (s (concatenate 'string
+                                    (directory-namestring (user-homedir-pathname))
+                                    path
+                                    puzzle "/" "input.1.txt"))
+      (solution s))))
+
+(repl-main)
