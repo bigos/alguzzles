f32db6278cbe38d8cb1db955d529ddfb84b4b160
J. Podkanski <ruby.object@googlemail.com>
2014-10-14 19:12:52 +0100

    stub

--- is-fibo/solution.lisp
+++ is-fibo/solution.lisp
@@ -1,10 +1,20 @@
+(defun fibonaccip (i)
+  (oddp i))
+
 (defun solution (&optional stream)
   (let* ((testcases (parse-integer (read-line stream)))
          (data (make-array (list testcases 1)
                            :initial-contents
                            (loop repeat testcases
                               collect (list (parse-integer (read-line stream)))))))
-   (format t "~S ~S~%" testcases data)))
+    (format t "~S ~S~%" testcases data)
+    (loop for x from 0 below testcases
+       do
+         (format t
+                 "~a~%"
+                 (if (fibonaccip x)
+                     "IsFibo"
+                     "IsNotFibo")))))
 
 ;; (solution) ; uncomment this when running on hacker-rank
 
