0d6e10e754f5be200322f306686b3944a1008086
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2014-10-14 16:58:47 +0100

    new puzzle

---
+++ is-fibo/solution.lisp
@@ -0,0 +1,22 @@
+(defun solution (&optional stream)
+  (let* ((testcases (parse-integer (read-line stream)))
+         (data (make-array (list testcases 1)
+                           :initial-contents
+                           (loop repeat testcases
+                              collect (parse-integer (read-line stream))))))
+   (format t "~S ~S~%" testcases data)))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun repl-main ()
+  (let ((path (if(search "chess" (machine-instance))
+                 "Documents/hackerrank/"
+                 "Programming/hackerrank/"))
+        (puzzle "is-fibo"))
+    (with-open-file (s (concatenate 'string
+                                    (directory-namestring (user-homedir-pathname))
+                                    path
+                                    puzzle "/" "input.1.txt"))
+      (solution s))))
+
+(repl-main)
