f61506dcccc51a24162a1908006c7cec64591d76
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2014-10-15 15:10:37 +0100

    some proggress

--- angry-children/solution.lisp
+++ angry-children/solution.lisp
@@ -1,11 +1,12 @@
 ;; (puzzle 7 3 #(1 2 3 4 5 6 7))
-(defun puzzle (n k data &optional (level 0))
+(defun puzzle (n k data ar &optional (level 0))
   (format t "~A ~A ~A ~A~%" n k  data level)
   (loop for x from (- n k level) below (- n level)
-     do
-       (format t "~A~%" x))
-  (when (< level (1- k)) (puzzle 5 3 data (1+ level)))
-  )
+     do (progn
+          (if (< level (1- k))
+              (puzzle 5 3 data ar (1+ level)))
+          (progn  (setf (aref ar level) x)
+                  (format t ">>>>  ~A ~A ~%"  ar  level )))))
 
 (defun solution (&optional stream)
   (let* ((n (parse-integer (read-line stream)))
@@ -13,9 +14,11 @@
          (data (make-array (list n)
                            :initial-contents
                            (loop repeat n
-                              collect  (parse-integer (read-line stream))))))
+                              collect  (parse-integer (read-line stream)))))
+         (ar (make-array (list k)
+                         :initial-element 0)))
     (format nil "~a ~a ~a~%" n k data)
-    (puzzle n k data)))
+    (puzzle n k data ar)))
 
 ;; (solution) ; uncomment this when running on hacker-rank
 
