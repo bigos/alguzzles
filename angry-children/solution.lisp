1e3bb7bbe6f9ad9ff44ef0505a57e7887bfd3740
J. Podkanski <ruby.object@googlemail.com>
2014-10-16 02:36:42 +0100

    more progress

--- angry-children/solution.lisp
+++ angry-children/solution.lisp
@@ -5,12 +5,18 @@
 ;;                (format t " ~A~%" (list a b c)))))
 
 (defun ppp (v level maxlevel ar)
-  (loop for x from (+ v level) to  4 do
+  (loop for x from (+ v 1) upto  4 do
        (if (>= level maxlevel)
-         (format t "~A ~A ~A~%" v level ar)
-         (progn
-           (incf (aref ar level))
-           (ppp v (1+ level) maxlevel ar)))))
+           (format nil "~A ~A ~A~%" v level ar)
+           (progn
+             (if (/= (aref ar 0)
+                     (aref ar 1)
+                     (aref ar 2))
+                 (format t "+++ ~A ~A ~A~%" v level ar)
+                 (format t "----- ~A ~A ~A~%" v level ar))
+             (setf (aref ar level) x)
+             (ppp x (1+ level) maxlevel ar))))
+  (format nil "------------~%" ))
 
 ;; (puzzle 7 3 #(1 2 3 4 5 6 7))
 (defun puzzle (n k data ar &optional (level 0))
