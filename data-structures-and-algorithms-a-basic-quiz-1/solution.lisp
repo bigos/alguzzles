e48f0b0a9bd2935136a3934f546e97a3fd1e644c
Jacek Podkanski <ruby.object@googlemail.com>
2014-10-26 22:52:31 +0000

    new puzzle

---
+++ data-structures-and-algorithms-a-basic-quiz-1/solution.lisp
@@ -0,0 +1,18 @@
+(defun solution ()
+  (format t "solve the quiz manually"))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun repl-main ()
+  (let ((path (if(search "chess" (machine-instance))
+                 "Documents/hackerrank/"
+                 "Programming/hackerrank/"))
+        (puzzle "data-structures-and-algorithms-a-basic-quiz-1"))
+
+    (with-open-file (s (concatenate 'string
+                                    (directory-namestring (user-homedir-pathname))
+                                    path
+                                    puzzle "/" "input.1.txt"))
+      (solution ))))
+
+(repl-main)
