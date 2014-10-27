ff8aacf31a125da88e9688f5dca97f5acfd7c0be
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2014-10-27 10:07:48 +0000

    new puzzle

---
+++ bigger-is-greater/solution.lisp
@@ -0,0 +1,26 @@
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun solution (&optional stream)
+  (let* ((n (parse-integer (read-line stream)))
+         (data (loop repeat n
+                  collect (read-line stream))))
+    (format t "~A ~A~%" n data)))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun repl-main ()
+  (let ((path (if(search "chess" (machine-instance))
+                 "Documents/hackerrank/"
+                 "Programming/hackerrank/"))
+        (puzzle "bigger-is-greater"))
+    (with-open-file (s (concatenate 'string
+                                    (directory-namestring (user-homedir-pathname))
+                                    path
+                                    puzzle "/" "input.1.txt"))
+      (solution s))))
+
+(repl-main)
