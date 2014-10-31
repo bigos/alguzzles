9d75c74f17953211899184f89c65845d94d51889
J. Podkanski <ruby.object@googlemail.com>
2014-10-31 20:59:01 +0000

    so close yet so far

--- bigger-is-greater/solution.lisp
+++ bigger-is-greater/solution.lisp
@@ -4,11 +4,32 @@
      collect (subseq string i j)
      while j))
 
+(defun next-perm (vec cmp)  ; modify vector
+  (declare (type (simple-array * (*)) vec))
+  (macrolet ((el (i) `(aref vec ,i))
+             (cmp (i j) `(funcall cmp (el ,i) (el ,j))))
+    (loop with len = (1- (length vec))
+       for i from (1- len) downto 0
+       when (cmp i (1+ i)) do
+         (loop for k from len downto i
+            when (cmp i k) do
+              (rotatef (el i) (el k))
+              (setf k (1+ len))
+              (loop while (< (incf i) (decf k)) do
+                   (rotatef (el i) (el k)))
+              (return-from next-perm vec)))))
+
+(defun puzzle (x)
+  (let ((o (concatenate 'string "" x)))
+    (loop for a = x then (next-perm a #'char<) while a do
+         (format t "~a ~a ~a~%" a o (string< a (concatenate 'string "" o))))))
+
 (defun solution (&optional stream)
   (let* ((n (parse-integer (read-line stream)))
          (data (loop repeat n
                   collect (read-line stream))))
-    (format t "~A ~A~%" n data)))
+    (format nil "~A ~A~%" n data)
+    (loop for x in data do (puzzle x))))
 
 ;; (solution) ; uncomment this when running on hacker-rank
 
