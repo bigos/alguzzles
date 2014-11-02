aa4de9bda06ab4a7002a198c8c44bd09af32defc
Jacek Podkanski <ruby.object@googlemail.com>
2014-11-02 13:52:32 +0000

    last warmup puzzle set up

---
+++ sherlock-and-queries/solution.lisp
@@ -0,0 +1,33 @@
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+
+(defun solution (&optional stream)
+  (let* ((first-line (split-and-parse (read-line stream)))
+         (n (car first-line))
+         (m (cadr first-line))
+         (a (split-and-parse (read-line stream)))
+         (b (split-and-parse (read-line stream)))
+         (c (split-and-parse (read-line stream))))))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun repl-main ()
+  (let ((path (if(search "chess" (machine-instance))
+                 "Documents/hackerrank/"
+                 "Programming/hackerrank/"))
+        (puzzle "sherlock-and-queries"))
+    (with-open-file (s (concatenate 'string
+                                    (directory-namestring (user-homedir-pathname))
+                                    path
+                                    puzzle "/" "input.1.txt"))
+      (solution s))))
+
+(repl-main)
