8109813b6dfe08f02295f8bf93c46e65090c38c2
Jacek Podkanski <ruby.object@googlemail.com>
2014-11-08 16:04:23 +0000

    more

--- sherlock-and-queries/solution.lisp
+++ sherlock-and-queries/solution.lisp
@@ -11,7 +11,9 @@
 
 (defun puzzle (m n a b c)
   (let ((big-no (+ (expt 10 9) 7))
-        (factors (make-array (list m) :initial-value 1)))
+        (factors (make-array (list m) :initial-element 1)))
+    (loop for i from 0 below m do
+)
     (loop for i from 0 below m do
          (loop for j from 0 below n do
               (progn
