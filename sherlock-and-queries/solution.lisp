71f262303f7c7907bef6bcdf401993c4d344a2a9
Jacek Podkanski <ruby.object@googlemail.com>
2014-11-14 23:04:13 +0000

    code cleanup

--- sherlock-and-queries/solution.lisp
+++ sherlock-and-queries/solution.lisp
@@ -1,4 +1,4 @@
- (defvar lprimos nil)
+(defvar lprimos nil)
 (defvar ta nil)
 (defvar modulo (+ (expt 10 9) 7))
 (defvar ak nil)
@@ -9,39 +9,31 @@
 (defvar nn nil)
 (defvar nm nil)
 
-
-
 (defun primos(n)
   (loop with s = (list 2)
-    for i from 3 to (1+ (sqrt n)) do
-      (when  (loop for p in s never  (zerop (mod i p)))
-        (setq s (nconc s (list i))))
-       finally (return s)))
-
+     for i from 3 to (1+ (sqrt n)) do
+       (when  (loop for p in s never  (zerop (mod i p)))
+         (setq s (nconc s (list i))))
+     finally (return s)))
 
 (defun exponente(n p)
   (loop with num = n
-        with e = 0
+     with e = 0
      while (zerop (mod num p)) do
        (incf e)
        (setq num (/ num p))
-       finally (return e)))
-
-
+     finally (return e)))
 
 (defun divisores(num)
   (loop with n = num
-       with expo = nil
+     with expo = nil
      for p in lprimos
      when (zerop (mod n p)) collect
        (progn (setq expo (exponente n p)
                     n (/ n (expt p expo)))
               (list p expo))
-       into list
+     into list
      finally (return (if (= n 1) list (cons (list n 1) list)))))
-
-
-
 
 (defun ldivisores(num)
   (loop with s1 = (list 1)
@@ -61,16 +53,14 @@
   (loop for i from 1 to (1- n) do (princ (aref a i)) (princ #\Space))
   (princ (aref a n)))
 
-
 (defun out-aa()
   (let ((*standard-output* *standard-output*))
- (fresh-line)
- (loop for i from 1 to (1- nn) do (princ (aref aa i)) (princ #\Space))
- (princ (aref aa nn))))
-
+    (fresh-line)
+    (loop for i from 1 to (1- nn) do (princ (aref aa i)) (princ #\Space))
+    (princ (aref aa nn))))
 
 (defun haz(strm)
-(let ((*standard-input* strm))
+  (let ((*standard-input* strm))
     (setq nn (read)
           nm (read))
     (setq aa (make-array (1+ nn) :element-type 'fixnum)
@@ -81,12 +71,11 @@
     (loop for i from 1 to nm do (setf (aref ac i) (read)))
     (procesa2)))
 
-
 (setq  lprimos (primos 100000))
 
 (defun multiplo(num n)
   (loop for i from 1
-    while (<= (* num i) n) collect (* num i)))
+     while (<= (* num i) n) collect (* num i)))
 
 (defun tarta(num)
   (loop with ee = 0  with pp = nil with pe = nil
@@ -94,21 +83,17 @@
        (when (> e ee)
          (setq  ee e  pp p))
      finally (return (list (/ num pp) (progn (setq pe (expt pp ee))
-                                              (mapcar (lambda(x)(* x pe)) (ldivisores (/ num pe))))))))
-
-
+                                             (mapcar (lambda(x)(* x pe)) (ldivisores (/ num pe))))))))
 
 (defun crea-tarta()
   (setq ta (make-array (1+ nn)))
- (loop for i from 2 to nn do
-      (setf (aref ta i) (tarta i))))
-
-
+  (loop for i from 2 to nn do
+       (setf (aref ta i) (tarta i))))
 
 (defun crea-as()
   (setq as (make-array (1+ nn) :initial-element 1))
   (loop for i from 1 to nm
-        for j = (aref ab i) do
+     for j = (aref ab i) do
        (when (<= 1 j nn)
          (setf (aref as j) (mod (* (aref as j) (aref ac i)) modulo)))))
 
@@ -123,12 +108,9 @@
                                      finally (return prod)))
                                modulo))))
 
-
-
 (defun pon-aa()
   (loop for i from 1 to nn do
        (setf (aref aa i) (mod  (* (aref aa i) (aref ak i)) modulo))))
-
 
 (defun procesa2()
   (crea-as)
@@ -136,8 +118,6 @@
   (crea-st)
   (pon-aa)
   (out-aa))
-
-;;;;;;;;;;;;;;;
 
 (defun repl-main ()
   (let ((path (if (search "chess" (the string (machine-instance)))
