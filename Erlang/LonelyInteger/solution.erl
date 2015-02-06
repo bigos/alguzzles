91e5fd67e1f9cbe8565c05ec5b54c82fb88e44af
Jacek Podkanski <ruby.object@googlemail.com>
2015-02-06 17:00:11 +0000

    prepared

--- Erlang/LonelyInteger/solution.erl
+++ Erlang/LonelyInteger/solution.erl
@@ -1,7 +1,21 @@
 -module(solution).
 -export([main/0]).
 
+
+select_input() ->
+    Hst = net_adm:localhost(),
+    if Hst == "jacek-desktop" -> 
+            {ok, Device} = file:open("./input1.txt",[read]),
+            Device;
+       true -> standard_io
+    end.
+
+read_integers(Device) ->
+    io:fread(Device, [], "~d").
+
 main() ->
+    Dev = select_input(),
+    {ok, Line} = read_integers(Dev),
   .
 
 
