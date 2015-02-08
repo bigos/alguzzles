6ea74c6068d87bf503c28f5715f84eb99e087aa8
Jacek Podkanski <ruby.object@googlemail.com>
2015-02-08 05:23:55 +0000

    didn't progress even an inch

--- Haskell/LonelyInteger/solution.hs
+++ Haskell/LonelyInteger/solution.hs
@@ -1,8 +1,5 @@
-zzz x = print (head  x)
 main = do
   val1 <- getLine
   val2 <- getLine
   print (read val1 :: Int)
   print (map read (words val2) :: [Int])
-
-  -- don't waste your time with functional programming go back to Lisp
