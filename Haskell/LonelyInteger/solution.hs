1dd29b178d458d93f6e0a0d7b351c11b2b529eeb
Jacek Podkanski <ruby.object@googlemail.com>
2015-02-08 06:59:47 +0000

    progress

--- Haskell/LonelyInteger/solution.hs
+++ Haskell/LonelyInteger/solution.hs
@@ -1,5 +1,17 @@
+-- very good writeup
+-- http://blog.sigfpe.com/2007/11/io-monad-for-people-who-simply-dont.html
+
+isTheSame x y = x==y
+
+get2Lines = do
+    line1 <- getLine
+    line2 <- getLine
+    return (line1, line2)
+
 main = do
-  val1 <- getLine
-  val2 <- getLine
-  print (read val1 :: Int)
-  print (map read (words val2) :: [Int])
+  (val1, val2) <- get2Lines
+  let va = (read val1 :: Int)
+  let vb = (map read (words val2) :: [Int])
+
+  print va
+  print vb
