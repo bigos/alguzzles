5e754cf853d17fd58ca04d734c5b26a8d6b43d37
Jacek Podkanski <ruby.object@googlemail.com>
2015-02-08 13:14:51 +0000

    more proggress

--- Haskell/LonelyInteger/solution.hs
+++ Haskell/LonelyInteger/solution.hs
@@ -1,19 +1,34 @@
+import Data.List
 -- very good writeup
 -- http://blog.sigfpe.com/2007/11/io-monad-for-people-who-simply-dont.html
 
 isTheSame :: (Int, Int) -> Bool
 isTheSame (x, y) = (x == y)
 
+splitList :: (Int, [a]) -> ([a], [a])
+splitList (ln, lst) = splitAt (div ln 2) lst
+
+sortLists :: Ord a => ([a], [a]) -> ([a], [a])
+sortLists sl =
+  let l1 = Data.List.sort (fst sl)
+      l2 = Data.List.sort (snd sl)
+  in (l1, l2)
+
 get2Lines = do
-    line1 <- getLine
-    line2 <- getLine
-    return (line1, line2)
+  print "enter line 1"
+  line1 <- getLine
+  print "enter line 2"
+  line2 <- getLine
+  return (line1, line2)
 
 main = do
   (val1, val2) <- get2Lines
   let va = (read val1 :: Int)
   let vb = (map read (words val2) :: [Int])
   let vr = (isTheSame (va, head vb) )
+  let sl = splitList (va, vb)
+  let sorted = sortLists sl
+  print sorted
   print vr
   print va
   print vb
