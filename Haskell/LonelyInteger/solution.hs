9339f8ebfd18bc3b7262e5ae78733ea2fab70ced
Jacek Podkanski <ruby.object@googlemail.com>
2015-02-08 04:40:32 +0000

    read correct type values

--- Haskell/LonelyInteger/solution.hs
+++ Haskell/LonelyInteger/solution.hs
@@ -1,8 +1,8 @@
+zzz x = print (head  x)
 main = do
   val1 <- getLine
   val2 <- getLine
-
-  print val1
+  print (read val1 :: Int)
   print (map read (words val2) :: [Int])
 
   -- don't waste your time with functional programming go back to Lisp
