21851ee8dede8395a69f388856b9b4fcc4537c1c
Jacek Podkanski <ruby.object@googlemail.com>
2015-02-09 23:39:18 +0000

    bit flipping works

--- Haskell/FlippingBits/solution.hs
+++ Haskell/FlippingBits/solution.hs
@@ -1,5 +1,7 @@
 import Data.List
 import Control.Monad
+import Data.Bits
+import Data.Word
 
 -- run it in terminal
 -- $ cat ./input1.txt | runhaskell solution.hs
@@ -8,5 +10,6 @@ main :: IO ()
 main = do
   t0 <- readLn :: IO Int
   str <- replicateM t0 getLine
-  let nums = map read str :: [Int]
+  let nums = map read str :: [Word32]
   print nums
+  print (complement (head nums) )
