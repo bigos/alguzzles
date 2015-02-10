91f7a00b9fa5bf691a6a9ea7615ee5f16e90b425
Jacek Podkanski <ruby.object@googlemail.com>
2015-02-10 08:01:48 +0000

    don't know yet how to do loops in Haskell

---
+++ Haskell/UtopianTree/solution.hs
@@ -0,0 +1,26 @@
+import Control.Monad
+
+-- run it in terminal
+-- $ cat ./input1.txt | runhaskell solution.hs
+
+calcGrowth :: Int -> Int
+calcGrowth 0 = 1
+calcGrowth 1 = 2
+calcGrowth x = (x * 2) + (x * 1) -- not working yet
+-- working Lisp example
+-- (defun calc (i)
+--   (loop for x from 0 to i
+--      for total = 1 then (if (oddp x)
+--                             (* total 2)
+--                             (+ 1 total))
+--      finally (return total)))
+
+main :: IO ()
+main = do
+  t <- readLn :: IO Int
+  strs <- replicateM t getLine
+  let cycles = map read strs :: [Int]
+  print t
+  print cycles
+  let answers = map calcGrowth cycles
+  forM_ answers print
