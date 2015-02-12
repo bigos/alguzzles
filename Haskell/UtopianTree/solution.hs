83152f587b804cabb2336d7d26d6ed146e48593e
Jacek Podkanski <ruby.object@googlemail.com>
2015-02-12 20:59:44 +0000

    empty files for the new puzzle

--- Haskell/UtopianTree/solution.hs
+++ Haskell/UtopianTree/solution.hs
@@ -2,6 +2,20 @@ import Control.Monad
 
 -- run it in terminal
 -- $ cat ./input1.txt | runhaskell solution.hs
+lg :: Int -> Int -> Int -> Int
+lg s e a =
+  if s < e
+  then
+  if even s
+  then
+    lg (s+1) e (a+1)
+  else
+    lg (s+1) e (a*2)
+  else
+    a
+
+loopGrowth :: Int -> Int
+loopGrowth x = lg 0 x 0
 
 calcGrowth :: Int -> Int
 calcGrowth y =
@@ -14,5 +28,6 @@ main = do
   let cycles = map read strs :: [Int]
   -- print t
   -- print cycles
-  let answers = map calcGrowth cycles
+  -- let answers = map calcGrowth cycles
+  let answers = map loopGrowth cycles
   forM_ answers print
