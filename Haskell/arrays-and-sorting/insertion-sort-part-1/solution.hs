90dbea7ffd317a223eec11da493f7cf192567ab1
Jacek Podkanski <ruby.object@googlemail.com>
2015-02-17 22:40:14 +0000

    started working on shifting list values

--- Haskell/arrays-and-sorting/insertion-sort-part-1/solution.hs
+++ Haskell/arrays-and-sorting/insertion-sort-part-1/solution.hs
@@ -1,3 +1,15 @@
+import Control.Monad
+import Data.List
+import Data.Maybe
+import Debug.Trace
+
+
+my n v ar
+  | trace (show v) False = undefined
+  | otherwise = ss
+  where vi = fromJust (elemIndex v ar)
+        ss = splitAt vi ar
+
 main :: IO ()
 main = do
   s <- readLn :: IO Int
