6d6e2b9832681133b61a387830583ea1931d8c4a
Jacek Podkanski <ruby.object@googlemail.com>
2015-02-18 01:50:04 +0000

    still experimenting

--- Haskell/arrays-and-sorting/insertion-sort-part-1/solution.hs
+++ Haskell/arrays-and-sorting/insertion-sort-part-1/solution.hs
@@ -3,14 +3,27 @@ import Data.List
 import Data.Maybe
 import Debug.Trace
 
-my n v ar
-  | trace (show v) False = undefined
-  | otherwise = (ss, vi, sr)
-  where vi = fromJust (elemIndex v ar)
-        ss = splitAt vi ar
-        firstlast = (last (fst ss))
-        secondfirst = (head (snd ss))
-        sr = if secondfirst >= firstlast then secondfirst else firstlast
+mybeg i ar = take i ar
+myfin i ar = drop (i+1) ar
+
+aiv i v ar = join [(mybeg i ar) , [v] , (myfin i ar)]
+
+insort i v ar
+  | trace (show i ++ show v ++ show ar ++ "  >"++show start++show finish) False = undefined
+  | i < 1 = i
+  | otherwise = insort (i-1) v (aiv i v ar)
+  where start = take i ar
+        finish = drop (i+1) ar
+
+-- myy n v ar
+--   | trace (show v) False = undefined
+--   | otherwise = (ss, vi, sr)
+--   where vi = fromJust (elemIndex v ar)
+--         ss = splitAt vi ar
+--         firstlast = (last (fst ss))
+--         secondfirst = (head (snd ss))
+--         sr = if secondfirst >= firstlast then secondfirst else firstlast
+
 
 
 main :: IO ()
