4dc8067ce1e0ff38bc60adb719200b57217b1e67
Jacek Podkanski <ruby.object@googlemail.com>
2015-02-21 13:55:50 +0000

    two testcases pass, one gives wrong answer and one times out

--- Haskell/dynamic-programming/the-maximum-subarray/solution.hs
+++ Haskell/dynamic-programming/the-maximum-subarray/solution.hs
@@ -17,9 +17,12 @@ slices xs = tail $ inits xs ++ slices (tail xs)
 maxcont ar =  maximum ( map (\x -> sum x) (slices ar))
 maxnoncont ar = maximum ( map (\x -> sum (filter (\y -> y >= 0) x)) (slices ar))
 
+showmaxes :: [Int] -> String
+showmaxes ar = show (maxcont ar) ++ " " ++show (maxnoncont ar)
+
 main :: IO ()
 main = do
   t <- readLn :: IO Int
-  print t
+  -- print t
   inputs <- replicateM t getData :: IO [(Int, [Int])]
-  mapM_ (\x -> show (maxcont (snd x))++ show(maxnoncont (snd x)) ) inputs
+  mapM_ (\x -> putStrLn (showmaxes (snd x))) inputs
