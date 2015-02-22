0e6e3c03925057eb24d8b52e7267d66b6214cd5a
Jacek Podkanski <ruby.object@googlemail.com>
2015-02-22 19:17:19 +0000

    removed lots of code
    ode

--- Haskell/dynamic-programming/the-maximum-subarray/solution.hs
+++ Haskell/dynamic-programming/the-maximum-subarray/solution.hs
@@ -1,5 +1,6 @@
 import Control.Monad
 import Data.List
+import Test.QuickCheck
 
 getData :: IO (Int, [Int])
 getData = do
@@ -10,16 +11,10 @@ getData = do
 str2Int :: String -> Int
 str2Int = read :: String -> Int
 
--- slices :: Eq a => [a] -> [[a]]
--- slices [] = []
--- slices xs = tail $ inits xs ++ slices (tail xs)
 
-maxcont ar = maximum (scanl (\acc x -> acc+x) 0 ar)
--- maxcont ar =  maximum ( map (\x -> sum x) (slices ar))
-maxnoncont ar = (foldl (\acc x -> acc+x) 0 (filter (\y -> y>=0) ar))
 
--- maxnoncont ar = if res == 0 then maxcont ar else res
---   where res = maximum ( map (\x -> sum (filter (\y -> y >= 0) x)) (slices ar))
+
+
 
 maxSubarray :: (Num a, Ord a) => [a] -> a
 maxSubarray [x] = x
@@ -29,23 +24,8 @@ msReducer :: (Num a, Ord a) => (a, a) -> a -> (a, a)
 msReducer (maxEndingHere, maxSoFar) x = (meh, maxSoFar `max` meh)
     where meh = x `max` (maxEndingHere + x)
 
-maxsubseq = snd . foldl f ((0,[]),(0,[])) where
-	f ((h1,h2),sofar) x = (a,b) where
-		a = max (0,[]) (h1 + x, h2 ++ [x])
-		b = max sofar a
-
-max_sublist l = reverse $ fst $ foldl find_max ([],(0,[],0)) l where
-  find_max (max, (sum, r, rsum)) x = (max', (sum', r', rsum'))
-    where
-    (r', rsum')  = if (rsum > 0)
-                   then (x:r, rsum+x)
-                   else ([x], x)
-    (max', sum') = if (rsum' > sum)
-                   then (r', rsum')
-                   else (max, sum)
-
 showmaxes :: [Int] -> String
-showmaxes ar = show (maxcont ar) ++ " " ++show (maxnoncont ar)
+showmaxes ar = show (maxSubarray ar) ++ " " ++show ( maxSubarray $ sort  ar)
 
 main :: IO ()
 main = do
