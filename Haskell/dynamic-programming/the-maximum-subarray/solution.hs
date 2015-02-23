69eeb7ab514df4d3b35fb7bdf7ca034960594059
Jacek Podkanski <ruby.object@googlemail.com>
2015-02-23 02:59:50 +0000

    on the right track

--- Haskell/dynamic-programming/the-maximum-subarray/solution.hs
+++ Haskell/dynamic-programming/the-maximum-subarray/solution.hs
@@ -3,15 +3,16 @@ import Data.List
 import Debug.Trace
 
 -- test case 01 terminates due to timeout
+-- need to be able to accumulate values vithout using sort in showmaxes function
 
 -- kodan [2,-1,2,3,4,-5] 0 0 (-10000)
 kodan :: [Int] -> Int -> Int -> Int -> Int
 kodan [] max_ending_here max_so_far largest
-  | trace (show (max_ending_here, max_so_far, largest) ++ " <====== ") False = undefined
+  -- | trace (show (max_ending_here, max_so_far, largest) ++ " <====== ") False = undefined
   | otherwise = if max_so_far == 0 then largest else max_so_far
 
 kodan ar max_ending_here max_so_far largest_neg
-  | trace (show (ar, max_ending_here, max_so_far, largest_neg) ++ " <++++++ ") False = undefined
+  -- | trace (show (ar, max_ending_here, max_so_far, largest_neg) ++ " <++++++ ") False = undefined
   | otherwise = kodan (tail ar) max_ending_here2 max_so_far2 largest_neg2
   where x = head ar
         max_ending_here2 = (0 `max` (max_ending_here) + x)
