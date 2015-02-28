c561bdea56ec8434f764f0c14962e5db4f7cff41
Jacek Podkanski <ruby.object@googlemail.com>
2015-02-28 01:09:31 +0000

    prepared graph puzzle

---
+++ Haskell/graph-theory/even-tree/solution.hs
@@ -0,0 +1,22 @@
+import Control.Monad
+import Data.List
+import Debug.Trace
+
+process ::[[Int]] -> Int
+process ar = 1 -- fixme
+
+getData :: IO (Int, Int,[[Int]])
+getData = do
+  dd <- fmap (map read . words) getLine
+  let n = head dd
+  let m = last dd
+  ar <- replicateM m (fmap (map read . words) getLine)
+  return (n, m, ar)
+
+main :: IO ()
+main = do
+  inputs <- getData
+  let (_, _, ar) = inputs
+  -- print inputs
+  -- print ar
+  print (process  ar)
