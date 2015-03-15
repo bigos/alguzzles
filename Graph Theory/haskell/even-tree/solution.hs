deff8c55a6e0b3e994de6f7133ca6957880dd85a
Jacek Podkanski <ruby.object@googlemail.com>
2015-03-15 19:49:58 +0000

    code cleanup

--- Graph Theory/haskell/even-tree/solution.hs
+++ Graph Theory/haskell/even-tree/solution.hs
@@ -11,23 +11,11 @@ arr =  [[2,1],[3,1],[4,3],[5,2],[6,1],[7,2],[8,6],[9,8],[10,8]]
 mygr = buildG (minmaxvert ar) (allverts ar)
   where ar = [[2,1],[3,1],[4,3],[5,2],[6,1],[7,2],[8,6],[9,8],[10,8]]
 
--- *Main> let reme = remedges mygr [[1,3],[1,6]]
--- *Main> components reme
--- *Main> map (\x -> flatten x) (components reme) -- yess!!!
-
--- this one finds correct results
-gettingThere = ( (map (\x -> (length $ join $ forests mygr x,x, numberOfOddForests mygr x)) (vertComb (take 5 arr))))
-
-
-
-
-
-vertlen :: [Vertex] -> Int
-vertlen vv = length (vv)
+-- -------------------------------------
 
 finalResult gr ar = maximum $ map (\x -> length x) (ress2 gr ar)
 
-ress2 gr ar = map (\y -> fst (snd y)) $ filter (\x -> (snd (snd x)) == vertlen( vertices gr)) (ress gr ar)
+ress2 gr ar = map (\y -> fst (snd y)) $ filter (\x -> (snd (snd x)) == length (vertices gr)) (ress gr ar)
 
 ress :: Graph -> [[Int]] ->[ (Int,( [[Int]], Int))]
 ress gr ar = filter (\y -> fst y == 0) $ map (\x -> ((numberOfOddForests gr x) ,( x, ( length (join $ forests gr x)))) ) (vertComb ( ar))
@@ -50,13 +38,6 @@ remedges gr reme = buildG range newedges
         eds = map (\x ->  fst x) newedges ++ map (\x -> snd x) newedges
         range = (minimum eds, maximum eds)
 
-
-wow nd = reachable grWithRemovedEdges nd -- nd is a node number eg. 6
-  where gr = mygr
-        grWithRemovedEdges = remedges gr [[1,3],[1,6]]
-        res1 = reachable grWithRemovedEdges 1
-        res2 = reachable grWithRemovedEdges 6 -- finds connections in decomposed tree
-
 allverts :: [[Int]] -> [(Int, Int)]
 allverts ar = (map (\x -> (last x, head x)) ar) ++ (map (\x -> (head x, last x)) ar)
 
@@ -64,8 +45,7 @@ minmaxvert :: [[Int]] -> (Int, Int)
 minmaxvert ar = (minimum (map (\x -> (minimum x) ) ar), maximum (map (\x -> (maximum x)) ar) )
 
 process :: Int -> Int -> [[Int]] -> Graph -> Int
-process n m ar gr
-  | otherwise = finalResult gr ar
+process _ _ ar gr = finalResult gr ar
 
 
 getData :: IO (Int, Int,[[Int]])
