0fc2471bc1f1ba8ee35aadef1f5654b8ced97e75
Jacek Podkanski <ruby.object@googlemail.com>
2015-03-15 20:28:48 +0000

    some refactoring

--- Graph Theory/haskell/even-tree/solution.hs
+++ Graph Theory/haskell/even-tree/solution.hs
@@ -18,16 +18,19 @@ finalResult gr ar = maximum $ map (\x -> length x) (ress2 gr ar)
 ress2 gr ar = map (\y -> fst (snd y)) $ filter (\x -> (snd (snd x)) == length (vertices gr)) (ress gr ar)
 
 ress :: Graph -> [[Int]] ->[ (Int,( [[Int]], Int))]
-ress gr ar = filter (\y -> fst y == 0) $ map (\x -> ((numberOfOddForests gr x) ,( x, ( length (join $ forests gr x)))) ) (vertComb ar)
+ress gr ar = filter (\y -> fst y == 0) $ map (\x -> thedata gr x) (vertComb ar)
 
+thedata gr x = ( (numberOfOddForests gr x) ,( x, (length (join $ forests gr x))) )
+
+-- check subsequences
 vertComb :: [[Int]] -> [[[Int]]]
 vertComb ar = tail $ init $ subsequences (ar)
 
 numberOfOddForests :: Graph -> [[Int]] -> Int
 numberOfOddForests gr remd = length $ filter (\x -> odd (length x)) (forests gr remd)
 
-forests gr remd = map (\x -> flatten x) (components reme)
-  where reme = remedges gr remd
+-- check components
+forests gr remd = map (\x -> flatten x) (components (remedges gr remd))
 
 -- removing edges from a graph
 diflit = (edges mygr) \\ (allverts [[1,3],[6,1]])
