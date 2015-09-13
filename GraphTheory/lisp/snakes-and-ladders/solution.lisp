dca0f887dbc3b6dc9b258a9418f4b3989247b596
Jacek Podkanski <ruby.object@googlemail.com>
2015-09-13 23:23:08 +0100

    array based connections

--- GraphTheory/lisp/snakes-and-ladders/solution.lisp
+++ GraphTheory/lisp/snakes-and-ladders/solution.lisp
@@ -80,25 +80,24 @@
 (defun add-to-visited (n)
   (push n *visited*))
 
-(defun node-type (node)
-  (when (> (length (cadr node)) 1)
-      (if (> (caadr node)
-             (cadadr node))
-          'snake
-          'ladder)))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun connections (edges)
-  (loop for x from 1 to 100
-     collecting
-       (list x
-             (loop for e in edges
-                when (eq (car e) x)
-                collect (cadr e)))))
-
-(defun boustrophedon ()
-  (loop for x from 1 below 100
-       collecting (list x (1+ x))))
+;;; usage: (arr-connections (ladders-1) (snakes-1)
+(defun arr-connections (ladders snakes)
+  (let* ((max-val 100)
+         (max-step 6)
+         (my-arr (make-array max-val :initial-element nil)))
+    (loop for x from 1 below max-val do
+         (setf (elt my-arr x)
+               (loop for d from 1 to max-step
+                  when (<= (+ x d) max-val)
+                  collect (+ x d))))
+    (loop for l in ladders do
+         (setf (elt my-arr (car l))
+               (cdr l)))
+    (loop for l in snakes do
+         (setf (elt my-arr (car l))
+               (cdr l)))
+    my-arr))
 
 ;; (solve-me (ladders-1) (snakes-1))
 (defun solve-me (ladders snakes)
@@ -109,25 +108,6 @@
     (format t "connections ~A~%" (connections edges))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defun the-data ()
-  '((1 (2)) (2 (3 72)) (3 (4)) (4 (5)) (5 (6)) (6 (7 80)) (7 (8))
-    (8 (9 52)) (9 (10)) (10 (11)) (11 (12)) (12 (13)) (13 (14))
-    (14 (15)) (15 (16)) (16 (17)) (17 (18)) (18 (19)) (19 (20))
-    (20 (21)) (21 (22)) (22 (23)) (23 (24)) (24 (25)) (25 (26))
-    (26 (27 42)) (27 (28)) (28 (29)) (29 (30)) (30 (31)) (31 (32))
-    (32 (33)) (33 (34)) (34 (35)) (35 (36)) (36 (37)) (37 (38 29))
-    (38 (39)) (39 (40 11)) (40 (41)) (41 (42)) (42 (43)) (43 (44 33))
-    (44 (45)) (45 (46)) (46 (47)) (47 (48)) (48 (49)) (49 (50))
-    (50 (51)) (51 (52 19)) (52 (53)) (53 (54 7)) (54 (55)) (55 (56))
-    (56 (57)) (57 (58)) (58 (59)) (59 (60 5)) (60 (61)) (61 (62))
-    (62 (63)) (63 (64)) (64 (65)) (65 (66)) (66 (67)) (67 (68))
-    (68 (69)) (69 (70)) (70 (71)) (71 (72)) (72 (73)) (73 (74))
-    (74 (75)) (75 (76)) (76 (77)) (77 (78 21)) (78 (79)) (79 (80 23))
-    (80 (81)) (81 (82 3)) (82 (83)) (83 (84)) (84 (85)) (85 (86))
-    (86 (87)) (87 (88)) (88 (89)) (89 (90)) (90 (91)) (91 (92))
-    (92 (93)) (93 (94)) (94 (95)) (95 (96)) (96 (97)) (97 (98))
-    (98 (99)) (99 (100)) (100 NIL)))
 
 (defun ladders-1 ()
   '((8 52) (6 80) (26 42) (2 72)))
