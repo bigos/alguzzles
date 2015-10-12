b3f63bb4df7e06e1c285d8a9568e3b836bfd8c5b
Jacek Podkanski <ruby.object@googlemail.com>
2015-10-12 22:03:31 +0100

    pangrams finished

--- Strings/lisp/pangrams/solution.lisp
+++ Strings/lisp/pangrams/solution.lisp
@@ -1,6 +1,18 @@
+(defun letters-used (string)
+  (let ((arr-char (make-array 128)))
+    (loop for x from 0 below (length string)
+       with c
+       do
+         (setf c (elt string x))
+         (setf (elt arr-char (char-code c)) c))
+    arr-char))
+
 (defun pangram-p (string)
-  (let ((todo))
-    nil))
+  (let ((letters (letters-used string)))
+    (every (lambda (x) x)
+           (loop for x from (char-code #\a) to (char-code #\z)
+              collect (or (not (numberp (elt letters x)))
+                          (not (numberp (elt letters (char-code (char-upcase (code-char x)))))))))))
 
 (defun solution (&optional stream)
   (let* ((test-string (read-line stream)))
