3647d47d2a2bac95912643f881e1ad2b415ad630
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2015-10-13 09:28:56 +0100

    got the data

---
+++ Warmup/lisp/diagonal-difference/solution.lisp
@@ -0,0 +1,34 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((rows (parse-integer (read-line stream))))
+    (format t "~A~%" rows)
+    (princ
+     (loop for r from 0 below rows
+        collecting (split-and-parse (read-line stream))))))
+
+ ;; (solution) ; uncomment this when running on hacker-rank
+
+;;; still need to add  removing vertices
+(defun repl-main ()
+  (let ((path (if(search "chess" (machine-instance))
+                 "Documents/hackerrank/"
+                 "Programming/hackerrank/")))
+    (with-open-file (s (concatenate 'string
+                                    (directory-namestring (user-homedir-pathname))
+                                    path
+                                    "Warmup/lisp/diagonal-difference/"
+                                    "input0.txt"))
+      (solution s))))
