d01dbff1f7cb69da05538b2361fa76600a6621d6
Jacek Podkanski <ruby.object@googlemail.com>
2015-10-14 08:14:19 +0100

    pairs - preparedfor solving

---
+++ Search/lisp/pairs/solution.lisp
@@ -0,0 +1,35 @@
+(defun solve-me (l1 l2)
+  (format t "~A ~A" l1 l2))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((l1 (split-and-parse (read-line stream)))
+        (l2 (split-and-parse (read-line stream))))
+    (solve-me l1 l2)))
+
+ ;; (solution) ; uncomment this when running on hacker-rank
+
+;;; still need to add  removing vertices
+(defun repl-main ()
+  (let ((path (if(search "chess" (machine-instance))
+                 "Documents/hackerrank/"
+                 "Programming/hackerrank/")))
+    (with-open-file (s (concatenate 'string
+                                    (directory-namestring (user-homedir-pathname))
+                                    path
+                                    "Search/lisp/pairs/"
+                                    "input0.txt"))
+      (solution s))))
