b6a5c61807464e13774c2c396944d31b25fbb907
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2015-10-14 15:17:58 +0100

    added binary search function

--- Search/lisp/pairs/solution.lisp
+++ Search/lisp/pairs/solution.lisp
@@ -1,19 +1,33 @@
+(defun binary-search (value array)
+  (let ((low 0)
+        (high (1- (length array))))
+    (do () ((< high low) nil)
+      (let ((middle (floor (/ (+ low high) 2))))
+        (cond ((> (aref array middle) value)
+               (setf high (1- middle)))
+              ((< (aref array middle) value)
+               (setf low (1+ middle)))
+              (t (return middle)))))))
+
 (defun solve-me (n k set)
   (let ((diffs 0)
-        (set2 set))
+        (a (make-array n :initial-element 0)))
+    (loop for i from 0 below n do
+         (setf (elt a i) (elt set i)))
     (loop for x in set do
-         (loop for y in set2 do
-              (when (eq k (abs (- x y)))
-                (incf diffs))))
-    (/ diffs 2)))
+         (when (or
+                ;; binary search for x + k
+                ;; binary search for x - k
+                (= 1 1))
+           (incf diffs))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun split-by-one-space (string)
-  (loop for i = 0 then (1+ j)
-     as j = (position #\Space string :start i)
-     collect (subseq string i j)
-     while j))
+  (defun split-by-one-space (string)
+    (loop for i = 0 then (1+ j)
+       as j = (position #\Space string :start i)
+       collect (subseq string i j)
+       while j)))
 
 (defun split-and-parse (string)
   (map 'list
@@ -23,7 +37,7 @@
 
 (defun solution (&optional stream)
   (let ((l1 (split-and-parse (read-line stream)))
-        (l2 (split-and-parse (read-line stream))))
+        (l2 (sort (split-and-parse (read-line stream)) #'<)))
     (princ (solve-me (car l1) (cadr l1) l2))))
 
  ;; (solution) ; uncomment this when running on hacker-rank
@@ -37,5 +51,5 @@
                                     (directory-namestring (user-homedir-pathname))
                                     path
                                     "Search/lisp/pairs/"
-                                    "input5.txt"))
+                                    "input0.txt"))
       (solution s))))
