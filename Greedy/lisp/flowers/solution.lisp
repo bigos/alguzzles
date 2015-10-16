df2a88a85a26dc24eb558f8c691273f6fc77aecd
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2015-10-16 14:49:18 +0100

    prepared new puzzle

---
+++ Greedy/lisp/flowers/solution.lisp
@@ -0,0 +1,31 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((n-and-k (split-and-parse (read-line stream)))
+        (ints (split-and-parse (read-line stream))))
+    (format t "~A ~A~%" n-and-k ints)))
+
+ ;; (solution) ; uncomment this when running on hacker-rank
+
+;;; still need to add  removing vertices
+(defun repl-main ()
+  (let ((path (if(search "chess" (machine-instance))
+                 "Documents/hackerrank/"
+                 "Programming/hackerrank/")))
+    (with-open-file (s (concatenate 'string
+                                    (directory-namestring (user-homedir-pathname))
+                                    path
+                                    "Greedy/lisp/flowers/"
+                                    "input0.txt"))
+      (solution s))))
