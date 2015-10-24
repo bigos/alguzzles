d0a401e65602488c8c1b6469a47919dc76c7acc6
Jacek Podkanski <ruby.object@googlemail.com>
2015-10-24 05:01:32 +0100

    prepared new puzzle

---
+++ DynamicProgramming/lisp/string-reduction/solution.lisp
@@ -0,0 +1,41 @@
+(defun solve-me (n cases)
+  (format t "~A ~A~%" n cases))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+
+(defun string-to-characters (string)
+  (loop for c across string
+     collect c))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((n (parse-integer (read-line stream)))
+        (cases))
+    (dotimes (x n)
+      (setf cases (concatenate 'list
+                               cases
+                               (list (string-to-characters (read-line stream))))))
+    (solve-me n cases)))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
