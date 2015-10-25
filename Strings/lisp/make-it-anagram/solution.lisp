47945caac32bfaa9867944828e989d6d95feb44b
Jacek Podkanski <ruby.object@googlemail.com>
2015-10-25 23:34:45 +0000

    prepared new puzzle

---
+++ Strings/lisp/make-it-anagram/solution.lisp
@@ -0,0 +1,33 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((n (parse-integer (read-line stream)))
+        (nk) (nints-a) (nints-b))
+    (dotimes (x n)
+      (setf nk (split-and-parse (read-line stream)))
+      (setf nints-a (split-and-parse (read-line stream)))
+      (setf nints-b (split-and-parse (read-line stream)))
+      (solve-me (car nk) (cadr nk) nints-a nints-b))))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
