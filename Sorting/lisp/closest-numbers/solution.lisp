01fa326a04dbdc8be7ea986f17d46f821b64b482
Jacek Podkanski <ruby.object@googlemail.com>
2015-10-26 21:49:10 +0000

    prepared new puzzle

---
+++ Sorting/lisp/closest-numbers/solution.lisp
@@ -0,0 +1,32 @@
+(defun solve-me (n a)
+  (format t "~A ~A" n a))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((n (parse-integer (read-line stream)))
+        (a (split-and-parse (read-line stream))))
+    (solve-me n a)))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
