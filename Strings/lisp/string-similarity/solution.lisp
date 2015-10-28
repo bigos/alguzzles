c743115fec110f4bd2a1fc297a03bcbf6a889d61
Jacek Podkanski <ruby.object@googlemail.com>
2015-10-28 00:08:29 +0000

    prepared new puzzle

---
+++ Strings/lisp/string-similarity/solution.lisp
@@ -0,0 +1,32 @@
+(defun solve-me (tc strings)
+  (format t "~A ~A" tc strings))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let* ((tc (parse-integer (read-line stream)))
+         (strings ( loop for x from 1 to tc collect (read-line stream))))
+    (solve-me tc strings)))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
