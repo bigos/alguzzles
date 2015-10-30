c9afb81d36c1d4e843fdd95c0d22cd0924659941
Jacek Podkanski <ruby.object@googlemail.com>
2015-10-30 22:03:36 +0000

    playing with sorting

--- Sorting/lisp/running-time-of-algorithms/solution.lisp
+++ Sorting/lisp/running-time-of-algorithms/solution.lisp
@@ -1,5 +1,24 @@
 (defun solve-me (n a)
-  (format t "~A ~A~%" n a))
+  (format t "~A ~A~%" n a)
+  (labels
+      ((gteq (x)
+         (format t "~&********* ~A~%" x)
+         (if (or (> x 0) (< x (1- n)))
+             (<= (aref a x) (aref a (1+ x)))
+             T))
+       (swap-vals (x)
+         (let ((tv))
+           (setf tv (aref a x))
+           (setf (aref a x) (aref a (1+ x)))
+           (setf (aref a (1+ x)) tv))))
+    (loop for x = 0 then (if (gteq x)
+                             (+ x 1)
+                             (+ x 0))
+       while (< x n)
+       do (unless (gteq x) (progn (swap-vals x)
+                                  (when (> x 0)
+                                    (setf x (1- x)))))))
+  a)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun split-by-one-space (string)
@@ -15,8 +34,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun solution (&optional stream)
-  (let ((n (parse-integer (read-line stream)))
-        (a (split-and-parse (read-line stream))))
+  (let* ((n (parse-integer (read-line stream)))
+        (a (make-array n :initial-contents (split-and-parse (read-line stream)))))
     (solve-me n a)))
 
 ;; (solution) ; uncomment this when running on hacker-rank
