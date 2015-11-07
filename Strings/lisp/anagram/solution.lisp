dd1c97661f1366ca1ee9e77ebc4f6e813c3153b0
Jacek Podkanski <ruby.object@googlemail.com>
2015-11-07 22:48:08 +0000

    looks good

--- Strings/lisp/anagram/solution.lisp
+++ Strings/lisp/anagram/solution.lisp
@@ -1,11 +1,41 @@
+ (defun find-different-count (s1 s2)
+   (let ((ht1 (make-hash-table))
+         (ht2 (make-hash-table))
+         (htr (make-hash-table))
+         (hc1) (hc2)
+         (hcdiff)
+         (hcdiffsum 0))
+     (loop for c1 in s1 do
+          (incf (gethash c1 ht1 0))
+          (incf (gethash c1 htr 0)))
+     (loop for c2 in s2 do
+          (incf (gethash c2 ht2 0))
+          (incf (gethash c2 htr 0)))
+     ;; (format t "~&=====~%")
+     (maphash (lambda (k v)
+                (setf hc1 (gethash k ht1 0))
+                (setf hc2 (gethash k ht2 0))
+                (setf hcdiff (abs (- hc1 hc2)))
+                (incf hcdiffsum hcdiff)
+                ;; (format t "~@C: ~D (~S ~S) ~A| ~A~%" k v
+                ;;         hc1
+                ;;         hc2
+                ;;         hcdiff
+                ;;         hcdiffsum)
+                )
+              htr)
+      (/ hcdiffsum 2)))
+
 (defun find-solution (l a)
   (let* ((half-l (/ l 2))
          (s1 (sort (subseq a 0 (- half-l 0)) 'char<))
          (s2 (sort (subseq a half-l) 'char<)))
-     (format t "~s ~S ~%" s1 s2)
-    (count nil
-           (map 'list (lambda (x)  (eq (car x) (cadr x)))
-                (mapcar #'list s1 s2 )))))
+    ;; (format t "~s ~S ~%" s1 s2)
+    (find-different-count s1 s2)
+    ;; (count nil
+    ;;        (map 'list (lambda (x)  (eq (car x) (cadr x)))
+    ;;             (mapcar #'list s1 s2 )))
+    ))
 
 (defun solve-me (str)
   (let ((l (length str))
@@ -40,7 +70,7 @@
                       :directory
                       (pathname-directory
                        (parse-namestring *load-pathname*))
-                      :name "input0" :type "txt"))
+                      :name "input00" :type "txt"))
     (solution s)))
 
 (main)
