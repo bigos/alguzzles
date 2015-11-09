c37685d9441cd88d093bc9b5d8be8d259db45eea
Jacek Podkanski <ruby.object@googlemail.com>
2015-11-09 21:44:15 +0000

    three test cases have failed

--- Implementation/lisp/library-fine/solution.lisp
+++ Implementation/lisp/library-fine/solution.lisp
@@ -1,5 +1,22 @@
+(defun date-part (dmy units)
+  (elt units (position dmy (list 'd 'm 'y))))
+
 (defun solve-me (actual expected)
-       (format t "~A ~A" actual expected))
+  ;; (format t "~A ~A" actual expected)
+  (if (> (date-part 'y actual)
+         (date-part 'y expected))
+      10000
+      (if (and (eq (date-part 'y actual)
+                   (date-part 'y expected))
+               (> (date-part 'm actual)
+                  (date-part 'm expected)))
+          (* 500 (- (date-part 'm actual)
+                    (date-part 'm expected)))
+          (if (eq (date-part 'm actual)
+                  (date-part 'm expected))
+              (* 15 (- (date-part 'd actual)
+                       (date-part 'd expected)))
+              0))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun split-by-one-space (string)
@@ -17,7 +34,7 @@
 (defun solution (&optional stream)
   (let ((d1 (split-and-parse (read-line stream)))
         (d2 (split-and-parse (read-line stream))))
-    (solve-me d1 d1)))
+    (princ (solve-me d1 d2))))
 
 ;; (solution) ; uncomment this when running on hacker-rank
 
