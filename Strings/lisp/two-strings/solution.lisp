c92d1608f71ed5264b5ee98152d0519e8b5db54d
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2015-11-09 11:08:02 +0000

    prepared the puzzle

---
+++ Strings/lisp/two-strings/solution.lisp
@@ -0,0 +1,33 @@
+(defun solve-me (s1 s2)
+  (format t "~s   ~s~%" s1 s2))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let* ((tc (parse-integer (read-line stream))))
+    (dotimes (x tc)
+      (solve-me  (read-line stream)
+                 (read-line stream)))))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
