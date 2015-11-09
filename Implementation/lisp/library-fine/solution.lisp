9b285d76cac12c595d124f0d1fd7473e65372127
Jacek Podkanski <ruby.object@googlemail.com>
2015-11-09 20:53:01 +0000

    new puzzle started

---
+++ Implementation/lisp/library-fine/solution.lisp
@@ -0,0 +1,32 @@
+(defun solve-me (actual expected)
+       (format t "~A ~A" actual expected))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((d1 (split-and-parse (read-line stream)))
+        (d2 (split-and-parse (read-line stream))))
+    (solve-me d1 d1)))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
