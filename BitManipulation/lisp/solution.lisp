98c9c639c086d4e27fd5cd7288ca0d20de62190e
Jacek Podkanski <ruby.object@googlemail.com>
2015-11-10 23:02:54 +0000

    prepared new puzzle

---
+++ BitManipulation/lisp/solution.lisp
@@ -0,0 +1,35 @@
+(defun solve-me (n l)
+  (format t "~A ~A~%" n l))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((tc (parse-integer (read-line stream))))
+    (dotimes (x tc)
+      (solve-me
+       (parse-integer (read-line stream))
+       (split-and-parse (read-line stream))))))
+
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
