0e4bc237e4773705d82e935724f507941a108910
Jacek Podkanski <ruby.object@googlemail.com>
2015-11-11 23:39:51 +0000

    prepared new puzzle

---
+++ Implementation/lisp/the-time-in-words/solution.lisp
@@ -0,0 +1,32 @@
+(defun solve-me (h m)
+  (format t "~A ~A" h m))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((h (parse-integer (read-line stream)))
+        (m (parse-integer (read-line stream))))
+    (solve-me h m)))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
