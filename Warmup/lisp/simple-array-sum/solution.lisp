ada56d049647bf3f61d68153330222c89a23505d
Jacek Podkanski <ruby.object@googlemail.com>
2015-11-11 22:40:02 +0000

    simple array sum passed

---
+++ Warmup/lisp/simple-array-sum/solution.lisp
@@ -0,0 +1,33 @@
+(defun solve-me (n i)
+  (princ (loop for x in i summing x)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((n (parse-integer (read-line stream))))
+    (solve-me
+     n (split-and-parse (read-line stream)))))
+
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input06" :type "txt"))
+    (solution s)))
+
+(main)
