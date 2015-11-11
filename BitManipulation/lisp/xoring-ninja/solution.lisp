1baf9b0688da82c7602ebc41b9e846f3c80415d4
Jacek Podkanski <ruby.object@googlemail.com>
2015-11-11 20:49:13 +0000

    solved the puzzle

--- BitManipulation/lisp/xoring-ninja/solution.lisp
+++ BitManipulation/lisp/xoring-ninja/solution.lisp
@@ -1,41 +1,13 @@
-(defun next-combination (n a)
-  (let ((k (length a)) m)
-    (loop for i from 1 do
-         (when (> i k) (return nil))
-         (when (< (aref a (- k i)) (- n i))
-           (setf m (aref a (- k i)))
-           (loop for j from i downto 1 do
-                (incf m)
-                (setf (aref a (- k j)) m))
-           (return t)))))
-
-;; (defun all-combinations (n k)
-;;   (if (or (< k 0) (< n k)) '()
-;;       (let ((a (make-array k)))
-;;         (loop for i below k do (setf (aref a i) i))
-;;         (loop collect (coerce a 'list) while (next-combination n a)))))
-
-(defun map-combinations (n k fun)
-  (if (and (>= k 0) (>= n k))
-      (let ((a (make-array k)))
-        (loop for i below k do (setf (aref a i) i))
-        (loop do (funcall fun (coerce a 'list)) while (next-combination n a)))))
+(require 'sb-rotate-byte)
+;; (sb-rotate-byte:rotate-byte 3 (byte 32 0) 3)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun solve-me (n a)
-  (let ((res 0))
-    (loop for x from 1 to n do
-         (map-combinations n x 'null
-                           ;; (lambda (y)
-                           ;;   (let ((tr 0))
-                           ;;     (loop for i in y do
-                           ;;          (setf tr (logxor tr (elt a i))))
-                           ;;     (incf res tr)))
-                           )
-         )
-    (princ (mod res  (+ 7 (expt 10 9))))
-    (terpri)))
+  (princ (mod (* (apply 'logior a)
+                 (expt 2 (1- n)))
+              (+ 7 (expt 10 9))))
+  (terpri))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun split-by-one-space (string)
@@ -56,7 +28,7 @@
     (dotimes (x tc)
       (setf n (parse-integer (read-line stream)))
       (solve-me
-       n (make-array n :initial-contents (split-and-parse (read-line stream)))))))
+       n (split-and-parse (read-line stream))))))
 
 
 ;; (solution) ; uncomment this when running on hacker-rank
