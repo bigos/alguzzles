f2821aa8c4c35360d070e0104d897a1345b3f3cf
Jacek Podkanski <ruby.object@googlemail.com>
2015-11-12 22:59:03 +0000

    puzzle accepted

--- Implementation/lisp/the-time-in-words/solution.lisp
+++ Implementation/lisp/the-time-in-words/solution.lisp
@@ -1,5 +1,58 @@
+(defun digit-word (n)
+  (case n
+    (0 "") (1 "one") (2 "two") (3 "three") (4 "four")
+    (5 "five") (6 "six") (7 "seven") (8 "eight") (9 "nine")
+    (10 "ten") (11 "eleven") (12 "twelve") (13 "thirteen") (14 "fourteen")
+    (15 "fifteen") (16 "sixteen") (17 "seventeen") (18 "eighteen") (19 "nineteen")
+    ))
+
+(defun minpart (n)
+  (let ((nx (if (<= n 30)
+                n
+                (- 60 n))))
+    (format nil
+            "~a ~a"
+            (if (>= nx 20)
+                (format nil "twenty ~a" (digit-word (- nx 20)))
+                (digit-word nx))
+            (if (= nx 1)
+                "minute"
+                "minutes"))))
+
+(defun minute-word (h m)
+  (let ((topast)
+        (hr-part)
+        (min-part))
+    (if (= m 0)
+        (format t "~A o' clock" (digit-word h))
+        (progn
+          (cond
+            ((= m 30)
+             (setf topast "past"
+                   hr-part (digit-word h)
+                   min-part "half"))
+            ((= m 15)
+             (setf topast "past"
+                   hr-part (digit-word h)
+                   min-part "quarter"))
+            ((= m 45)
+             (setf topast "to"
+                   hr-part (digit-word (1+ h))
+                   min-part "quarter"))
+            ((< m 30)
+             (setf topast "past"
+                   hr-part (digit-word h)
+                   min-part (minpart m))
+             )
+            ((> m 30)
+             (setf topast "to"
+                   hr-part (digit-word (1+ h))
+                   min-part (minpart m))
+             ))
+          (format t "~A ~A ~A" min-part topast hr-part)))))
+
 (defun solve-me (h m)
-  (format t "~A ~A" h m))
+  (minute-word h m))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun split-by-one-space (string)
