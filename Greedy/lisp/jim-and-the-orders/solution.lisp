ff39a9559c43761507bdfa5dd424bc5280390705
Jacek Podkanski <ruby.object@googlemail.com>
2015-11-16 01:05:35 +0000

    new puzzle

---
+++ Greedy/lisp/jim-and-the-orders/solution.lisp
@@ -0,0 +1,37 @@
+;;;
+
+(defun solve-me (orders)
+  (princ orders))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let* ((tc (parse-integer (read-line stream)))
+         (orders
+          (loop for x from 0 below tc
+             for o = (split-and-parse (read-line stream))
+             collect (list x (car o) (cadr o)))))
+    (solve-me orders)))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
