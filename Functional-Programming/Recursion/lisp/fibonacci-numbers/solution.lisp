8a7414e26c895f589d226353462d1f252869e19c
Jacek Podkanski <ruby.object@googlemail.com>
2015-11-16 22:54:20 +0000

    another puzzle done

---
+++ Functional-Programming/Recursion/lisp/fibonacci-numbers/solution.lisp
@@ -0,0 +1,32 @@
+(defun fibonacci (n &optional (a 0) (b 1) (acc ()))
+  (if (zerop n)
+      (princ (car acc))
+      (fibonacci (1- n) b (+ a b) (cons a acc))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (fibonacci (parse-integer (read-line stream))))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
