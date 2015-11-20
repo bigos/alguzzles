ca9ca96f1bf4f0f4ea9a33dc4fb5472ea556c5c2
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2015-11-20 13:07:18 +0000

    next puzzle accepted

---
+++ Functional-Programming/Recursion/lisp/string-mingling/solution.lisp
@@ -0,0 +1,35 @@
+(defun solve-me (p q)
+  (loop for x1 across p
+     for y1 across q do
+       (princ x1)
+       (princ y1)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((p (read-line stream))
+        (q (read-line stream)))
+    (solve-me p q)))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
