f5f33125b64358476204f0ed44b3488f5b500931
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2015-11-20 13:34:50 +0000

    prepared new puzzle

---
+++ Functional-Programming/Introduction/lisp/evaluating-e-power-x/solution.lisp
@@ -0,0 +1,33 @@
+(defun solve-me (x)
+  (format t "the x is ~a~%" x))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((n (parse-integer (read-line stream))))
+    (dotimes (_ n)
+      (solve-me
+       (read-from-string (read-line stream))))))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
