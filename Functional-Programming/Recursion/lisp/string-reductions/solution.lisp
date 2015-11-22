d90c2f30186dac89dbc5a55b0ef0bcc7831210c3
Jacek Podkanski <ruby.object@googlemail.com>
2015-11-22 21:59:24 +0000

    prepared new puzzle

---
+++ Functional-Programming/Recursion/lisp/string-reductions/solution.lisp
@@ -0,0 +1,28 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((str (read-line stream)))
+    (princ str)))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
