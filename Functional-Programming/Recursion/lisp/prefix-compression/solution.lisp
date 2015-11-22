8f9a9400660477392cdb4a7ed6f76ed0ad0b0c46
Jacek Podkanski <ruby.object@googlemail.com>
2015-11-22 23:19:46 +0000

    prepared new puzzle

---
+++ Functional-Programming/Recursion/lisp/prefix-compression/solution.lisp
@@ -0,0 +1,32 @@
+(defun solve-me (x y)
+  (format t "~A~%~A~%" x y))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((x (read-line stream))
+        (y (read-line stream)))
+    (solve-me x y)))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
