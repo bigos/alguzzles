85cf11429c8784b77e73bf63aee9846e21c5a825
Jacek Podkanski <ruby.object@googlemail.com>
2015-11-22 18:47:35 +0000

    five testcases passed this time

--- Functional-Programming/Recursion/haskell/super-digit/solution.hs
+++ Functional-Programming/Recursion/haskell/super-digit/solution.hs
@@ -4,13 +4,13 @@ import Control.Monad
 solveMe :: [Int] -> [Int]
 solveMe n
   | length n == 1 = n
-  | otherwise     = tail n
+  | otherwise     = solveMe (map (\x -> read [x] :: Int) (show (sum n)))
 
-intToStr :: Int -> [Char]
-intToStr n = show n
+-- intToStr :: Int -> [Char]
+-- intToStr n = show n
 
 strToInts :: String -> [Int]
-strToInts s = map (read . (:"")) s
+strToInts s = map (\x -> read [x] :: Int) s
 
 prepareArgs :: [Char] -> Int -> [Int]
 prepareArgs n k =  (concat $ replicate k (strToInts n) )
@@ -22,4 +22,4 @@ main = do
     -- assign args, reading second arg as integer using read
     let myargs = prepareArgs (head nk) (read ( head (tail nk)))
     let res = solveMe myargs
-    print res
+    print ( head res)
