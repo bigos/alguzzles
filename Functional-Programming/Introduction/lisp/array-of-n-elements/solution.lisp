aea1a67ab4df03b6fd80e655f232704603b61a71
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2015-11-25 16:49:56 +0000

    work in progress

---
+++ Functional-Programming/Introduction/lisp/array-of-n-elements/solution.lisp
@@ -0,0 +1,39 @@
+(defun solve-me (n l)
+  ;; (format t "~&puzzle ~A ~A~%" n l)
+  (if (zerop l )
+      nil
+      (progn
+        (princ (car n))
+        (when (> l 1) (princ ", "))
+        (solve-me (cdr n) (1- l)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let* ((n (parse-integer (read-line stream))))
+    (princ "[")
+    (solve-me (loop for x from 1 to n collect x) n)
+    (princ "]")))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
