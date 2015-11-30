4e81398484ccc8c543249060a5efa550a9907972
Jacek Podkanski <ruby.object@googlemail.com>
2015-11-30 03:11:16 +0000

    some progress

--- Functional-Programming/AdHoc/lisp/captain-prime/solution.lisp
+++ Functional-Programming/AdHoc/lisp/captain-prime/solution.lisp
@@ -1,5 +1,65 @@
+(defun prime-p (n)
+  (let ((x (parse-integer n)))
+    (or (eq 1 x)
+        (not
+         (loop for d from 2 below (sqrt x)
+            for m = (mod x d)
+            when (zerop m)
+            collect d
+            until (zerop m))))))
+
+;; (successive-left "1367" 4 T )
+(defun successive-left (n ln r)
+  (let ((ss (subseq n 1)))
+    (let ((p (prime-p ss)))
+      (format t "~A ~A ~a~%" p ss (eq p r))
+      (if (not (eq p r))
+          nil
+          (if (= ln 2)
+              (eq p r)
+              (successive-left ss (1- ln) r))))))
+
+;; (successive-right "2333" 4 T )
+(defun successive-right (n ln r)
+  (let ((ss (subseq n 0 (1- ln))))
+    (let ((p (prime-p ss)))
+      (format t "~A ~A ~a~%" p ss (eq p r))
+      (if (not (eq p r))
+          nil
+          (if (= ln 2)
+              (eq p r)
+              (successive-right ss (1- ln) r))))))
+
+
+(defun contains-zero-p (n)
+  (position #\0 n))
+
+(defun central-p (n ln)
+  (or (not (contains-zero-p n))
+      (prime-p n)
+      (successive-left n ln T)
+      (successive-right n ln T)))
+
+(defun left-p (n ln)
+  (or (not (contains-zero-p n))
+      (prime-p n)
+      (successive-left n ln T)
+      (successive-right n ln NIL)))
+
+(defun right-p (n ln)
+  (or (not (contains-zero-p n))
+      (prime-p n)
+      (successive-left n ln NIL)
+      (successive-right n ln T)))
+
 (defun solve-me (n)
-  (format t "~A~%" n))
+  ;; (format t "~A~%" n)
+  (let ((ln (length n)))
+    (princ (cond ((central-p n ln) "CENTRAL")
+                 ((left-p n ln) "LEFT")
+                 ((right-p n ln) "RIGHT")
+                 (T "DEAD")))
+    (terpri)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun split-by-one-space (string)
@@ -17,7 +77,7 @@
 (defun solution (&optional stream)
   (let* ((persons (parse-integer (read-line stream))))
     (loop for x from 1 to persons do
-         (solve-me (parse-integer (read-line stream))))))
+         (solve-me (read-line stream)))))
 
 ;; (solution) ; uncomment this when running on hacker-rank
 
