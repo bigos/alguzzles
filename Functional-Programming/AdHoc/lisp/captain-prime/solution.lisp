83c764bac1dfc09f5fe264db597a1d6944aa8cdb
Jacek Podkanski <ruby.object@googlemail.com>
2015-12-06 00:28:42 +0000

    some test cases pass

--- Functional-Programming/AdHoc/lisp/captain-prime/solution.lisp
+++ Functional-Programming/AdHoc/lisp/captain-prime/solution.lisp
@@ -1,58 +1,57 @@
 (defun prime-p (n)
   ;;(format t "~&prime-p ~A~%" n)
   (let ((x (parse-integer n)))
-    (or (eq 1 x)
-        (not
-         (loop for d from 2 below (sqrt x)
-            for m = (mod x d)
-            when (zerop m)
-            collect d
-            until (zerop m))))))
+    (not
+     (loop for d from 2 to (sqrt x)
+        for m = (mod x d)
+        when (zerop m)
+        collect d
+        until (zerop m)))))
 
 ;; (successive-left "1367" 4 T )
-(defun successive-left (n ln r)
-  ;; (format t "~&left ~A ~A ~A~%" n ln r)
+(defun successive-left (n ln )
+  ;;(format t "~&left ~A ~A ~%" n ln )
   (let ((ss (subseq n 1)))
     (let ((p (prime-p ss)))
-      ;; (format t "~A ~A ~a~%" p ss (eq p r))
       (if (not p)
           nil
           (if (= ln 2)
               p
-              (successive-left ss (1- ln) r))))))
+              (successive-left ss (1- ln)))))))
 
 ;; (successive-right "2333" 4 T )
-(defun successive-right (n ln r)
-  ;; (format t "~&right ~A ~A ~A~%" n ln r)
+(defun successive-right (n ln )
+  ;;(format t "~&right ~A ~A~%" n ln )
   (let ((ss (subseq n 0 (1- ln))))
     (let ((p (prime-p ss)))
-      ;; (format t "~A ~A ~a~%" p ss (eq p r))
       (if (not p)
           nil
           (if (= ln 2)
               p
-              (successive-right ss (1- ln) r))))))
+              (successive-right ss (1- ln)))))))
+
+
 
 (defun contains-zero-p (n)
   (position #\0 n))
 
 (defun central-p (n ln)
-  ;; (format t "~%~%~&trying central~%")
+  ;;(format t "~%~%~&trying central ~A ~A~%" n ln)
   (and
-   (successive-left n ln T)
-   (successive-right n ln T)))
+   (successive-left n ln)
+   (successive-right n ln)))
 
 (defun left-p (n ln)
   ;; (format t "~%~%~&trying left~%")
   (and
-   (successive-left n ln T)
-   (not (successive-right n ln T))))
+   (successive-left n ln)
+   (not (successive-right n ln))))
 
 (defun right-p (n ln)
   ;; (format t "~%~%~&trying right~%")
   (and
-   (not (successive-left n ln T))
-   (successive-right n ln T)))
+   (not (successive-left n ln))
+   (successive-right n ln)))
 
 (defun solve-me (n)
   ;; (format t "~A~%" n)
@@ -63,7 +62,9 @@
                     "CENTRAL"
                     (if (left-p n ln)
                         "LEFT"
-                        (if (right-p n ln) "RIGHT")))
+                        (if (right-p n ln)
+                            "RIGHT"
+                            "DEAD")))
                "DEAD"))
     (terpri)))
 
