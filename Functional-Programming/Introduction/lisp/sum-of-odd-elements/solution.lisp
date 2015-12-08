b1462702066cf6d3bcda7d2cfc66ba059c8fb92c
Jacek Podkanski <ruby.object@googlemail.com>
2015-12-08 00:01:46 +0000

    another puzzle passed

---
+++ Functional-Programming/Introduction/lisp/sum-of-odd-elements/solution.lisp
@@ -0,0 +1,38 @@
+(defun fn (l &optional (acc 0))
+  (if (null l)
+      acc
+      (fn (cdr l) (if (oddp (car l))
+                      (+ acc (car l))
+                      acc))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((l (loop for rl = (read-line stream nil nil)
+              while rl
+              collect (parse-integer rl))))
+    ;; (format t "read ~A" l)
+    (princ (fn l))))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
