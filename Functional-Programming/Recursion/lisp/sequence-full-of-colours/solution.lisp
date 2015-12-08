f8f194fdf580c214421bcdbbfb4a4d58f3f19601
Jacek Podkanski <ruby.object@googlemail.com>
2015-12-08 02:33:14 +0000

    prepared new puzzle

---
+++ Functional-Programming/Recursion/lisp/sequence-full-of-colours/solution.lisp
@@ -0,0 +1,35 @@
+(defun solve-me (l)
+  l)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((tc (parse-integer (read-line stream))))
+    (loop for c from 1 to tc do
+         (princ (solve-me (loop for c
+                             across (read-line stream)
+                             collect c)))
+         (terpri))))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
