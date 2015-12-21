3ac12dbe7bae492885fb10660c75776dc4fac805
Jacek Podkanski <ruby.object@googlemail.com>
2015-12-21 16:53:25 +0000

    still searching

--- Functional-Programming/Introduction/lisp/area-under-curve-and-volume-of-revolving-curve/solution.lisp
+++ Functional-Programming/Introduction/lisp/area-under-curve-and-volume-of-revolving-curve/solution.lisp
@@ -1,3 +1,28 @@
+(defparameter *small-dx* 0.001)
+
+(defun integrate(f a b)
+  "integrate a function f from a to b"
+
+  (defun integrate-gen(f x b dx sum)
+    "the generalized inner function for tail-recursion.
+       integrate a function f from x to b"
+    (if (> x b)
+        sum
+        (integrate-gen f (+ x dx) b dx (+ sum (* (funcall f x) dx)))))
+
+  (integrate-gen f a b *small-dx* 0))
+
+(let
+    ((f1 (lambda (x) 1))
+     (f2 (lambda (x) (expt x 2)))
+     (f3 (lambda (x) (* x (exp (expt x 2))))))
+  (print (integrate f1 0 5))
+  (print (integrate f3 0 1)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
 ;; (solve-me '(1 2 3 4 5) '(6 7 8 9 10) '(1 4))
 (defun solve-me (as bs lr)
   (if (not as)
