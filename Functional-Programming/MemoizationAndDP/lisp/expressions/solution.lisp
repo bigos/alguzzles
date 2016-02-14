3e507ef0181eb169b9ded8639f84cdb20fb64b98
Jacek Podkanski <ruby.object@googlemail.com>
2016-02-14 23:09:19 +0000

    prepared new puzzle

---
+++ Functional-Programming/MemoizationAndDP/lisp/expressions/solution.lisp
@@ -0,0 +1,32 @@
+(defun solve-me (tc l)
+  (format t "args: ~A ~A~%" tc l))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((tc (parse-integer (read-line stream)))
+        (l (split-and-parse (read-line stream))))
+    (solve-me tc l)))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
