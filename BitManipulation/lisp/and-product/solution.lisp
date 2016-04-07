57208926e54984b17a5a4caffff70ef3747aacab
Jacek Podkanski <ruby.object@googlemail.com>
2016-04-07 14:48:31 +0100

    puzzle skeleton

---
+++ BitManipulation/lisp/and-product/solution.lisp
@@ -0,0 +1,35 @@
+(proclaim '(optimize (speed 3)))
+
+(defun solve-me (n)
+  (format t "solving ~A~%" n))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((tc (parse-integer (read-line stream))))
+    (dotimes (x tc)
+      (solve-me (split-and-parse (read-line stream))))))
+
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
