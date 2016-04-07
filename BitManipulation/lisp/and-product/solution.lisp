46fc6d152257c8c58466b446cdb8728e79c37c8f
Jacek Podkanski <ruby.object@googlemail.com>
2016-04-07 18:44:39 +0100

    still no luck, failing test case added

--- BitManipulation/lisp/and-product/solution.lisp
+++ BitManipulation/lisp/and-product/solution.lisp
@@ -64,7 +64,7 @@
                       :directory
                       (pathname-directory
                        (parse-namestring *load-pathname*))
-                      :name "input0" :type "txt"))
+                      :name "input10" :type "txt"))
     (solution s)))
 
 (main)
