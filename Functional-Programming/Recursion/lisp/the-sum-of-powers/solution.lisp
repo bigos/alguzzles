e2617635b22ef895d21b5173e57388862a24c9fb
Jacek Podkanski <ruby.object@googlemail.com>
2016-04-11 23:32:56 +0100

    prepared new puzzle

---
+++ Functional-Programming/Recursion/lisp/the-sum-of-powers/solution.lisp
@@ -0,0 +1,35 @@
+(defun sums-of-powers (x b)
+  ;; for now just return 1
+  1)
+
+(defun solve-me (x n)
+  (print (sums-of-powers x n)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (solve-me (parse-integer (read-line stream))
+            (parse-integer (read-line stream))))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
