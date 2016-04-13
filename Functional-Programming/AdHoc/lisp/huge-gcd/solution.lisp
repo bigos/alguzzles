388c1382e6bf962a8aca8a43e63dd3ffe8bd8347
Jacek Podkanski <ruby.object@googlemail.com>
2016-04-13 01:47:00 +0100

    lots of timeouts

--- Functional-Programming/AdHoc/lisp/huge-gcd/solution.lisp
+++ Functional-Programming/AdHoc/lisp/huge-gcd/solution.lisp
@@ -1,5 +1,39 @@
+(defun small-divisors (n fls)
+  (loop for d from 1 to fls
+     when (zerop (mod n d))
+     collect d))
+
+(defun divisors (n)
+  (let* ((nsqrt (sqrt n))
+         (small-divisors (small-divisors n (floor nsqrt)))
+         (reversed-divisors))
+    ;; do not include sqrt twice
+    (setf reversed-divisors             ;we do the assignment only once, so it's not cheating
+          (if (eq (floor nsqrt)
+                  (ceiling nsqrt))
+              (cdr (reverse small-divisors))
+              (reverse small-divisors)))
+    (if (= n 1)
+        '(1)
+        (concatenate 'list
+                     small-divisors
+                     (map 'list (lambda (x) (/ n x)) reversed-divisors)))))
+
+(defun find-gcd (da db)
+  (let ((db-hash (make-hash-table))
+        (dad (divisors da)))
+    (loop for d in (divisors db) do
+         (setf (gethash d db-hash) d))
+    (loop for d in dad
+       for result = 1 then (if (eq d (gethash d db-hash))
+                               d
+                               result)
+       finally (return result))))
+
 (defun solve-me (n na m mb)
-  (format t "solve ~A ~A   ~A ~A~%" n na m mb))
+  (declare (ignore n m))
+  (find-gcd (apply '* na)
+            (apply '* mb)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun split-by-one-space (string)
@@ -19,7 +53,7 @@
         (na (split-and-parse (read-line stream)))
         (m (parse-integer (read-line stream)))
         (mb (split-and-parse (read-line stream))))
-    (solve-me n na m mb)))
+    (format t "~a" (solve-me n na m mb))))
 
 ;; (solution) ; uncomment this when running on hacker-rank
 
