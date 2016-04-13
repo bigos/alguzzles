4218a37de3c738b8ea1ca6fcca00dd6aa6a2f649
Jacek Podkanski <ruby.object@googlemail.com>
2016-04-13 02:49:38 +0100

    slooow

--- Functional-Programming/AdHoc/lisp/huge-gcd/solution.lisp
+++ Functional-Programming/AdHoc/lisp/huge-gcd/solution.lisp
@@ -1,11 +1,24 @@
-(defun small-divisors (n fls)
-  (loop for d from 1 to fls
-     when (zerop (mod n d))
-     collect d))
+(defun factor (n &optional (acc '()))
+  (when (> n 1) (loop with max-d = (isqrt n)
+                   for d = 2 then (if (evenp d) (1+ d) (+ d 2)) do
+                     (cond ((> d max-d) (return (cons (list n 1) acc)))
+                           ((zerop (rem n d))
+                            (return (factor (truncate n d) (if (eq d (caar acc))
+                                                               (cons
+                                                                (list (caar acc) (1+ (cadar acc)))
+                                                                (cdr acc))
+                                                               (cons (list d 1) acc)))))))))
+
+(defun get-factors (factor-list)
+  (let ((ht (make-hash-table)))
+    (loop for e in factor-list do
+         (setf (gethash (car e) ht) (car e))
+         (setf (gethash (cadr e) ht) (cadr e)))
+    ht))
 
 (defun divisors (n)
   (let* ((nsqrt (sqrt n))
-         (small-divisors (small-divisors n (floor nsqrt)))
+         (small-divisors (factor n))
          (reversed-divisors))
     ;; do not include sqrt twice
     (setf reversed-divisors             ;we do the assignment only once, so it's not cheating
@@ -19,7 +32,22 @@
                      small-divisors
                      (map 'list (lambda (x) (/ n x)) reversed-divisors)))))
 
+;;; a bit faster than divisors, but still not good enough
+(defun factors (n &aux (lows '()) (highs '()))
+  (do ((limit (1+ (isqrt n))) (factor 1 (1+ factor)))
+      ((= factor limit)
+       (when (= n (* limit limit))
+         (push limit highs))
+       (remove-duplicates (nreconc lows highs)))
+    (multiple-value-bind (quotient remainder) (floor n factor)
+      (when (zerop remainder)
+        (push factor lows)
+        (push quotient highs)))))
+
+;;; timeout on args 154742504910672534362390528 150094635296999121
+;;; first big number chokes small-divisors
 (defun find-gcd (da db)
+  (format t "~A ~A~%" da db )
   (let ((db-hash (make-hash-table))
         (dad (divisors da)))
     (loop for d in (divisors db) do
@@ -62,7 +90,7 @@
                       :directory
                       (pathname-directory
                        (parse-namestring *load-pathname*))
-                      :name "input0" :type "txt"))
+                      :name "input1" :type "txt"))
     (solution s)))
 
 (main)
