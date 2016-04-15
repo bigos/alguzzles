b11e8f8b70dae318ab2fcaf929a217c1e878d7ca
Jacek Podkanski <ruby.object@googlemail.com>
2016-04-15 01:38:21 +0100

    note

--- Functional-Programming/AdHoc/lisp/huge-gcd/solution.lisp
+++ Functional-Programming/AdHoc/lisp/huge-gcd/solution.lisp
@@ -1,24 +1,56 @@
 (defun factor (n &optional (acc '()))
-  (when (> n 1) (loop with max-d = (isqrt n)
-                   for d = 2 then (if (evenp d) (1+ d) (+ d 2)) do
-                     (cond ((> d max-d) (return (cons (list n 1) acc)))
-                           ((zerop (rem n d))
-                            (return (factor (truncate n d) (if (eq d (caar acc))
-                                                               (cons
-                                                                (list (caar acc) (1+ (cadar acc)))
-                                                                (cdr acc))
-                                                               (cons (list d 1) acc)))))))))
+  (when (> n 1)
+    (loop with max-d = (isqrt n)
+       for d = 2 then (if (evenp d) (1+ d) (+ d 2)) do
+         (cond ((> d max-d) (return (cons (list n 1) acc)))
+               ((zerop (rem n d))
+                (return (factor (truncate n d) (if (eq d (caar acc))
+                                                   (cons
+                                                    (list (caar acc) (1+ (cadar acc)))
+                                                    (cdr acc))
+                                                   (cons (list d 1) acc)))))))))
 
-(defun get-factors (factor-list)
-  (let ((ht (make-hash-table)))
-    (loop for e in factor-list do
-         (setf (gethash (car e) ht) (car e))
-         (setf (gethash (cadr e) ht) (cadr e)))
-    ht))
+(defun prime-factors (n)
+  (remove-duplicates
+   (sort
+    (loop for f in (factor n)
+       collect (car f) collect (cadr f))
+    '<)))
+
+;; (defun find-divisor (n prime-factors sqr)
+;;   (let ((acc))
+;;     (loop for a in prime-factors do
+;;          (loop for b in prime-factors do
+;;               (when (and (< (* a b) sqr)
+;;                          (zerop (rem n (* a b))))
+;;                 (push (* a b) acc))
+;;             until (> b sqr)
+;;               )
+;;        until (> a sqr))
+;;     acc))
+
+(defun find-divisor (n x sqr acc)
+  (loop for p in acc ))
+;;; i need to figure out how to do it using recursive tree
+(defun small-divisors (n)
+  (remove-duplicates (sort (find-divisor n 1 (isqrt n) '(prime-factors n))
+                           '<)))
+
+;; (factor 9114)
+;; ((31 1) (7 2) (3 1) (2 1))
+;; prime factors to small divisors
+;; (list (* 1 1) (* 2 1) (* 3 1) (* 3 2) (* 7 1) (* 7 2) (* 7 3)
+;; (* 31 1) (* 7 3 2) (* 7 7) (* 31 2) (* 31 3) )
+;; (1 2 3 6 7 14 21 31 42 49 62 93)
+(defun small-divisors-old (n)
+  "still too slow"
+  (loop for d from 1 to (isqrt n)
+     when (zerop (mod n d))
+     collect d))
 
 (defun divisors (n)
   (let* ((nsqrt (sqrt n))
-         (small-divisors (factor n))
+         (small-divisors (small-divisors n))
          (reversed-divisors))
     ;; do not include sqrt twice
     (setf reversed-divisors             ;we do the assignment only once, so it's not cheating
@@ -32,22 +64,9 @@
                      small-divisors
                      (map 'list (lambda (x) (/ n x)) reversed-divisors)))))
 
-;;; a bit faster than divisors, but still not good enough
-(defun factors (n &aux (lows '()) (highs '()))
-  (do ((limit (1+ (isqrt n))) (factor 1 (1+ factor)))
-      ((= factor limit)
-       (when (= n (* limit limit))
-         (push limit highs))
-       (remove-duplicates (nreconc lows highs)))
-    (multiple-value-bind (quotient remainder) (floor n factor)
-      (when (zerop remainder)
-        (push factor lows)
-        (push quotient highs)))))
-
 ;;; timeout on args 154742504910672534362390528 150094635296999121
-;;; first big number chokes small-divisors
 (defun find-gcd (da db)
-  (format t "~A ~A~%" da db )
+  (format t "divisors of ~A ~A~%" da db)
   (let ((db-hash (make-hash-table))
         (dad (divisors da)))
     (loop for d in (divisors db) do
@@ -90,7 +109,7 @@
                       :directory
                       (pathname-directory
                        (parse-namestring *load-pathname*))
-                      :name "input1" :type "txt"))
+                      :name "input0" :type "txt"))
     (solution s)))
 
 (main)
