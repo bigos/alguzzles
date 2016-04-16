fb0350fe6102db1260cbbeffe8a10a134f241afa
Jacek Podkanski <ruby.object@googlemail.com>
2016-04-16 06:44:08 +0100

    prepared new puzzle

---
+++ Functional-Programming/AdHoc/lisp/subset-sum/solution.lisp
@@ -0,0 +1,36 @@
+(defun solve-me (a s)
+  -1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((size-of-a (parse-integer (read-line stream)))
+        (a (split-and-parse (read-line stream)))
+        (tc (parse-integer (read-line stream)))
+        (s))
+    (loop for x from 1 to tc do
+         (setf s (parse-integer (read-line stream)))
+         (format t "~a" (solve-me a s)))))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
