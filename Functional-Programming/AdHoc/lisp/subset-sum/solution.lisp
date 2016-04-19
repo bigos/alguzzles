7154c9992d40d1e18145b98ce91036943509c21d
Jacek Podkanski <ruby.object@googlemail.com>
2016-04-19 21:55:30 +0100

    duh

--- Functional-Programming/AdHoc/lisp/subset-sum/solution.lisp
+++ Functional-Programming/AdHoc/lisp/subset-sum/solution.lisp
@@ -1,39 +1,6 @@
 (declaim (optimize (space 0) (safety 0) (speed 3)))
 
-(defun next-combination (n a)
-  (let ((k (length a)) m)
-    (loop for i from 1 do
-         (when (> i k) (return nil))
-         (when (< (aref a (- k i)) (- n i))
-           (setf m (aref a (- k i)))
-           (loop for j from i downto 1 do
-                (incf m)
-                (setf (aref a (- k j)) m))
-           (return t)))))
-
-(defun all-combinations (n k)
-  (if (or (< k 0) (< n k)) '()
-      (let ((a (make-array k)))
-        (loop for i below k do (setf (aref a i) i))
-        (loop collect (coerce a 'list) while (next-combination n a)))))
-
-(defun map-combinations (n k fun)
-  (if (and (>= k 0) (>= n k))
-      (let ((a (make-array k)))
-        (loop for i below k do (setf (aref a i) i))
-        (loop do (funcall fun (coerce a 'list)) while (next-combination n a)))))
-
-(defun comb (m list)
-  (let ((result))
-    (labels ((comb1 (l c m)
-               (when (>= (length l) m)
-                 (when (zerop m)
-                   (return-from comb1 (push c result)))
-                 (comb1 (cdr l) c m)
-                 (comb1 (cdr l) (cons (first l) c) (1- m)))))
-      (comb1 list nil m))
-    result))
-
+;;; we do not need to use combinations, if we sort the numbers we can figure out minimal set for required number, duh
 (defun solve-me (a s)
   (loop for x from 1 upto (length a)
      for y = (every 'null (map 'list (lambda (x) (>= (apply '+ x) s) ) (comb x a)))
