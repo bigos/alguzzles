8d7e742a9da882253b7db689fbcbf56beb2c5cb8
Jacek Podkanski <ruby.object@googlemail.com>
2016-04-20 00:44:41 +0100

    progress

--- Functional-Programming/AdHoc/lisp/subset-sum/solution.lisp
+++ Functional-Programming/AdHoc/lisp/subset-sum/solution.lisp
@@ -1,13 +1,25 @@
-;;; (declaim (optimize (space 0) (safety 0) (speed 3)))
+(declaim (optimize (space 0) (safety 0) (speed 3)))
 
-(defun solve-me (a s)
-  (loop for x in a
-     for y = x then (+ x y)
-     for z = 1 then (1+ z)
-     until (>= y s)
-     finally (return (if (>= y s)
-                         z
-                         -1))))
+(defun binary-search-val (low high added s)
+  (let ((pointer (floor (/ (+ low high) 2))))
+    ;; (format t "~A ~A ~A    ~A   ~A~%" low pointer high (aref added pointer) s)
+    (cond ((and (zerop pointer)
+                (>= (aref added pointer) s))
+           (progn ;; (format t "~&cond1~%")
+                  pointer))
+          ((and (>= (aref added pointer) s)
+                (<  (aref added (1- pointer)) s))
+           (progn ;; (format t "~&cond2~%")
+                  pointer))
+
+          (T (if (< (aref added pointer) s)
+               (binary-search-val pointer high added s)
+               (binary-search-val low pointer added s))))))
+
+(defun solve-me (size-of-a added s)
+  (if (< (aref added (1- size-of-a)) s)
+      -1
+      (1+ (binary-search-val 0 (1- size-of-a) added s ))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun split-by-one-space (string)
@@ -23,14 +35,17 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun solution (&optional stream)
-  (let ((size-of-a (parse-integer (read-line stream)))
-        (a (sort  (split-and-parse (read-line stream)) '>))
-        (tc (parse-integer (read-line stream)))
-        (s))
-    (declare (ignore size-of-a))
+  (let* ((size-of-a (parse-integer (read-line stream)))
+         (a (sort  (split-and-parse (read-line stream)) '>))
+         (tc (parse-integer (read-line stream)))
+         (s)
+         (added (make-array (list size-of-a) :initial-contents (loop for x in a
+                                                                  for y = x then (+ x y)
+                                                                  collect y))))
     (loop for x from 1 to tc do
          (setf s (parse-integer (read-line stream)))
-         (format t "~a~%" (solve-me a s)))))
+         ;; (format t "~&~A ~A~%" added s)
+         (format t "~A~%" (solve-me size-of-a added s)))))
 
 ;; (solution) ; uncomment this when running on hacker-rank
 
@@ -39,7 +54,7 @@
                       :directory
                       (pathname-directory
                        (parse-namestring *load-pathname*))
-                      :name "input0" :type "txt"))
+                      :name "input09" :type "txt"))
     (solution s)))
 
 (main)
