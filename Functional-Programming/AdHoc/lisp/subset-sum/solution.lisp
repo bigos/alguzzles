11dc0ca4cec94e1e7979ea02379d03f4496eb48c
Jacek Podkanski <ruby.object@googlemail.com>
2016-04-19 14:43:46 +0100

    some progress

--- Functional-Programming/AdHoc/lisp/subset-sum/solution.lisp
+++ Functional-Programming/AdHoc/lisp/subset-sum/solution.lisp
@@ -1,15 +1,29 @@
-(defun comb (m list)
+(defun comb (m list s)
   (let ((result))
-    (labels ((comb1 (l c m)
+    (labels ((summing (l)
+               (loop for x in l
+                  when (listp x) sum (car x)
+                  unless (listp x) sum x))
+             (comb1 (l c m)
                (when (>= (length l) m)
-                 (if (zerop m) (return-from comb1 (push c result)))
+                 (when (zerop m)
+
+                   ;; higher order function should go here
+                   (if (>=  (summing result) s)
+                       (return-from comb (length result)))
+                   (return-from comb1 (push c result))
+
+                   )
                  (comb1 (cdr l) c m)
                  (comb1 (cdr l) (cons (first l) c) (1- m)))))
       (comb1 list nil m))
-    result))
+    -1))
 
 (defun solve-me (a s)
-  -1)
+  (loop for x from 1 to (length a)
+     with y = (comb x a s)
+     until (> y -1)
+       finally (return y)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun split-by-one-space (string)
