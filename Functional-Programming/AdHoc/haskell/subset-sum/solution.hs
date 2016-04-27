c2662666bc56a65e7149353b497c6d482fc9764c
Jacek Podkanski <ruby.object@googlemail.com>
2016-04-27 15:17:16 +0100

    success in reading various types of data

--- Functional-Programming/AdHoc/haskell/subset-sum/solution.hs
+++ Functional-Programming/AdHoc/haskell/subset-sum/solution.hs
@@ -1,14 +1,14 @@
 -- solve :: Int -> [Int] -> Int -> (Int,[Int],Int)
 -- solve n a s = (n, a, s)
-
-
-
+getData :: IO (Int, [Int], Int, Int)
 getData = do
   n <- getLine
   a <- getLine
   tc <- getLine
   as <- getLine
-  return (n, a, tc, as)
+  return (read n,
+          map (\x -> read x :: Int) (words a), -- read space separated string as array of ints
+          read tc, read as)
 
 main :: IO ()
 main = do
