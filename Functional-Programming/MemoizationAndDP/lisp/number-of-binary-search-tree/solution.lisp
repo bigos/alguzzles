d3ae48068316b2316349bc7c793021f4ad866dfb
Jacek Podkanski <ruby.object@googlemail.com>
2016-05-07 16:23:35 +0100

    new puzzle

---
+++ Functional-Programming/MemoizationAndDP/lisp/number-of-binary-search-tree/solution.lisp
@@ -0,0 +1,35 @@
+;;;
+
+(defun solve-me (s)
+  s)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((tc (parse-integer (read-line stream))))
+    (loop for x from 1 to tc do
+         (format t "~a~%" (solve-me
+                           (parse-integer (read-line stream)))))))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
