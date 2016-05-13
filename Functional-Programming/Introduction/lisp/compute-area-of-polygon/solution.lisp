4247d3ea92d18267164bb26df0d7ae0285033f3a
Jacek Podkanski <ruby.object@googlemail.com>
2016-05-13 01:14:57 +0100

    more notes

--- Functional-Programming/Introduction/lisp/compute-area-of-polygon/solution.lisp
+++ Functional-Programming/Introduction/lisp/compute-area-of-polygon/solution.lisp
@@ -1,27 +1,36 @@
+;; http://code.activestate.com/recipes/578047-area-of-polygon-using-shoelace-formula/
+
 (defun points (l first-point last-seen acc)
-  (format t "~&~A : ~A ~A : ~A~%" l first-point last-seen acc)
+  ;; (format t "~&~A : ~A ~A : ~A~%" l first-point last-seen acc)
   (if (null l)
-      acc
+      (apply #'+ acc)
       (points (cdr l)
               (if first-point
                   first-point
                   (car l))
-              (car l)                   ;; last seen
-              (if last-seen             ;; acc
+              (car l)       ;; last seen
+              (if last-seen ;; acc
                   (cons (triangle-area (car l) last-seen first-point)
                         acc)
-                  '())
-              )))
+                  '()))))
+
 ;;; now i need area of triangle from the coordinates
 (defun triangle-area (a b c)
-  (list a b c))
+  ;; (format t "~&a ~A b ~A c ~A~%" a b c)
+  (abs (/ (+ (* (car a)
+                (- (cadr b) (cadr c)))
+             (* (car b)
+                (- (cadr c) (cadr a)))
+             (* (car c)
+                (- (cadr a) (cadr b))))
+          2.0)))
 
 ;;; use this to have starting point for the first triangle
-(defun triangle-points (l first-point last-seen acc)
+(defun triangle-points (l acc)
   (points (cddr l) (car l) (nth 1 l) acc))
 
 (defun solve-me (l)
-  (triangle-points l nil nil nil))
+  (triangle-points l nil))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun split-by-one-space (string)
@@ -49,7 +58,7 @@
                       :directory
                       (pathname-directory
                        (parse-namestring *load-pathname*))
-                      :name "input0" :type "txt"))
+                      :name "input1" :type "txt"))
     (solution s)))
 
 (main)
