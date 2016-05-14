035a659c2a0ae548dae3d7595c8b3e88b17bf278
Jacek Podkanski <ruby.object@googlemail.com>
2016-05-14 04:31:16 +0100

    stuck

--- Functional-Programming/Introduction/lisp/concave-polygon/solution.lisp
+++ Functional-Programming/Introduction/lisp/concave-polygon/solution.lisp
@@ -1,12 +1,46 @@
+;;; concave
+(defparameter zzz (solve-me '((-1 1) (0 2) (1 3) (3 3) (3 2) (3 -1) (2 -1) (1 -1) (1 1))))
+;;; convex
+(defparameter aaa (solve-me '((-1 1) (0 2) (1 3) (3 3) (3 2) (3 -1) (2 -1) (1 -1) (-1 0))))
+
+;;; (loop for x in zzz do (format t "~A ~A ~A~%" x (my-test x) (my-vectors x)))
+
 (defun substract-vectors (a b)
   (list (- (car a) (car b))
         (- (cadr a) (cadr b))))
 
 (defun my-vectors (points)
-  (list (substract-vectors (elt points 1)
-                           (elt points 0))
-        (substract-vectors (elt points 2)
-                           (elt points 0))))
+  (list
+   (substract-vectors (elt points 1)
+                      (elt points 0))
+   (substract-vectors (elt points 2)
+                      (elt points 0))))
+(defun rtd (r)
+  (* r (/ 180.0 pi)))
+
+(defun vector-dot (a b)
+  (+ (* (car a) (car b))
+     (* (cadr a) (cadr b))))
+
+(defun vector-dziel (a b)
+  (* (sqrt (+ (expt (car a) 2)
+              (expt (cadr a) 2)))
+     (sqrt (+ (expt (car b) 2)
+              (expt (cadr b) 2)))))
+
+;;; i have problem getting expected vector angle in degrees
+;; radians to degrees seems to work
+;; the formula seems to work
+;; but the degree angles are wrong, why?
+
+(defun my-test (points)
+  (let* ((res (my-vectors points))
+         (v1 (car res))
+         (v2 (cadr res))
+         (dot (vector-dot v1 v2))
+         (dziel (vector-dziel v1 v2)))
+         (format t "~A ~A ~A ~A ~A~%" v1 v2 dot dziel (/ dot dziel))
+    (list 'aaaaa dot  dziel (rtd (cos (/ dot dziel))) 'zzzz)))
 
 (defun points (l last-point before-last-point acc two-last-points)
   (if (null l)
