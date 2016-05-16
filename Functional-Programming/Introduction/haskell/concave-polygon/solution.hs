92f081ebf50bd4d1fe04bdb8a83221374e261b1e
Jacek Podkanski <ruby.object@googlemail.com>
2016-05-16 14:08:49 +0100

    read lines a list of integers

--- Functional-Programming/Introduction/haskell/concave-polygon/solution.hs
+++ Functional-Programming/Introduction/haskell/concave-polygon/solution.hs
@@ -3,9 +3,11 @@ import Data.Array
 import Data.Traversable
 import Control.Monad
 
+str2ints :: String -> [Int]
+str2ints s = map (\x -> read x) (words s)
 
 main :: IO()
 main = do
   nlines <- getLine
   inputs <- replicateM (read  nlines :: Int) getLine
-  print inputs
+  print (map (\x -> str2ints x) inputs)
