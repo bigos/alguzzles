6e1ca2a675908d52efdfb4b78fa3d8d9a3ac3a5f
Jacek Podkanski <ruby.object@googlemail.com>
2016-05-16 15:26:49 +0100

    getting the needed data in haskell

--- Functional-Programming/Introduction/haskell/concave-polygon/solution.hs
+++ Functional-Programming/Introduction/haskell/concave-polygon/solution.hs
@@ -6,10 +6,28 @@ import Control.Monad
 str2ints :: String -> [Int]
 str2ints s = map (\x -> read x) (words s)
 
-doit dd = print (map (\x -> (x!!0, x!!1)) dd)
+tuplify :: [String] -> [(Int, Int)]
+tuplify zz = map (\x -> (x !! 0, x !! 1)) (map str2ints zz)
+
+findminx :: [(Int, Int)] -> Int
+findminx a = minimum $ map fst a
+
+findminy :: [(Int, Int)] -> Int
+findminy a = minimum $ map snd a
+
+findmaxx :: [(Int, Int)] -> Int
+findmaxx a = maximum $ map fst a
+
+findmaxy :: [(Int, Int)] -> Int
+findmaxy a = maximum $ map snd a
+
+doit :: [(Int, Int)] -> IO()
+doit ii = do
+  print (findminx ii, findminy ii, findmaxx ii, findmaxy ii ,"\n",ii)
+
 
 main :: IO()
 main = do
   nlines <- getLine
   inputs <- replicateM (read  nlines :: Int) getLine
-  doit  (map (\x -> str2ints x) inputs)
+  doit (tuplify inputs)
