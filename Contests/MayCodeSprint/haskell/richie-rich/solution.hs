5dc609e18fe6a3d9ac948141c7b35782de96f6ab
Jacek Podkanski <ruby.object@googlemail.com>
2016-05-22 01:32:08 +0100

    time for rest

--- Contests/MayCodeSprint/haskell/richie-rich/solution.hs
+++ Contests/MayCodeSprint/haskell/richie-rich/solution.hs
@@ -4,6 +4,19 @@ import Data.Function
 import Control.Monad
 import Text.Printf
 
+-- finish another time
+
+-- calculate levenshtein distance between two strings
+levenshtein::[Char] -> [Char] -> Int
+levenshtein "" "" = 0
+levenshtein "" s2 = length s2
+levenshtein s1 "" = length s1
+levenshtein s1 s2
+   | last s1 == last s2 = levenshtein (init s1) (init s2)
+   | otherwise = minimum [1 + levenshtein (init s1) s2,
+                          1 + levenshtein s1 (init s2),
+                          1 + levenshtein (init s1) (init s2)]
+
 solve [] [] asc bsc = (asc, bsc)
 solve (a:as) (b:bs) asc bsc
   | a > b = solve as bs (asc + 1) bsc
