6c95e5e759afdae83c6951b52092940cbe48e619
Jacek Podkanski <ruby.object@googlemail.com>
2016-05-22 15:10:05 +0100

    preparing Haskell input

--- Contests/MayCodeSprint/haskell/absolute-permutation/solution.hs
+++ Contests/MayCodeSprint/haskell/absolute-permutation/solution.hs
@@ -11,10 +11,21 @@ strf :: Int -> Int -> String
 strf a b = printf "%d %d" a b
 
 
+--solveMe :: Int -> Int -> [Int]
+solveMe a b = [b,a]
+
+--solve :: [Int] -> [Int]
+solve x = solveMe a b
+  where a = x !! 0
+        b = x !! 1
 
 main :: IO()
 main = do
   tc <- getLine
   inputs <- replicateM (read  tc) getLine
   let zzz = map (\ x -> splitAndRead x) inputs
-  print zzz
+  -- print zzz
+  let rrr = map (\ x -> solve x) zzz
+  print rrr
+
+sampledata = [[2,1],[3,0],[3,2]] -- zzz
