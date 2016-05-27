32ea394212a0af3bcf0fc32688e04aa813d90637
Jacek Podkanski <ruby.object@googlemail.com>
2016-05-27 15:13:13 +0100

    progress

--- Functional-Programming/FunctionalStructures/fighting-armies/lisp/solution.lisp
+++ Functional-Programming/FunctionalStructures/fighting-armies/lisp/solution.lisp
@@ -1,3 +1,33 @@
+(defun insertion-sort (list &optional (predicate #'>))
+  "Return a sorted copy of list."
+  (labels ((insert (item list)
+             (cond ((null list)
+                    (list item))
+                   ((funcall predicate item (car list))
+                    (cons item list))
+                   (t
+                    (cons (car list) (insert item (cdr list)))))))
+    (when list
+      (insert (car list) (insertion-sort (cdr list) predicate)))))
+
+(defun rec-merge-sort (i j &optional (acc nil))
+  (cond ((and (null i)
+              (null j))
+         (reverse acc))
+        ((and (null i)
+              j)
+         (rec-merge-sort i (cdr j) (cons (car j) acc)))
+        ((and i
+              (null j))
+         (rec-merge-sort (cdr i) j (cons (car i) acc)))
+        ((>= (car i)
+             (car j))
+            (rec-merge-sort (cdr i) j (cons (car i) acc)))
+        ((< (car i)
+            (car j))
+           (rec-merge-sort i (cdr j) (cons (car j) acc)))
+        (T (cerror "missed" "case missing"))))
+
 (defun command-nums ()
   (list nil find-strongest strongest-died recruit merge))
 
@@ -8,10 +38,19 @@
   (setf (aref *armies* i) (cdr (aref *armies* i))))
 
 (defun recruit (i c)
-  (setf (aref *armies* i) c))
+  (setf (aref *armies* i)
+        (insertion-sort (cons c (aref *armies* i)))))
 
 ;;; merge armies keeping correct sorting
-(defun merge (i j))
+(defun merge (i j)
+  ;; recursive merge i and j onto i
+  (setf (aref *armies* i)
+        (rec-merge-sort (aref *armies* i)
+                              (aref *armies* j)))
+  ;; make j to be nil
+  (setf (aref *armies* j) nil))
+
+
 
 (defun process-event (l)
   (format t "processing ~A ~A~%" *n* l))
