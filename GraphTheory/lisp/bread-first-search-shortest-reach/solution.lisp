c78881bd3113f52f1f49e31bc50911dd28753616
Jacek Podkanski <ruby.object@googlemail.com>
2016-06-04 16:24:23 +0100

    new puzzle

---
+++ GraphTheory/lisp/bread-first-search-shortest-reach/solution.lisp
@@ -0,0 +1,44 @@
+(defun solve-me (n m edges s)
+  (format t "args are ~A ~A ~A ~A~%" n m edges s))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((tc (parse-integer (read-line stream)))
+        (nm)
+        (n)
+        (m)
+        (edges)
+        (s))
+    (dotimes (_a tc)
+      (setf nm (split-and-parse (read-line stream)))
+      (setf n (car nm)
+            m (cadr nm))
+      (dotimes (_b m)
+        (push (split-and-parse (read-line stream)) edges))
+      (setf s (parse-integer (read-line stream)))
+      (solve-me n m edges s)
+      )))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
