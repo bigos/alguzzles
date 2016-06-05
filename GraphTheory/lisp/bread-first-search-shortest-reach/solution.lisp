dc195fac8a5d149fcb3037dd2c320e6156762ad8
Jacek Podkanski <ruby.object@googlemail.com>
2016-06-05 01:24:54 +0100

    making progress

--- GraphTheory/lisp/bread-first-search-shortest-reach/solution.lisp
+++ GraphTheory/lisp/bread-first-search-shortest-reach/solution.lisp
@@ -1,23 +1,50 @@
 (setf *print-circle* T)
-(defparameter *visited* nil)
 
 (defstruct node
   (id)
   (neighbours))
 
 (defun solve-me (n m edges s)
-  (let ((nodes (make-array (list (1+ n)))))
+  (let ((nodes (make-array (list (1+ n))))
+        (res)
+        (visited)
+        (found))
     (format t "args are ~A ~A ~A ~A~%" n m edges s)
-    ;; finish me
-    (loop for i from 1 to n do
-         (setf (aref nodes i) (make-node :id i)))
-    (loop for vert in edges do
-         (progn
-           (push (car vert) (node-neighbours (aref nodes (cadr vert))))
-           (push (cadr vert) (node-neighbours (aref nodes (car vert))))))
+    (labels ((bdf-shortest (s i)
+               (setf visited nil
+                     found nil)
+               (format t "~&======================= ~A ~A~%" s i)
+               (bdf-rec s i 0)
+               (unless found (format t ":-( :-( :-( )))not found ~A~%" i)))
+             (neighbours (i)
+               (node-neighbours (aref nodes i)))
+             (not-yet-visited-neighbours (i)
+               (remove-if (lambda (x) (some (lambda (y) (eq x y)) visited))
+                          (neighbours i)))
+             (bdf-rec (s e c)
+               (if (eq s e)
+                   (progn
+                     (setf found T)
+                     (format t "!!!!!!! target found ~A~%" c))
+                   (progn
+                     (push s visited)
+                     (format t "visited ~A  c ~A~%" visited c)
+                     (format t "not yet visited neighbours ~A~%" (not-yet-visited-neighbours s))
+                     (loop for nv in (not-yet-visited-neighbours s) do
+                          (unless found
+                            (bdf-rec nv e (1+ c)))
+                          )))))
 
-
-    ))
+      (loop for i from 1 to n do
+           (setf (aref nodes i) (make-node :id i)))
+      (loop for vert in edges do
+           (progn
+             (push (car vert)
+                   (node-neighbours (aref nodes (cadr vert))))
+             (push (cadr vert)
+                   (node-neighbours (aref nodes (car vert))))))
+      (setf res (loop for i from 1 to n
+                   unless (eq i s) collect (bdf-shortest s i))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun split-by-one-space (string)
@@ -40,14 +67,14 @@
         (edges)
         (s))
     (dotimes (_a tc)
+      (setf edges nil)
       (setf nm (split-and-parse (read-line stream)))
       (setf n (car nm)
             m (cadr nm))
       (dotimes (_b m)
         (push (split-and-parse (read-line stream)) edges))
       (setf s (parse-integer (read-line stream)))
-      (solve-me n m edges s)
-      )))
+      (solve-me n m edges s))))
 
 ;; (solution) ; uncomment this when running on hacker-rank
 
