f7fa36ad66349530f8230c0fa50f27b84829b1b9
Jacek Podkanski <ruby.object@googlemail.com>
2016-06-25 16:57:11 +0100

    new puzzle

---
+++ Strings/haskell/palindrome-index/solution.hs
@@ -0,0 +1,11 @@
+import Data.List
+import Data.Array
+
+
+main :: IO ()
+main = do
+  inputArgs <- getData
+  process inputArgs
+
+-- do notaion explained
+-- https://en.wikibooks.org/wiki/Haskell/do_notation
