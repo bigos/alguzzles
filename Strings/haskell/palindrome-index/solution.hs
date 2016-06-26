3de371cf0e0da99fadd83aac32ae0bf6a084a18f
Jacek Podkanski <ruby.object@googlemail.com>
2016-06-26 09:36:20 +0100

    read the data

--- Strings/haskell/palindrome-index/solution.hs
+++ Strings/haskell/palindrome-index/solution.hs
@@ -1,13 +1,12 @@
 import Data.List
 import Data.Array
-import System.IO
 
 main :: IO ()
 main = do
-  args <- getContents
-  -- inputArgs <- getData
-  --process inputArgs
-  print (lines args)
+  mydata <- getContents
+  let mylines = lines mydata
+  let tc = read (head mylines) ::Int
+  print (tc ,   (tail mylines ))
 
 -- do notaion explained
 -- https://en.wikibooks.org/wiki/Haskell/do_notation
