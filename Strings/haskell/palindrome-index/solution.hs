77ffca20c34d6366c9f1451ce641db2fffa0c093
Jacek Podkanski <ruby.object@googlemail.com>
2016-06-26 16:42:44 +0100

    wrong results

--- Strings/haskell/palindrome-index/solution.hs
+++ Strings/haskell/palindrome-index/solution.hs
@@ -1,19 +1,28 @@
 import Prelude
 import Data.Map (toList, fromListWith)
 
-palindromic input = length odds < 2
-  where
-    odds = filter odd (map snd (strFreq input))
+palindrome a = reverse a == a
 
-strFreq :: String -> [(Char, Int)]
-strFreq input = toList $ fromListWith (+) [(c, 1) | c <- input]
+stringRange s = [0 .. (length s - 1)]
+
+tryRemove x i = (take i x) ++ (drop (i+1) x)
+
+solveMe x l = head $ filter (\ y -> y > (-2)) indexes
+  where
+    indexes = map (\a -> if (palindrome (tryRemove x a)) then a else (-9)) l
+
+solve x = if palindrome x then (-1) else solveMe x (stringRange x)
+
+printRes :: Int -> IO()
+printRes x = do
+  print x
 
 main :: IO ()
 main = do
   mydata <- getContents
   let mylines = lines mydata
   let tc = read (head mylines) ::Int
-  print (tc ,   (tail mylines ))
+  mapM_ (\ x -> printRes (solve x)) (tail mylines )
 
 -- do notaion explained
 -- https://en.wikibooks.org/wiki/Haskell/do_notation
