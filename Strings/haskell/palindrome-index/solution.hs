3d1cb19c01e455a0e4aab558df495c61d59c9d79
Jacek Podkanski <ruby.object@googlemail.com>
2016-06-27 13:07:53 +0100

    dead end

--- Strings/haskell/palindrome-index/solution.hs
+++ Strings/haskell/palindrome-index/solution.hs
@@ -1,9 +1,21 @@
+import Debug.Trace
 import Prelude
-import Data.Map (toList, fromListWith)
 
+-- version with trace
+-- palindrome a = trace ("print a:  " ++ show a) $ reverse a == a
 palindrome a = reverse a == a
 
 stringRange s = [0 .. (length s - 1)]
+
+siftRemoved :: Int -> String -> String -> String -> Int
+siftRemoved i h _ []
+  | palindrome h = i
+  | otherwise = 0-1
+siftRemoved i h o r
+  | palindrome (h ++ r) = i
+  | otherwise = siftRemoved (i+1) (h ++ o) [head r] (tail r)
+
+siftMe s = siftRemoved 0 [] [] s
 
 tryRemove x i = (take i x) ++ (drop (i+1) x)
 
