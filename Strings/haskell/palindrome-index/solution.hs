f141c7e95a44323dc6ef59c9bc89477fd1522db0
Jacek Podkanski <ruby.object@googlemail.com>
2016-07-11 16:30:01 +0100

    experiment

--- Strings/haskell/palindrome-index/solution.hs
+++ Strings/haskell/palindrome-index/solution.hs
@@ -8,19 +8,21 @@ palindrome a = reverse a == a
 stringRange s = [0 .. (length s - 1)]
 
 
-                -- inefficient
+
 -- tryRemove x i = (take i x) ++ (drop (i+1) x)
 tryRemove x i = (fst d) ++ (drop 1 (snd d))
   where d = splitAt i x
 
-solveMe x l = if indexes == []
-  then (-1)
-  else head $ filter (\ y -> y > (-2)) indexes
+stringReversed :: String -> (String, String)
+stringReversed s = (s, reverse s)
 
-  where
-    indexes = map (\a -> if (palindrome (tryRemove x a)) then a else (-9)) l
+stringsSplit (s1, s2) x = (tryRemove s1 x, tryRemove s2 ((length s1) - x - 1))
 
-solve x = if palindrome x then (-1) else solveMe x (stringRange x)
+
+solveMe x l = map (\y -> sa y) l
+  where sa z = stringsSplit (stringReversed x) z
+
+--solve x = if palindrome x then (-1) else solveMe x (stringRange x)
 
 printRes :: Int -> IO()
 printRes x = do
@@ -31,7 +33,7 @@ main = do
   mydata <- getContents
   let mylines = lines mydata
   let tc = read (head mylines) ::Int
-  mapM_ (\ x -> printRes (solve x)) (tail mylines )
+  print tc
 
 -- do notaion explained
 -- https://en.wikibooks.org/wiki/Haskell/do_notation
