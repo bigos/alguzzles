cc450309530fa5b008feccc18888be752c659661
Jacek Podkanski <ruby.object@googlemail.com>
2016-07-13 23:24:19 +0100

    puzzle accepted

--- Implementation/lisp/minimum-distances/solution.lisp
+++ Implementation/lisp/minimum-distances/solution.lisp
@@ -1,5 +1,32 @@
-(defun solve (a)
-  (format t "going to solve ~A~%" a))
+(defparameter *result* -1)
+
+(defun zzz (r rv c cv)
+  (if (eq cv rv)
+      (progn
+        (let ((res (- c r)))
+          (if (> res 0)
+              (cond ((eq *result* -1)
+                     (setf *result* res))
+                    ((< res *result*)
+                     (setf *result* res))))
+          res))
+      0))
+
+(defun solve (sa a)
+  ;; (format t "going to solve ~A~%" a)
+  (loop
+     for r from 0 below sa
+     for rv = (nth r a) then (nth r a) do
+       ;; (loop for cx from 0 below r do (format t "     "))
+       (loop
+          for c from r below sa
+          for cv = (nth c a) then (nth c a) do
+            (zzz r rv c cv)
+            ;; (format t "~A ~A  " (nth c a) (zzz r rv c cv))
+            )
+       ;; (terpri)
+       )
+  (format t "~A~%" *result*))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun split-by-one-space (string)
@@ -17,7 +44,7 @@
 (defun solution (&optional stream)
   (let ((sa (parse-integer (read-line stream)))
         (a (split-and-parse (read-line stream))))
-    (solve a)))
+    (solve sa a)))
 
 ;; (solution) ; uncomment this when running on hacker-rank
 
