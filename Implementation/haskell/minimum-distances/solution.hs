d4c825ab0e536a11d07b2456ddf220d66a9c3747
Jacek Podkanski <ruby.object@googlemail.com>
2016-07-13 11:52:45 +0100

    created new puzzle

---
+++ Implementation/haskell/minimum-distances/solution.hs
@@ -0,0 +1,21 @@
+import Control.Applicative
+import Control.Monad
+import System.IO
+
+
+main :: IO ()
+main = do
+    n_temp <- getLine
+    let n = read n_temp :: Int
+    a_temp <- getLine
+    let a = map read $ words a_temp :: [Int]
+
+getMultipleLines :: Int -> IO [String]
+
+getMultipleLines n
+    | n <= 0 = return []
+    | otherwise = do
+        x <- getLine
+        xs <- getMultipleLines (n-1)
+        let ret = (x:xs)
+        return ret
