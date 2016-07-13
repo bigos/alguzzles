b6e0b00b9e3fceabffd0270ce140a3161e1b9fca
Jacek Podkanski <ruby.object@googlemail.com>
2016-07-13 14:07:53 +0100

    need to read about haskell arrays

--- Implementation/haskell/minimum-distances/solution.hs
+++ Implementation/haskell/minimum-distances/solution.hs
@@ -2,9 +2,12 @@ import Control.Applicative
 import Control.Monad
 import System.IO
 import Debug.Trace
+import Data.Array
 
 solve :: [Int] -> Int
 solve x = trace ("tracing " ++ show x) $  x!!1
+  where lx = length x
+        arr = array (lx, lx) [(a, b) | a <- x, b <- x]
 
 main :: IO ()
 main = do
