e2453dc2d108ccf2251b6bbeb030de38d40396c0
Jacek Podkanski <ruby.object@googlemail.com>
2016-07-15 19:27:44 +0100

    done

--- ArtificialIntelligence/probability-and-statistics-foundations/lisp/solution.lisp
+++ ArtificialIntelligence/probability-and-statistics-foundations/lisp/solution.lisp
@@ -1,12 +1,27 @@
 ;;; not applicable
 
 (defun my-random ()
-  (let* ((upto 5)
-        (limit 1)
-        (upper 100)
-        (random-result
-         (count-if (lambda (x) x)
-                   (map 'list (lambda (x) (>= x limit))
-                        (loop for x from 1 to upper collect (1+ (random upto)))))))
-    (* 1.0 (/ upper random-result ))
-    ))
+  (let ((from 1)
+         (upto 6)
+         (max-tries (- 240 6)))
+    (loop for x from 1 to max-tries
+       collect (random-range from upto))))
+
+(defun random-range (from to)
+  (let ((diff (- to from)))
+    (+ from (random (1+ diff)))))
+
+(defun possibilities (from to throws)
+  "Dice from 1 to 6 with 2 throws"
+  (expt (- (+ 1 to) from) throws))
+
+;;; table of sums leading to solution
+
+;; | x | 1 | 2 | 3 | 4 | 5 | 6 |
+;; |---+---+---+---+---+---+---|
+;; | 1 | 2 | 3 | 4 | 5 | 6 | 7 |
+;; | 2 | 3 | 4 | 5 | 6 | 7 | 8 |
+;; | 3 | 4 | 5 | 6 | 7 | 8 | 9 |
+;; | 4 | 5 | 6 | 7 | 8 | 9 | a |
+;; | 5 | 6 | 7 | 8 | 9 | a | b |
+;; | 6 | 7 | 8 | 9 | a | b | c |
