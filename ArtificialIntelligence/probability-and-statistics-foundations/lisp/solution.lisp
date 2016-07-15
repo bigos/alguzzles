e699dd88c516d9b1d3ce297d6f131f9c47253d1c
Jacek Podkanski <ruby.object@googlemail.com>
2016-07-15 17:48:19 +0100

    hmm

--- ArtificialIntelligence/probability-and-statistics-foundations/lisp/solution.lisp
+++ ArtificialIntelligence/probability-and-statistics-foundations/lisp/solution.lisp
@@ -1,1 +1,12 @@
 ;;; not applicable
+
+(defun my-random ()
+  (let* ((upto 5)
+        (limit 1)
+        (upper 100)
+        (random-result
+         (count-if (lambda (x) x)
+                   (map 'list (lambda (x) (>= x limit))
+                        (loop for x from 1 to upper collect (1+ (random upto)))))))
+    (* 1.0 (/ upper random-result ))
+    ))
