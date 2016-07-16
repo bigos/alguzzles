b8fde3c42f0895a59e5952f212c10404d46fa0d9
Jacek Podkanski <ruby.object@googlemail.com>
2016-07-16 17:16:30 +0100

    hmm

--- ArtificialIntelligence/probability-and-statistics-foundations/lisp/day-2-basic-probability-puzzles-3/solution.lisp
+++ ArtificialIntelligence/probability-and-statistics-foundations/lisp/day-2-basic-probability-puzzles-3/solution.lisp
@@ -1,1 +1,9 @@
 ;;; need to understand probability
+
+(defun probability (wanted-outcomes possible-outcomes)
+  "calculate probability dividing number of wanted by number of
+  possible outcomes"
+  (/ wanted-outcomes
+     possible-outcomes))
+
+;;; independent events - when one event does not influence the second event
