7b15610b9408930f4f4f8b29669de8323f8e8751
Jacek Podkanski <ruby.object@googlemail.com>
2016-08-04 01:14:14 +0100

    varied-variations written

--- ArtificialIntelligence/probability-and-statistics-foundations/lisp/day-2-basic-probability-puzzles-3/solution.lisp
+++ ArtificialIntelligence/probability-and-statistics-foundations/lisp/day-2-basic-probability-puzzles-3/solution.lisp
@@ -21,3 +21,35 @@
              (seq x 1 d1)
              (seq x d1 d2)
              (seq x d3 upto))))
+
+;;; usage (varied-variations 24 3 '(4 3 2))
+(defun varied-variations (n l level-limits)
+  (let ((buckets
+         (make-array l :initial-element 0))
+        (limit
+         (make-array l :initial-contents level-limits))
+        (results))
+    (labels ((reset-bucket (level)
+               (loop for x from 0 below level do (setf (elt buckets x) 0)))
+             (this-level-limit (level)
+               (elt limit level))
+             (variation (level)
+               ;; get value and append it to results
+
+               (when (zerop level )
+                  (format t "======= ~A~%" buckets)
+               )
+
+               ;; increase value
+               (incf (elt buckets level))
+               ;; go to next level if necessary
+               (when (>= (elt buckets level) (this-level-limit level))
+                 (when (< level (1- l))
+                   (variation (1+ level))))
+               ;; zero lower levels
+               (reset-bucket level)
+               (setf level 0)))
+      (loop  for x from 1 to n
+         do
+           (variation 0)))
+    results))
