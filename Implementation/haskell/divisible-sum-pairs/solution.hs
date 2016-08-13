f72b576433df81f712861491f1d7fd32de72cac7
Jacek Podkanski <ruby.object@googlemail.com>
2016-08-14 00:24:50 +0100

    puzzle accepted

--- Implementation/haskell/divisible-sum-pairs/solution.hs
+++ Implementation/haskell/divisible-sum-pairs/solution.hs
@@ -1,5 +1,15 @@
 import Control.Monad
 
+elat x a = head $ drop x a
+
+inds :: Int -> [(Int, Int)]
+inds n = [(x,y) | x <- [0..nn], y <- [0..nn], x < y]
+  where nn = n-1
+
+
+solution n k a = length $ filter (\x -> x == 0) ij
+  where ij = map (\x -> mod (addij x) k) (inds n)
+        addij x = (elat (fst x) a + elat (snd x) a)
 
 splitAndRead :: String -> [Int]
 splitAndRead str = map read (words str)
@@ -10,4 +20,4 @@ main = do
   aa0 <- getLine
   let nk = splitAndRead nk0
   let aa = splitAndRead aa0
-  print [nk, aa]
+  print $ solution (head nk) (head $ tail nk) aa
