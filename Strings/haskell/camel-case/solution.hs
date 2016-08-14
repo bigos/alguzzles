86a851f3371770ba4637cae901d3a941ea6dbd94
Jacek Podkanski <ruby.object@googlemail.com>
2016-08-14 21:49:05 +0100

    puzzle accepted

--- Strings/haskell/camel-case/solution.hs
+++ Strings/haskell/camel-case/solution.hs
@@ -1,8 +1,11 @@
--- solution.hs
+import Data.Char
 
+solution :: String -> Int
+solution s = upcases + 1
+  where upcases = length $ filter (\x -> x == True)  [ isUpper x | x <- s ]
 
 main :: IO()
 main = do
   s0 <- getLine
   let s = s0
-  print s
+  print $ solution s
