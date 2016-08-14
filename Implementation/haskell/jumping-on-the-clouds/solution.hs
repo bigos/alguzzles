b77ed62def0b85fcfa02d0139a35f40fba0ed21d
Jacek Podkanski <ruby.object@googlemail.com>
2016-08-15 00:47:04 +0100

    puzzle accepted

--- Implementation/haskell/jumping-on-the-clouds/solution.hs
+++ Implementation/haskell/jumping-on-the-clouds/solution.hs
@@ -1,9 +1,26 @@
 import Data.Char
 
+split1 :: [Int] -> [[Int]] -> [Int] -> [[Int]]
+split1 [] a c = a ++ [c]
+split1 l  a c = if head l == 1 then x else y
+  where x = split1 (tail l) (a ++ [c]) []
+        y = split1 (tail l) a (c ++ [head l])
+
+split :: [Int] -> [[Int]]
+split l = split1 l [] []
+
+sublenx :: [Int] -> Int
+sublenx a = div (length a) 2
+
+solution s = sum sublens + divlen
+  where subs = split s
+        divlen = (length subs) - 1
+        sublens = map (\x -> sublenx x ) subs
+
 main :: IO()
 main = do
   n0 <- getLine
   s0 <- getLine
   let n = read n0 :: Int
-  let s =  map read $ words s0 :: [Int]
-  print (n,s)
+  let s = map read $ words s0 :: [Int]
+  print $ solution s
