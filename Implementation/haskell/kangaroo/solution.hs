f7bdbd4b6cba79b23dbf23ef0dd78a65e4ab8ae1
Jacek Podkanski <ruby.object@googlemail.com>
2016-08-15 21:00:01 +0100

    wrong answer

--- Implementation/haskell/kangaroo/solution.hs
+++ Implementation/haskell/kangaroo/solution.hs
@@ -1,1 +1,19 @@
--- solution.hs
+import Data.List
+
+convergent :: [Int] -> [Int] -> [Int]
+convergent a b = intersect a b
+
+positions s j = take 10 [s,(s+j)..]
+
+solution :: [Int] -> String
+solution l = if convergent a b == [] then "NO" else checkSteps
+  where a = positions (l!!0) (l!!1)
+        b = positions (l!!2) (l!!3)
+        zzz = filter (\z -> z == True) $ map (\x -> elemIndex x a == elemIndex x b ) (convergent a b)
+        checkSteps = if zzz /= [] then "YES" else "NO"
+
+main :: IO()
+main = do
+  l0 <- getLine
+  let l = map read (words l0) :: [Int]
+  putStr $ solution l
