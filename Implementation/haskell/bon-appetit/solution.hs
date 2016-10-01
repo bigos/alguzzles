8bd5892b7c9540dc799c3ea7d24263d1a2933748
Jacek Podkanski <ruby.object@googlemail.com>
2016-10-01 16:49:41 +0100

    read the input data

--- Implementation/haskell/bon-appetit/solution.hs
+++ Implementation/haskell/bon-appetit/solution.hs
@@ -1,1 +1,15 @@
 -- solution goes here
+import Control.Monad
+
+splitAndRead :: String -> [Int]
+splitAndRead str = map read (words str)
+
+main :: IO()
+main = do
+  line1 <- getLine
+  line2 <- getLine
+  line3 <- getLine
+  let nk0 = splitAndRead line1
+  let costs = splitAndRead line2
+  let charged = read line3 :: Int
+  print (nk0, costs, charged)
