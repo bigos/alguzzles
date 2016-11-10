efe595b0cbe88eb8caeb220e856ba16847217fa0
Jacek Podkanski <ruby.object@googlemail.com>
2016-11-10 03:40:09 +0000

    pattern

--- BitManipulation/lisp/sum-vs-xor/solution.lisp
+++ BitManipulation/lisp/sum-vs-xor/solution.lisp
@@ -1,9 +1,21 @@
+(proclaim '(optimize (speed 3) (safety 0)))
+
+(defparameter *cnt* 0)
+
+(defun pattern (n)
+  (loop for x from 0 to n do
+       (format t "~6,B ~3,d ~a ~3,d ~3,d~%" x x
+               (if (eq (+ n x) (logxor n x))
+                   #\= #\!)
+               (+ n x) (logxor n x))))
+
 (defun solve-me (n)
-  (loop for x from 0 to n
-     when (eq (+ n x)
-              (logxor n x))
-     collect x
-       ))
+  (loop for x from 0 to n do
+       (when (eq (+ n x)
+                 (logxor n x))
+         (incf *cnt*)))
+  (princ *cnt*)
+  (terpri))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun split-by-one-space (string)
@@ -20,8 +32,7 @@
 
 (defun solution (&optional stream)
   (let ((n (parse-integer (read-line stream))))
-    (princ (length (solve-me n)))
-    (terpri)))
+    (solve-me n)))
 
 ;; (solution) ; uncomment this when running on hacker-rank
 
@@ -30,7 +41,7 @@
                       :directory
                       (pathname-directory
                        (parse-namestring *load-pathname*))
-                      :name "input10" :type "txt"))
+                      :name "input0" :type "txt"))
     (solution s)))
 
 (main)
