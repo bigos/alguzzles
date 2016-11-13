69295db5264c2ebf31ac9ccd96f6f992e3105482
Jacek Podkanski <ruby.object@googlemail.com>
2016-11-13 16:01:20 +0000

    puzzle accepted

---
+++ Strings/lisp/string-construction/solution.lisp
@@ -0,0 +1,36 @@
+(defun solve-me (s)
+  (let ((ht (make-hash-table)))
+    (loop for c across s do
+         (incf (gethash c ht 0)))
+    (format t "~A~%" (hash-table-count ht))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((tc (parse-integer (read-line stream))))
+    (dotimes (x tc)
+      (solve-me (read-line stream)))))
+
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
