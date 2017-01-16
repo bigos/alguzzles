cc2ba62a98447d38ddb2b52a86af85863c8e6fde
Jacek Podkanski <ruby.object@googlemail.com>
2017-01-16 01:51:01 +0000

    prepared new puzzle

---
+++ Implementation/lisp/append-and-delete/solution.lisp
@@ -0,0 +1,33 @@
+(defun solve-me (ss st ik)
+  (format t "~A ~A ~A~%" ss st ik))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((ss (read-line stream))
+        (st (read-line stream))
+        (ik (car (split-and-parse (read-line stream)))))
+    (solve-me ss st ik)))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
