d7698e9c3cce72315b5eca7bf4306c9ba16fa6bf
Jacek Podkanski <ruby.object@googlemail.com>
2017-01-16 18:19:46 +0000

    some success reading data for clojure puzzles

--- Implementation/lisp/designer-pdf-viewer/solution.clj
+++ Implementation/lisp/designer-pdf-viewer/solution.clj
@@ -1,41 +1,22 @@
 (use 'clojure.java.io)
 
-(println "aaaaaaaaaaaaaaaaaaaaaa")
-(print (type *in*))
 
-(println "")
-
-(println (line-seq (java.io.BufferedReader. *in*)))
-;; (defn solution
-;;   []
-;;   (println (type *in*))
-;;   ;; (doseq [line (line-seq (java.io.BufferedReader *in*))]
-;;   ;;   println line)
-;;   )
-
-
-;;  (solution) ; uncomment on Hackerrank
-
-
-
-;;
-;;  )
 ;; -----------------------------------------------------------
-;; (defn read-lines
-;;   [f n]
-;;   (map f (repeatedly n read-line)))
+(defn read-lines
+  [f n]
+  (map f (repeatedly n read-line)))
 
-;; (defn as-int
-;;   [x]
-;;   (Integer/parseInt x))
+(defn as-int
+  [x]
+  (Integer/parseInt x))
 
-;; (defn split-line
-;;   [line]
-;;   (.split line " "))
+(defn split-line
+  [line]
+  (.split line " "))
 
-;; (defn as-ints
-;;   [line]
-;;   (map as-int (split-line line)))
+(defn as-ints
+  [line]
+  (map as-int (split-line line)))
 
 ;; (defn char-range [start end]
 ;;   (map char (range (int start) (inc (int end)))))
@@ -48,3 +29,8 @@
 ;;        (apply max)
 ;;        (* (count word))
 ;;        (println)))
+
+
+(let [[heights] (read-lines as-ints 1)
+      [word] (read-lines identity 1)]
+  (println (list heights "\n" word "\n")))
