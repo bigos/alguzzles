60d95e69071ae4dc1c61e914fcc109ca41d5d031
Jacek Podkanski <ruby.object@googlemail.com>
2017-01-16 00:51:37 +0000

    new puzzle

---
+++ Implementation/lisp/between-two-sets/solution.lisp
@@ -0,0 +1,34 @@
+(defun solve-me (n m la lb)
+  (format t "~A ~A    ~A ~A~%" n m la lb))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((nm (split-and-parse (read-line stream)))
+        (la (split-and-parse (read-line stream)))
+        (lb (split-and-parse (read-line stream))))
+    (solve-me (car nm) (cadr nm) la lb)))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
