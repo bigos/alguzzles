87d7d73a04dcb6d1f357dd02916c290aeb992d28
Jacek Podkanski <ruby.object@googlemail.com>
2017-01-17 23:51:35 +0000

    prepared a clojure puzzle for solving

---
+++ Search/lisp/gridland-metro/solution.clj
@@ -0,0 +1,31 @@
+(use 'clojure.java.io 'clojure.pprint)
+
+(defn solve-me
+  [n m k ii]
+  (cl-format true "~&solving ~s ~s ~s ~s~%" n m k ii))
+
+;; ---------- functions for reading the inputs ---------------------------------
+(defn read-lines
+  [f n]
+  (map f (repeatedly n read-line)))
+
+(defn as-int
+  [x]
+  (Integer/parseInt x))
+
+(defn split-line
+  [line]
+  (.split line " "))
+
+(defn as-ints
+  [line]
+  (map as-int (split-line line)))
+
+;;; this closure reads the data and sends it to solve-me
+(let [nmk (first (doall (read-lines as-ints 1)))]
+  (let [n (nth nmk 0)
+        m (nth nmk 1)
+        k (nth nmk 2)]
+    (let [ii (doall (read-lines as-ints k))]
+      ;; (cl-format true "~s ~s ~s ~s ~%" n m k ii)
+      (solve-me n m k ii))))
