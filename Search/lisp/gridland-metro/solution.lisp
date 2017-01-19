14f8e4f9ed02e8e0b23b982f084db12f3df81a54
Jacek Podkanski <ruby.object@googlemail.com>
2017-01-19 01:09:54 +0000

    looks much better but many test cases time out

--- Search/lisp/gridland-metro/solution.lisp
+++ Search/lisp/gridland-metro/solution.lisp
@@ -1,5 +1,31 @@
 (declaim (optimize (speed 3)))
 
+(defun remove-overlapping (ranges)
+  (remove-overlapping-2 (sort ranges (lambda (x y) (<= (car x) (car y))))
+                        nil))
+
+(defun remove-overlapping-2 (ranges acc)
+  (let ((a1 (caar ranges))
+        (a2 (cadar ranges))
+        (b1 (caadr ranges))
+        (b2 (cadadr ranges)))
+    (if (cdr ranges)
+        (if (<= b1 (1+ a2))
+            (remove-overlapping-2 (cons (list (min a1 b1)
+                                              (max a2 b2))
+                                        (cddr ranges))
+                                  acc)
+            (remove-overlapping-2 (cdr ranges)
+                                  (cons (car ranges)
+                                        acc)))
+        (cons (car ranges) acc))))
+
+(defun sum-ranges (ranges)
+  (if (null ranges)
+      0
+      (loop for x in (remove-overlapping ranges)
+         sum (1+ (- (cadr x) (car x))))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun split-by-one-space (string)
   (loop for i = 0 then (1+ j)
@@ -18,34 +44,18 @@
          (n (nth 0 nmk))
          (m (nth 1 nmk))
          (k (nth 2 nmk))
-         (rh (make-hash-table))
-         (max-lamps (* n m))
-         (tempar))
+         (rh (make-hash-table)))
     (loop
        for l from 1 to k
        for rl = (split-and-parse (read-line stream))
        do
          (push (cdr rl)
                (gethash (car rl) rh)))
-    (loop
-       for r from 1 to n
-       for rhv = (gethash r rh)
-       do
-         (when rhv
-           (setf tempar (make-array (list m)
-                                    :initial-element 1
-                                    :element-type 'bit))
-           (loop for rse in rhv
-              do
-                (loop for c from (1- (car rse)) below (cadr rse)
-                   do
-                     (unless (zerop (aref tempar c))
-                       (setf (aref tempar c) 0
-                             max-lamps (1- max-lamps)))))
-           (format t "~s ~s~%" rhv tempar)
-           ))
-
-    (format t "~S~%"  max-lamps)))
+    (princ
+     (loop
+        for r from 1 to n
+        for rhv = (gethash r rh)
+        sum (- m (sum-ranges rhv)) ))))
 
 ;; (solution) ; uncomment this when running on hacker-rank
 
@@ -54,7 +64,7 @@
                       :directory
                       (pathname-directory
                        (parse-namestring *load-pathname*))
-                      :name "input0" :type "txt"))
+                      :name "input06" :type "txt"))
     (solution s)))
 
 (main)
