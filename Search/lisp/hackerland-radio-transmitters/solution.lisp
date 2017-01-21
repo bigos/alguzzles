5f3470b2fdad981ec08ff30d4bfe0e574b735119
Jacek Podkanski <ruby.object@googlemail.com>
2017-01-21 04:31:47 +0000

    prepared new puzzle

---
+++ Search/lisp/hackerland-radio-transmitters/solution.lisp
@@ -0,0 +1,34 @@
+(defun solve-me (n k hs)
+  (format t "~A ~A  ~A~%" n k hs))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let* ((nk (split-and-parse (read-line stream)))
+         (n (nth 0 nk))
+         (k (nth 2 nk))
+         (hs (split-and-parse (read-line stream))))
+    (solve-me n k hs)))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
