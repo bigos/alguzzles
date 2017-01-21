467c44cdae1ef9e8fdf7db6cbbaed6c6e15f9ba8
Jacek Podkanski <ruby.object@googlemail.com>
2017-01-21 06:23:44 +0000

    puzzle accepted

--- Search/lisp/hackerland-radio-transmitters/solution.lisp
+++ Search/lisp/hackerland-radio-transmitters/solution.lisp
@@ -1,7 +1,47 @@
+(defun furthest (a x)
+  (cond
+    ((null (cadr a))
+     a)
+    ((and
+      (< (car a) x)
+      (> (cadr a) x))
+     a)
+    ((eql (car a) x)
+     a)
+    (t
+     (furthest (cdr a) x))))
+
 (defun solve-me (n k hs)
-  (format t "~A ~A  ~A~%" n k hs))
+  (declare (ignore n))
+  ;; (format t "~A ~A  ~A~%" n k hs)
+  ;; given a house
+  ;; find furthest reachable transmitter location
+  ;; find furthest reachable house
+  ;; go to next house
+  ;; repeat if there are houses left
+  (let ((fh)
+        (tx)
+        (lh)
+        (nx T)
+        (count 0))
+    (loop
+       until
+         (null nx)
+       do
+         (setf fh hs)
+         (setf tx
+               (furthest fh (+ (car fh) k)))
+         (setf lh
+               (furthest tx (+ (car tx) k)))
+         (setf nx (cdr lh)
+               hs (cdr lh))
+         (incf count)
+         )
+    (princ count)))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defun split-by-one-space (string)
   (loop for i = 0 then (1+ j)
      as j = (position #\Space string :start i)
@@ -17,9 +57,9 @@
 (defun solution (&optional stream)
   (let* ((nk (split-and-parse (read-line stream)))
          (n (nth 0 nk))
-         (k (nth 2 nk))
+         (k (nth 1 nk))
          (hs (split-and-parse (read-line stream))))
-    (solve-me n k hs)))
+    (solve-me n k (sort (remove-duplicates hs) '<))))
 
 ;; (solution) ; uncomment this when running on hacker-rank
 
