2c5cc2876ea59384882938f3e6aca3751e2c1178
Jacek Podkanski <ruby.object@googlemail.com>
2017-01-22 01:13:01 +0000

    puzzle accepted

--- Implementation/lisp/happy-ladybugs/solution.lisp
+++ Implementation/lisp/happy-ladybugs/solution.lisp
@@ -1,11 +1,33 @@
+(defun has-neighbour (a n)
+  (cond ((eql n 1)
+         nil)
+        ((eql n 2)
+         (eql (aref a 0)
+              (aref a 1)))
+        ((eql 1 1)
+         (and
+          (eql (aref a 0)
+               (aref a 1))
+          (every (lambda (x) (not (null x)))
+                 (loop for i from 1 to (- n 2)
+                    collect (or (eql (aref a i)
+                                     (aref a (1- i)))
+                                (eql (aref a i)
+                                     (aref a (1+ i))))))
+          (eql (aref a (- n 1))
+               (aref a (- n 2)))))))
+
 (defun solve-me (n b)
-  (declare (ignore n))
   (let ((h (make-hash-table))
+        (a (make-array (list n) ))
         (underscores)
         (counts))
     ;; (format t "~&=====> ~A ~A~%" n b)
-    (loop for c across b
+    (loop
+       for c across b
+       for i = 0 then (1+ i)
        do
+         (setf (aref a i) c)
          (incf (gethash c h 0)))
     (setf underscores (gethash #\_ h 0))
     ;; (format t "underscores found ~d~%" underscores)
@@ -13,10 +35,13 @@
                (unless (eql k #\_)
                  (push  (list k v) counts)))
              h)
+    ;; (format t "~S~%" a)
     ;; (format t "counts ~s~%" counts)
     (format t "~a~%"
-            (if (and (> underscores 0)
-                     (every (lambda (x) (> (cadr x) 1)) counts))
+            (if (or
+                 (and (> underscores 0)
+                      (every (lambda (x) (> (cadr x) 1)) counts))
+                 (has-neighbour a n))
                 "YES"
                 "NO"))))
 
@@ -50,7 +75,7 @@
                       :directory
                       (pathname-directory
                        (parse-namestring *load-pathname*))
-                      :name "input0" :type "txt"))
+                      :name "input01" :type "txt"))
     (solution s)))
 
 (main)
