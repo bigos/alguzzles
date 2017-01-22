c978984332c2f0ba11902faeafee33016cb549c0
Jacek Podkanski <ruby.object@googlemail.com>
2017-01-22 13:07:55 +0000

    prepared new puzzle

---
+++ Implementation/lisp/non-divisible-subset/solution.lisp
@@ -0,0 +1,33 @@
+(defun solve-me (n k aa)
+  (format t "~A ~A ~A ~%" n k aa))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((nk (split-and-parse (read-line stream)))
+        (aa (split-and-parse (read-line stream))))
+    (solve-me (car nk) (cadr nk) (sort aa '<))))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
