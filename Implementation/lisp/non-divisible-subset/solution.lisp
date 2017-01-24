641e9f41106f0bcdc381e9d478889164f2118f41
Jacek Podkanski <ruby.object@googlemail.com>
2017-01-24 02:10:53 +0000

    progress, but still timeouts, no more creashes

--- Implementation/lisp/non-divisible-subset/solution.lisp
+++ Implementation/lisp/non-divisible-subset/solution.lisp
@@ -1,7 +1,15 @@
 (declaim (optimize (speed 3) (space 0)))
 
-(defun combinator (cc k)
+;;; (comb 3 '(1 2 4 7) #'print)
+(defun comb (m list fn) ; without repetition
+  (labels ((comb1 (l c m)
+             (when (>= (length l) m)
+               (if (zerop m) (return-from comb1 (funcall fn c)))
+               (comb1 (cdr l) c m)
+               (comb1 (cdr l) (cons (first l) c) (1- m)))))
+    (comb1 list nil m)))
 
+(defun combinator (cc k) ; with repetition
   (cond ((zerop k) '(()))
         ((not cc) nil)
         (T (append
@@ -10,35 +18,42 @@
                  (combinator cc (- k 1)))
             (combinator (cdr cc) k)))))
 
-;; (remove-duplicates (remove-if (lambda (x) (not  (eql 2 (length x)))) (map 'list 'remove-duplicates (combinator '(1 7 2 4) 4))) :test'equalp)
-
-(defun subset-pairs (aa)
-  (remove-if (lambda (x) (eql (car x) (cadr x))) (combinator aa 2)))
-
 (defun divides-by (n d)
   (zerop (mod n d)))
 
-(defun solve-me (n k aa)
-                                        ;(format t "=========== ~A ~A ~A~%" n k aa)
-  (let ((found))
-    (loop for subset in (sort
-                         (remove-duplicates
-                          (map 'list #'remove-duplicates
-                               (combinator aa n))
-                          :test #'equal)
-                         (lambda (x y) (> (length x) (length y))))
-       for sp = (subset-pairs subset)
-       do
+(defparameter *cc* nil)
+(defparameter *dd* nil)
 
-         (if sp
-             (setf found (every 'null (loop for s in sp
-                                         collect
-                                           (divides-by (apply '+ s) k))))
-             (setf found (not (divides-by (car subset) k)))
-             )
-       until found
-       finally
-         (format t "~&~A~%" (length subset)))))
+(defun pushme-cc (x) (push x *cc*))
+(defun pushme-dd (x) (push x *dd*))
+
+(defun solve-me (n k aa)
+  ;; (format t "=========== ~A ~A ~A~%" n k aa)
+  (let ((found))
+    (loop for x from n downto 2
+       do
+         (setf *cc* nil)
+         (comb x aa 'pushme-cc)
+         ;; (format t "cc: ~a~%" *cc*)
+         (loop for y in *cc*
+            do
+              ;; (format t "### ~a~%" y)
+              (setf *dd* nil)
+              (comb 2 y 'pushme-dd)
+
+              (when (every 'null
+                           (loop for z in *dd*
+                              collect (divides-by (apply #'+ z) k)))
+                (setf found y))
+            until found)
+       until found)
+    (unless found
+      (loop for a in aa
+         do
+           (when (divides-by a k)
+             (setf found (list a)))))
+    (format t "~A~%" (length found))
+    ))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -57,7 +72,7 @@
 (defun solution (&optional stream)
   (let ((nk (split-and-parse (read-line stream)))
         (aa (split-and-parse (read-line stream))))
-    (solve-me (car nk) (cadr nk) (sort aa '>))))
+    (solve-me (car nk) (cadr nk) aa)))
 
 ;; (solution) ; uncomment this when running on hacker-rank
 
@@ -66,7 +81,7 @@
                       :directory
                       (pathname-directory
                        (parse-namestring *load-pathname*))
-                      :name "input09" :type "txt"))
+                      :name "input0" :type "txt"))
     (solution s)))
 
 (main)
