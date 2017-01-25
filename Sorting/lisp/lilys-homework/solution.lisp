83c295c14d09c84f512a28aec10bc4537a666f6f
Jacek Podkanski <ruby.object@googlemail.com>
2017-01-25 23:50:01 +0000

    puzzle accepted

--- Sorting/lisp/lilys-homework/solution.lisp
+++ Sorting/lisp/lilys-homework/solution.lisp
@@ -7,13 +7,14 @@
          (aryb (make-array (list n) :initial-contents bb)))
 
     (labels ((swap-pos (a b)
-               (format T "SWAPPING ~A ~A~%" a b)
-               (setf
-                (gethash (car a) hi) (cdr b)
-                (gethash (car b) hi) (cdr a)
-                (aref arya (cdr a)) (car b)
-                (aref arya (cdr b)) (car a))
-               (incf swaps))
+               (unless (eql (car a) (car b))
+                 ;;(format T "SWAPPING ~A ~A~%" a b)
+                 (setf
+                  (gethash (car a) hi) (cdr b)
+                  (gethash (car b) hi) (cdr a)
+                  (aref arya (cdr a)) (car b)
+                  (aref arya (cdr b)) (car a))
+                 (incf swaps)))
 
              (dump-hash ()
                (format t "~&=== value => index  ")
@@ -21,34 +22,38 @@
                (format t "== ~s ~%" arya)
                )
              (swapme (x)
-               (format t "~&~A~%" (list (aref arya x)
-                                        'is-at-pos
-                                        (gethash (aref arya x) hi)
-                                        'has-to-be-swapped-with
-                                        (aref aryb x)
-                                        'which-as-at-pos
-                                        (gethash (aref aryb x) hi)))
+               ;; (format t "~&~A~%" (list (aref arya x)
+               ;;                          'is-at-pos
+               ;;                          (gethash (aref arya x) hi)
+               ;;                          'has-to-be-swapped-with
+               ;;                          (aref aryb x)
+               ;;                          'which-as-at-pos
+               ;;                          (gethash (aref aryb x) hi)))
 
                (list (cons (aref arya x) (gethash (aref arya x) hi))
                      (cons (aref aryb x) (gethash (aref aryb x) hi))))
              )
 
-      (format t "=== unsorted ~A sorted ~A  ary ~a  ~A~%" aa bb arya aryb)
+      ;; (format t "=== unsorted ~A sorted ~A  ary ~a  ~A~%" aa bb arya aryb)
       (loop
          for a in aa
          for i = 0 then (1+ i)
          do
            (setf (gethash a hi) i))
 
-      (dump-hash)
+      (loop
+         with y = 0
+         with dd
+         while (< y (1- n))
+         do
+                                        ;; (dump-hash)
+           (setf dd (swapme y))
+           (swap-pos (car dd) (cadr dd))
+                                        ;; (dump-hash)
+           (incf y)
+           ))
 
-      (swapme 0)
-      (swap-pos (cons 2 0) (cons 1 3))
-      (dump-hash)
-
-      )
-
-    (format t "~A~%" swaps)))
+     swaps))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -67,7 +72,8 @@
 (defun solution (&optional stream)
   (let ((n (parse-integer (read-line stream)))
         (aa (split-and-parse (read-line stream))))
-    (solve-me n aa)))
+    (princ (min (solve-me n aa)
+                (solve-me n (reverse aa))))))
 
 ;; (solution) ; uncomment this when running on hacker-rank
 
@@ -76,7 +82,7 @@
                       :directory
                       (pathname-directory
                        (parse-namestring *load-pathname*))
-                      :name "input0" :type "txt"))
+                      :name "input01" :type "txt"))
     (solution s)))
 
 (main)
