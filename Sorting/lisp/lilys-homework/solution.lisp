0b0890f69c20602c72ce01d9c44976c407bb5c65
Jacek Podkanski <ruby.object@googlemail.com>
2017-01-25 16:00:28 +0000

    only two twst cases passed

--- Sorting/lisp/lilys-homework/solution.lisp
+++ Sorting/lisp/lilys-homework/solution.lisp
@@ -1,5 +1,38 @@
 (defun  solve-me (n aa)
-  (format t "~A  ~A~%" n aa))
+  (declare (ignore n))
+  ;; (format t "~A~%"  aa)
+  (let ((bb (sort (copy-list aa) #'<))
+        (swaps 0)
+        (h (make-hash-table)))
+
+    (labels ((swap-pos (a b)
+               (let ((temp (gethash a h)))
+                 (setf (gethash a h) (gethash b h)
+                       (gethash b h) temp))
+               (incf swaps))
+             (dump-hash ()
+               (format t ">>> ~A~%"
+                       (loop for x in bb collect (list x 'at 'pos 'of (gethash x h))))))
+
+      ;; (format t "=== unsorted ~A sorted ~A~%" aa bb)
+      (loop
+         for a in aa
+         for b in bb
+         do
+           (setf (gethash a h) b))
+      (loop for b in aa
+         do
+           ;; (dump-hash)
+           (unless (eql b (gethash b h))
+               (progn
+                 ;; (format t "swapping ~A ~A~%" b (gethash b h) )
+                 (swap-pos b (gethash b h))))))
+
+    (format t "~A~%" swaps)))
+
+;; 2531
+;; 1532
+;; 1235
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
