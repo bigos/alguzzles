8032f232035f0223f7dbaf8cad2f76c3b54d0592
Jacek Podkanski <ruby.object@googlemail.com>
2017-01-26 18:26:55 +0000

    new puzzle

---
+++ DynamicProgramming/lisp/abbreviation/solution.lisp
@@ -0,0 +1,37 @@
+(defun solve-me (a b)
+  (format t "~A ~A~%~%" a b )
+  (princ "YES"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((q (parse-integer (read-line stream))))
+    (loop for x from 1 to q
+       do
+         (solve-me
+          (car (split-by-one-space (read-line stream)))
+          (car (split-by-one-space (read-line stream)))))))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
