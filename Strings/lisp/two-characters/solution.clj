927ebd22a488223ea58ced24433eed3983442461
Jacek Podkanski <ruby.object@googlemail.com>
2017-02-16 01:34:11 +0000

    looking good, but stuck at lazy sequences again

--- Strings/lisp/two-characters/solution.clj
+++ Strings/lisp/two-characters/solution.clj
@@ -30,18 +30,56 @@
         y l]
     [x y]))
 
+(defn compare-sorted [l]
+  (= l
+     (sort (fn [x y] (> x y)) l)))
+
+(defn first-choice [x y f]
+  (if (apply f [(first x) (first y)])
+    x
+    y))
+
+(defn foo [x y]
+  (let [a (first-choice x y >)
+        b (first-choice x y <)
+        maxnum 1000000]
+    (let [res (flatten (map (fn [x y] [x y]) a b))]
+      (if (= (first res) maxnum)
+        (rest res)
+        res))))
+
+(defn zipme [p]
+  (let [a (first p)
+        b (last p)
+        maxnum 1000000]
+    (let [res (cond
+                (= (count a) (count b))       (foo a b)
+                (= (+ 1 (count a)) (count b)) (foo (cons maxnum a) b)
+                (= (count a) (+ 1 (count b))) (foo a (cons maxnum b)))]
+      (if (and res (compare-sorted res))
+        (count res)
+        0))))
+
+
 (defn solve-me
   [sl s]
   (cl-format true "===== ~A ~S~%" sl s)
   (let [hi (indexes-hash s)
         hc (counts-hash s)]
-    (cl-format true "---- ~s~%" hi)
-    (cl-format true "---- ~s~%" hc)
-    (cl-format true "---- ~s~%" (sort-indexes (vals hi)))
+    ;; (cl-format true "---- ~s~%" hi)
+    ;; (cl-format true "---- ~s~%" hc)
+    ;; (cl-format true "---- ~s~%" (sort-indexes (vals hi)))
 
-    (cl-format true "---- ~s~%"
-               (remove (fn [x] (= (first x) (last x)))
-                       (combinations-wrep (sort-indexes (vals hi)))))))
+    ;; (cl-format true "---- ~s~%"
+    ;;            (remove (fn [x] (= (first x) (last x)))
+    ;;                    (combinations-wrep (sort-indexes (vals hi)))))
+
+    (cl-format true "----> ~s~%"
+               (first
+                (sort >
+                      (map zipme
+                           (remove (fn [x] (= (first x) (last x)))
+                                   (combinations-wrep (sort-indexes (vals hi))))))))))
 
 ;; ---------- functions for reading the inputs ---------------------------------
 (defn read-lines
@@ -61,7 +99,9 @@
   (map as-int (split-line line)))
 
 ;;; this closure reads the data and sends it to solve-me
-(let [[sl] (read-lines as-ints 1)
-      [s] (doall (read-lines identity 1))]
-  ;; (println (list heights "\n" word "\n" ))
-  (solve-me sl s))
+
+;;; uncomment the following code for hackerrank and lein exec
+
+;; (let [[sl] (read-lines as-ints 1)
+;;       [s] (doall (read-lines identity 1))]
+;;   (solve-me sl s))
