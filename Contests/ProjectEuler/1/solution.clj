d8dc90d6290ba0cc59ad59086109d5fb02335ab8
Jacek Podkanski <ruby.object@googlemail.com>
2017-02-18 07:10:16 +0000

    progress, but still one test case failed

--- Contests/ProjectEuler/1/solution.clj
+++ Contests/ProjectEuler/1/solution.clj
@@ -8,29 +8,13 @@
 
 (defn recursive [n acc l steps]
   (let [ss (if (= steps '()) '(3 2 1 3 1 2 3) steps) ]
-    (cl-format true "-- ~A ~A ~A ~A~%" n acc l ss)
-    (if (= 7 (count ss))
-      (cl-format true "!!! ~A  zzz ~A~%" acc (/ (/ (- acc 0 ) 5) 3) ))
-    ;; values of zzz - old-zzz differ by 7
-    ;; starting with 3
-    ;; previous zzz + zzz + 7 = new zzz
-    ;; 3 + 7 + 7 + 7 or
-    ;; for all positions except the first
-    ;; 7 * pos - 4, so
-    ;; 7-4 + 14-4 + 21-4 + 28-4
-    ;;  7 * 1 - 4 = 3, 7 * 2 - 4 = 10, 7 * 3 - 4 = 17, 7 * 4 -4 = 23
-    ;; 3*3*5 = 45, 13*3*5 = 195, 30*3*5=450
-    ;; 9+6=13, 21+9=30, 42+12=54, 70+15=85, 105+18=123, 147+21=168, 196+24=220, 252+27=279
-    ;; x+y=z where y=3*a and z is the result in the above line
-    ;; sequence (map (fn[x] [x (* x 7)]) (range 1 45)) has above values of x
-    ;; where first part is the key and the value becomes x two lines above
-    ;;  21 is 3*7, 42 is 6*7, 70 is 10*7, 105 is 15*7, 147 is 21*7, 196 is 28*7, 252 is 36*7
-    ;; 1*3*7, 2*3*7, 2*5*7, 3*5*7, 3*7*7, 4*7*7, 4*9*7
-    ;; triangular numbers?
-    ;; triangular number 45 times 7 plus 30 gives expected 345 which multiplied
-    ;; by 3 and 5 gives correct result (* 3 5 (+ 315 30))
-    ;; or (* 3 5 (+ 30 (* 45 7)))
+    ;; (cl-format true "-- ~A ~A ~A ~A~%" n acc l ss)
+    ;; (if (= 7 (count ss))
+    ;;   (cl-format true "!!! ~A  zzz ~A~%" acc (/ (/ (- acc 0 ) 5) 3) ))
     ;; fifteenator solves the problem
+    ;; calculate to nearest multiple of 15
+    ;; (recursive 0 0 60 '()) which has acc value of 810
+    ;; then calculate the rest  (recursive 60 810 70 '()) then finish up to 70
 
     (if (>= n l)
       acc
@@ -42,7 +26,12 @@
 
 (defn solve-me [l]
   (cl-format true "~A~%"
-             (recursive 0 0 l '())))
+             (if (< l 15)
+               (recursive 0 0 l '())
+               (let [fm (- l (mod l 15))
+                     acc (fifteenator (- (/ fm 15) 1))]
+                 ;; (cl-format true "~A   -- ~A     ~A~%" fm acc (/ fm 15))
+                 (recursive fm acc l '())))))
 
 ;; ---------- functions for reading the inputs ---------------------------------
 (defn read-lines
