0a8586926733c05ddcfc170a0c3a4602f464a9b0
Jacek Podkanski <ruby.object@googlemail.com>
2017-02-20 15:54:58 +0000

    hmmm

--- Contests/ProjectEuler/3/solution.clj
+++ Contests/ProjectEuler/3/solution.clj
@@ -1,9 +1,23 @@
-(use '[clojure.string :only (split triml)])
+(use '[clojure.string :only (split triml)]
+     'clojure.pprint)
 
-(let [t_t (read-line)
-      t (Integer/parseInt t_t)]
- (loop [a0 t]
-  (when (> a0 0)
-    (let [n_t (read-line)
-          n (Integer/parseInt n_t)])
-    (recur (- a0 1) ) )))
+(defn factors [n]
+  (filter #(zero? (rem n %)) (range 1 (inc n))))
+
+(defn recfactors [n c a])
+
+(defn solve-me [n]
+  (cl-format true "==== ~A~%" n))
+
+;;; -----------------------------------------------------------------------------
+(defn solution []
+  (let [t_t (read-line)
+        t (Integer/parseInt t_t)]
+    (loop [a0 t]
+      (when (> a0 0)
+        (let [n_t (read-line)
+              n (Integer/parseInt n_t)]
+          (solve-me n))
+        (recur (- a0 1) ) ))))
+
+(solution)
