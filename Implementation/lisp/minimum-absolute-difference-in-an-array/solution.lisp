fdd271612d82201063fa34a476c863114781f49b
Jacek Podkanski <ruby.object@googlemail.com>
2017-05-10 09:39:49 +0100

    puzzle accepted

---
+++ Implementation/lisp/minimum-absolute-difference-in-an-array/solution.lisp
@@ -0,0 +1,38 @@
+(defun solve-me (n ii)
+  (declare (ignore n))
+  (let ((last (expt 10 6)))
+    (format t "~a"
+            (loop for i in ii
+               minimize (abs (- i last))
+               do
+                 (setf last i)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((n (parse-integer (read-line stream)))
+        (ii (split-and-parse (read-line stream))))
+    (solve-me n (sort ii #'<))))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
