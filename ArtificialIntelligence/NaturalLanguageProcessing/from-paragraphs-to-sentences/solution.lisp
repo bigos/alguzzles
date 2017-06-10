43eb8ba0bc1f43f5a6360ec6ac43891f061cf9cf
Jacek Podkanski <ruby.object@googlemail.com>
2017-06-10 23:37:21 +0100

    started natural language parsing puzzle

---
+++ ArtificialIntelligence/NaturalLanguageProcessing/from-paragraphs-to-sentences/solution.lisp
@@ -0,0 +1,36 @@
+(defun solve-me (d)
+  (format t "~A~%" d))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+
+(defun read-all-data (stream)
+  (loop for line = (read-line stream nil 'eof)
+     until (equal line 'eof)
+     collect line))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((d (read-all-data stream)))
+    (solve-me d)))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
