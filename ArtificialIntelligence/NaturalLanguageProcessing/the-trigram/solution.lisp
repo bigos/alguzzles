6541303bd82ebaf56aa5713fefd2ec102432230c
Jacek Podkanski <ruby.object@googlemail.com>
2017-08-03 17:52:22 +0100

    another example

--- ArtificialIntelligence/NaturalLanguageProcessing/the-trigram/solution.lisp
+++ ArtificialIntelligence/NaturalLanguageProcessing/the-trigram/solution.lisp
@@ -1,26 +1,33 @@
 ;;; not solved yet
 
+;;; example of putting multiple lines on one sstring
+(let
+    ((str (make-array '(0) :element-type 'base-char
+                           :fill-pointer 0
+                           :adjustable T)))
+  (with-output-to-string (stream str)
+    (loop for s in '("ala ma kota. Ola"
+                     "ma psa. Ela "
+                     "ma papuge.")
+          do (format stream "~a " s )))
+  (format t "===== ~S" str))
+
 (with-input-from-string (s (format nil "Lorem ipsum.~%Dolor sic amet."))
   (format t "read ~s" (loop for l = (read-line s nil 'eof nil)
                             until (eql l 'eof)
                             collect l)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun split-by-one-space (string)
+(defun split-by-char (string char)
   (loop for i = 0 then (1+ j)
-        as j = (position #\Space string :start i)
+        as j = (position char string :start i)
         collect (subseq string i j)
         while j))
-
-(defun split-and-parse (string)
-  (map 'list
-       (lambda (x) (parse-integer x))
-       (split-by-one-space string)))
 
 (defun read-all-data (stream)
   (loop for line = (read-line stream nil 'eof)
         until (equal line 'eof)
-        collect (string-downcase line)))
+        collect (split-by-char (string-downcase line) #\.)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun solution (&optional stream)
@@ -33,8 +40,12 @@
   (with-open-file (s (make-pathname
                       :directory
                       (pathname-directory
-                       (parse-namestring *load-pathname*))
+                       (parse-namestr
+                        ing *load-pathname*))
                       :name "input0" :type "txt"))
     (solution s)))
 
 (main)
+(progn #|
+
+|#)
