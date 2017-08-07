3f9789cec40c127a4fdba6454a01698fcbd31f5e
Jacek Podkanski <ruby.object@googlemail.com>
2017-08-07 17:04:47 +0100

    sentence splitting

--- ArtificialIntelligence/NaturalLanguageProcessing/the-trigram/solution.lisp
+++ ArtificialIntelligence/NaturalLanguageProcessing/the-trigram/solution.lisp
@@ -1,23 +1,123 @@
 (declaim (optimize (debug 3)))
 
 ;;; not solved yet
+(defun else ()
+  T)
+
+(defun beginning-of-word (string start)
+  (loop for i from (1- start) downto 0
+        for c = (aref string i)
+        while (search "LETTER" (char-name c))
+        finally (return (subseq string (1+ i) start))))
+
+(defun qudoex-p (string start)
+  (let ((char (aref string start)))
+    (some (lambda (x) (eq x char))
+          '(#\? #\! ))
+    (cond
+      ((eq char #\!)
+       T)
+      ((eq char #\?)
+       T)
+      ((eq char #\.)
+       (let ((word (beginning-of-word string start)))
+         (cond ((eq (length word) 1)
+                nil)
+               ;; bad for the sentence division without sentence parsing
+               ;; https://english.stackexchange.com/questions/8382/when-etc-is-at-the-end-of-a-phrase-do-you-place-a-period-after-it
+               ((equalp word "Dr") ; add abbreviations ending with dot here
+                nil)
+               (T
+                T))))
+      (T
+       nil))))
+
+(defun space-p (char)
+  (some (lambda (x) (eq x char))
+        '(#\Space #\Newline #\Tab)))
+
+(defun quote-p (char)
+  (some (lambda (x) (eq x char))
+        '(#\" #\')))
+
+(defun upcase-p (char)
+  (search "CAPITAL_LETTER" (char-name char)))
+
+(defun arefy (seq i)
+  (if (>= i (length seq))
+      (aref seq (1- (length seq)))
+      (aref seq i)))
+
+;;; need to add handling of sentences starting with >. "Capital<
+(defun find-sentence-end (string start)
+  "Find sentence end in a STRING starting at START."
+  (macrolet
+      ((skip (char)
+         `(find-sentence-end string (1+ ,char)))
+       (next-char (n body)
+         `(let ((cur (arefy string (+ start ,n))))
+            ,body))
+       (end-of-sentence ()
+         `(+ start 1)))
+    (cond
+      ((>= start (1- (length string)))
+       (end-of-sentence))
+      ((qudoex-p string start)
+       (next-char 1
+                  (cond
+                    ((space-p cur)
+                     (next-char 2
+                                (cond
+                                  ((space-p cur)
+                                   (next-char 3
+                                              (cond
+                                                ((space-p cur)
+                                                 (end-of-sentence ))
+                                                ((upcase-p cur)
+                                                 (end-of-sentence ))
+                                                ((else)
+                                                 (skip start)))))
+                                  ((upcase-p cur)
+                                   (end-of-sentence))
+                                  ((quote-p cur)
+                                   (next-char 3
+                                              (cond
+                                                ((upcase-p cur)
+                                                 (end-of-sentence))
+                                                ((else)
+                                                 (skip start)))))
+                                  ((else)
+                                   (skip start)))))
+                    ((upcase-p cur)
+                     (end-of-sentence ))
+                    ((else)
+                     (skip start)))))
+      ((else)
+       (skip start)))))
+
+(defun indexes-to-sentences (indexes text &optional sentences)
+  (if (null indexes)
+      (reverse sentences)
+      (indexes-to-sentences (cdr indexes)
+                            text (push (subseq text
+                                               (car indexes)
+                                               (cadr indexes))
+                                       sentences))))
+
+(defun split-to-sentences (text)
+  (let ((indexes (loop for y = 0 then (find-sentence-end text y)
+                       collect y
+                       until (>= y (length text)))))
+    (format t "indexes ~a~%" indexes)
+    (indexes-to-sentences indexes text)))
 
 
 (defun solve-me (d)
-  (format t "the data: ~s~%" d))
+  (format t "the data: ~s~%" d)
+  (let ((ds (split-to-sentences d)))
+    (format t "sentences ~S~%" ds)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun make-array-for-reading ()
-  (make-array '(500)
-              :element-type 'base-char
-              :fill-pointer 0
-              :adjustable T))
-
-(defun split-by-char (string char)
-  (loop for i = 0 then (1+ j)
-        as j = (position char string :start i)
-        collect (subseq string i j)
-        while j))
-
 (defun read-all-data (stream)
   (let ((contents (make-string (file-length stream))))
     (read-sequence contents stream)
@@ -28,7 +128,7 @@
   (let ((d (read-all-data stream)))
     (solve-me d)))
 
-  ;; (solution) ; uncomment this when running on hacker-rank
+;; (solution) ; uncomment this when running on hacker-rank
 
 
 (defun main ()
