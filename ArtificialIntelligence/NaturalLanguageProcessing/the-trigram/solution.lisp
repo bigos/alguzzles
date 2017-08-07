81cd99670288e775deaa4453742afb3b0635ccaf
Jacek Podkanski <ruby.object@googlemail.com>
2017-08-07 15:25:53 +0100

    read all data

--- ArtificialIntelligence/NaturalLanguageProcessing/the-trigram/solution.lisp
+++ ArtificialIntelligence/NaturalLanguageProcessing/the-trigram/solution.lisp
@@ -1,23 +1,17 @@
+(declaim (optimize (debug 3)))
+
 ;;; not solved yet
 
-;;; example of putting multiple lines on one sstring
-(let
-    ((str (make-array '(0) :element-type 'base-char
-                           :fill-pointer 0
-                           :adjustable T)))
-  (with-output-to-string (stream str)
-    (loop for s in '("ala ma kota. Ola"
-                     "ma psa. Ela "
-                     "ma papuge.")
-          do (format stream "~a " s )))
-  (format t "===== ~S" str))
 
-(with-input-from-string (s (format nil "Lorem ipsum.~%Dolor sic amet."))
-  (format t "read ~s" (loop for l = (read-line s nil 'eof nil)
-                            until (eql l 'eof)
-                            collect l)))
-
+(defun solve-me (d)
+  (format t "the data: ~s~%" d))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun make-array-for-reading ()
+  (make-array '(500)
+              :element-type 'base-char
+              :fill-pointer 0
+              :adjustable T))
+
 (defun split-by-char (string char)
   (loop for i = 0 then (1+ j)
         as j = (position char string :start i)
@@ -25,27 +19,24 @@
         while j))
 
 (defun read-all-data (stream)
-  (loop for line = (read-line stream nil 'eof)
-        until (equal line 'eof)
-        collect (split-by-char (string-downcase line) #\.)))
+  (let ((contents (make-string (file-length stream))))
+    (read-sequence contents stream)
+    contents))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun solution (&optional stream)
-  (let ((d (read-line stream nil 'eof)))
+  (let ((d (read-all-data stream)))
     (solve-me d)))
 
-;; (solution) ; uncomment this when running on hacker-rank
+  ;; (solution) ; uncomment this when running on hacker-rank
+
 
 (defun main ()
   (with-open-file (s (make-pathname
                       :directory
                       (pathname-directory
-                       (parse-namestr
-                        ing *load-pathname*))
+                       (parse-namestring *load-pathname*))
                       :name "input0" :type "txt"))
     (solution s)))
 
 (main)
-(progn #|
-
-|#)
