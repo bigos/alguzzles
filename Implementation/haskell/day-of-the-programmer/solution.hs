356facc3367f8b54d896273a1f36ded6228ba36b
Jacek Podkanski <ruby.object@googlemail.com>
2018-01-24 03:06:30 +0000

    more cleanup

--- Implementation/haskell/day-of-the-programmer/solution.hs
+++ Implementation/haskell/day-of-the-programmer/solution.hs
@@ -1,10 +1,6 @@
 -- day of the programmer
 
-zerop :: Int -> Bool
-zerop 0 = True
-zerop _ = False
-
-divisible n d = zerop (mod n d)
+divisible n d = (mod n d) == 0
 
 julianLeap y = divisible y 4
 
@@ -12,19 +8,16 @@ georgianLeap y = a || b
   where a = divisible y 400
         b = (divisible y 4) && not (divisible y 100)
 
-inputYear :: IO Int
-inputYear = getLine >>= return . read
-
 leapYear y
   | y <= 1917 = julianLeap y
   | y == 1918 = False
   | y >= 1919 = georgianLeap y
 
-ys :: Int -> String
-ys y = show y
-
 solve y
   | y == 1918 = "26.09.1918"
-  | otherwise = if leapYear y then "12.09." else "13.09." ++ ys y
+  | otherwise = if leapYear y then "12.09." else "13.09." ++ show y
+
+inputYear :: IO Int
+inputYear = getLine >>= return . read
 
 main = inputYear >>= putStrLn . solve
