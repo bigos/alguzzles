fde385a46e57d5be6b04dab41d334277847e0e6c
Jacek Podkanski <ruby.object@googlemail.com>
2018-01-27 22:49:44 +0000

    stuck

--- Implementation/haskell/picking-numbers/solution.hs
+++ Implementation/haskell/picking-numbers/solution.hs
@@ -1,5 +1,15 @@
 -- picking numbers
 
+import Data.List
+
+comb :: Int -> [a] -> [[a]]
+comb 0 _      = [[]]
+comb _ []     = []
+comb m (x:xs) = map (x:) (comb (m-1) xs) ++ comb m xs
+
+--solve :: Int -> [Int] -> Int
+solve t d = 1
+
 readLineInts :: IO [Int]
 readLineInts = getLine >>=
   (\l -> return (map (\x -> (read x) :: Int) (words l)))
@@ -13,4 +23,4 @@ readInputData = readLineInts >>=
   (\d1 -> readLineInts >>=
     (\d2 -> return ((head d1), d2)))
 
-main = readInputData >>= (\d -> print d)
+main = readInputData >>= (\d -> print (solve (fst d) (snd d)))
