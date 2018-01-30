ffd7e4d57853db3128e383bb49f4c81b09093fd7
Jacek Podkanski <ruby.object@googlemail.com>
2018-01-30 02:15:54 +0000

    fast, but wrong results

--- Implementation/haskell/climbing-the-leaderboard/solution.hs
+++ Implementation/haskell/climbing-the-leaderboard/solution.hs
@@ -1,15 +1,23 @@
 -- climbing the leaderboard
 
-rank :: Int -> [Int] -> Int -> Int
-rank _ [] a = a
-rank r (s:ss) a =
-  if (r >= s)
-  then a
-  else rank r ss (if ss/= [] && head ss == s
-                  then a
-                  else (succ a))
+import Debug.Trace
 
-solve _ ss _ as = mapM_ (\x -> print (rank x ss 1)) as
+leader :: [Int] -> [Int] -> Int -> Int -> [Int] -> [Int]
+leader ss rs prev pl a = --trace ("--- " ++ show (ss,rs,"--",prev,pl,a)) $
+  if rs == []
+  then a
+  else
+    if ss == []
+    then leader ss (tail rs) prev (1+pl) (pl:a)
+    else
+      if prev == head ss
+      then  leader (tail ss) rs (head ss) pl a
+      else
+        if head rs >= head ss
+        then  leader ss (tail rs) (head ss) (1+pl) (pl:a)
+        else  leader (tail ss) rs (head ss) (1+pl) a
+
+solve _ ss _ as = (leader ss (reverse as) (1 + (head (reverse as))) 1 [])
 
 readNumber :: IO Int
 readNumber = getLine >>= (\n -> return (read n))
@@ -19,8 +27,10 @@ readInts = getLine >>= (\ln -> return (map (\x -> read x) (words ln)))
 
 main :: IO ()
 main =
-  readNumber >>=
-  (\n -> readInts >>=
+  readNumber         >>=
+  (\n  -> readInts   >>=
   (\ss -> readNumber >>=
-  (\m -> readInts >>=
-  (\as -> solve n ss m as))))
+  (\m  -> readInts   >>=
+  (\as -> mapM_ print (solve n ss m as)))))
+
+zzz = solve 7 [100, 100, 50, 40, 40, 20, 10] 4 [5, 25, 50, 120]
