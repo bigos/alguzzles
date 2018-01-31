bb1fbc8a00694a0bef4a1c7f900dae2533e83a50
Jacek Podkanski <ruby.object@googlemail.com>
2018-01-31 15:15:05 +0000

    Squashed commit of the following:
    commit 008a1435d6ae854165004930df9ccd2dce29bf10
    Author: Jacek Podkanski <ruby.object@googlemail.com>
    Date:   Wed Jan 31 15:12:44 2018 +0000
    
        code cleanup and still one wrong answer
    
    commit 93f22cea7e1d1fe7d02562031993939d680d49b6
    Author: Jacek Podkanski <ruby.object@googlemail.com>
    Date:   Wed Jan 31 13:55:44 2018 +0000
    
        only one wrong answer
    
    commit f3b504e12b6bd1459c2f0204fd7a5e6b24efe2e9
    Author: Jacek Podkanski <ruby.object@googlemail.com>
    Date:   Wed Jan 31 13:51:54 2018 +0000
    
        aha
    
    commit 48ba7d04df6b754d6af0235f6b81852dbdf05218
    Author: Jacek Podkanski <ruby.object@googlemail.com>
    Date:   Wed Jan 31 02:16:51 2018 +0000
    
        still debugging
    
    commit 597600dd0066c2f2481537ff40de09c7c5b7cc12
    Author: Jacek Podkanski <ruby.object@googlemail.com>
    Date:   Wed Jan 31 01:48:51 2018 +0000
    
        debugging
    
    commit 1c84fbfa35a397b8b1ab95d0b0b2c1a1331c1e25
    Author: Jacek Podkanski <ruby.object@googlemail.com>
    Date:   Tue Jan 30 23:31:20 2018 +0000
    
        suspected file reading problem

--- Implementation/haskell/climbing-the-leaderboard/solution.hs
+++ Implementation/haskell/climbing-the-leaderboard/solution.hs
@@ -1,22 +1,24 @@
 -- climbing the leaderboard
 
 import Debug.Trace
+import Data.Array
+import Data.List
 
 remdup :: [Int] -> [Int] -> [Int]
 remdup [] aa = reverse aa
-remdup ee aa = remdup (tail ee) (if aa/= [] && (head ee)==(head aa) then aa else ((head ee):aa))
+remdup (e:es) aa = remdup es (if aa/= [] && e==(head aa) then aa else (e:aa))
 
 unq l = remdup l []
 
 leader :: [Int] -> [Int] -> Int -> [Int] -> [Int]
-leader [] (r:rs) pl aa = leader [r] rs (0+pl) (pl:aa)
-leader _ [] _ aa = aa
+leader _  [] _ aa = aa
+leader [] (r:rs) pl aa = leader [] rs (if r== (head rs) then 0+pl else  1+pl) (pl:aa)
 leader (s:ss) (r:rs) pl az
-  | s >  r =  leader ss (r:rs) (1+pl) (az)
-  | s == r =  leader ss rs     (1+pl) (pl:az)
-  | s <  r =  leader (s:ss) rs (0+pl) (pl:az)
+  | s >  r    =  leader ss (r:rs) (1+pl) (az)
+  | otherwise =  leader (s:ss) rs (0+pl) (pl:az)
+  --where dbg zn = trace ("***"++ zn ++ show ((s:ss),(r:rs),"--",pl,az))
 
-solve _ ss _ as = (leader (unq ss) (reverse as) 1 [])
+solve _ ss _ as = (leader (unq ss) (reverse as)  1 [])
 
 readNumber :: IO Int
 readNumber = getLine >>= (\n -> return (read n))
@@ -32,6 +34,13 @@ main =
   (\m  -> readInts   >>=
   (\as -> mapM_ print (solve n ss m as)))))
 
-zzz = solve 7 [100, 100, 50, 40, 40, 20, 10] 4 [4,5,5, 25, 50, 120, 120]
+zzz = solve 7 [100, 100, 50, 40, 40, 20, 10] 4 [5, 25, 50, 120]
+aaa = solve 7 [100, 100, 50, 40, 40, 20, 10, 4, 3] 4 [5, 25, 50, 120]
+qqq = solve 7 [100, 100, 50, 40, 40, 20, 10] 4 [1,3,3,5, 25, 50, 98]
+www = solve 7 [100, 100, 98, 98, 97, 97] 4 [97, 97, 98, 98, 98,  99, 99,  120]
+eee = solve 7 [100, 100, 98, 98, 97, 97,90,0] 4 [2,3,4,96,97, 97, 98, 98, 98,  99, 99,  120]
 
-qqq = remdup [100, 100, 50, 40, 40, 20, 10] []
+-- figured out how to create arrays in Haskell
+ary = array (0, 9) [(i-1,i) | i<- [1..10]]
+-- and might use it for this imperative solution
+-- https://hebr3.github.io/2017/09/hacker-rank-climbing-the-leaderboard.html
