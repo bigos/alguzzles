a7555acb38f55dcf2fcd0de64e72baa5419ce474
Jacek Podkanski <ruby.object@googlemail.com>
2018-02-25 20:44:44 +0000

    four timeouts

---
+++ Sorting/lisp/big-sorting/solution.lisp
@@ -0,0 +1,36 @@
+;;; abc
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+        as j = (position #\Space string :start i)
+        collect (subseq string i j)
+        while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((tc (parse-integer (read-line stream))))
+    (loop for l in (sort
+                    (loop for x from 0 below tc
+                          collect (parse-integer (read-line stream)))
+                    (lambda (x y)
+                      (< x y)))
+          do (format t "~A~%" l))))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
