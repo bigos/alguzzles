43844315da2ac254cb910bf5ef930bd9022d98ae
Jacek Podkanski <ruby.object@googlemail.com>
2018-04-26 02:42:58 +0100

    stuck

--- Contests/ProjectEuler/9/solution.hs
+++ Contests/ProjectEuler/9/solution.hs
@@ -1,21 +1,36 @@
 -- solution
 import Control.Monad
 
+pytr :: Int -> [(Bool, Int, Int, Int)]
+pytr n =
+  filter
+    (\(_, a, b, c) -> a + b + c <= n)
+    [ (prim a b c, a, b, c)
+    | a <- xs
+    , b <- drop a xs
+    , c <- drop b xs
+    , a ^ 2 + b ^ 2 == c ^ 2 ]
+  where
+    xs = [1 .. n]
+    prim a b _ = gcd a b == 1
+
+pytr2 n = map (\(_,a,b,c) -> [a, b, c]) (pytr n)
+
 sq n i = n!!i * n!!i
 
 check :: [Integer] -> Bool
 check n = (sq n 0 + (sq n 1) == (sq n 2))
 
-mul x = head (reverse (scanl (\b a -> b*a) 1 x))
-
-fff n = filter (\x -> check x) (comb 3 [1..n])
+fff n = filter check (comb 3 [1..n])
 
 comb :: Int -> [a] -> [[a]]
 comb 0 _      = [[]]
 comb _ []     = []
 comb m (x:xs) = map (x:) (comb (m-1) xs) ++ comb m xs
 
-pythagoras n = map (\x -> if (n==(sum x)) then (mul x) else (-1)) (fff n)
+pypypy n = filter check (fff n)
+
+pythagoras n = map (\x -> if (n == (sum x)) then (product x) else (-1)) (fff n)
 
 pythagor1 n = filter (\x -> x /= (-1)) (pythagoras n)
 
