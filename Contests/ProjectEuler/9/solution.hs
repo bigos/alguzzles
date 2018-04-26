300b7b2ccc358172f88fca4890d27a5f5143cb2a
Jacek Podkanski <ruby.object@googlemail.com>
2018-04-26 10:53:15 +0100

    progress

--- Contests/ProjectEuler/9/solution.hs
+++ Contests/ProjectEuler/9/solution.hs
@@ -1,41 +1,12 @@
 -- solution
 import Control.Monad
 
-pytr :: Int -> [(Bool, Int, Int, Int)]
-pytr n =
-  filter
-    (\(_, a, b, c) -> a + b + c <= n)
-    [ (prim a b c, a, b, c)
-    | a <- xs
-    , b <- drop a xs
-    , c <- drop b xs
-    , a ^ 2 + b ^ 2 == c ^ 2 ]
-  where
-    xs = [1 .. n]
-    prim a b _ = gcd a b == 1
+-- precompute filter (\[a,b,c] -> a^2 + b^2 == c^2) (comb 3 [1..3000])
 
-pytr2 n = map (\(_,a,b,c) -> [a, b, c]) (pytr n)
+hhh n = filter (\x-> (floor x) == (ceiling x)) (ggg n)
 
-sq n i = n!!i * n!!i
+ggg n = map (\x -> sqrt ( n^2 - x^2)) [ n-1, (n-2) .. (n / 2)]
 
-check :: [Integer] -> Bool
-check n = (sq n 0 + (sq n 1) == (sq n 2))
-
-fff n = filter check (comb 3 [1..n])
-
-comb :: Int -> [a] -> [[a]]
-comb 0 _      = [[]]
-comb _ []     = []
-comb m (x:xs) = map (x:) (comb (m-1) xs) ++ comb m xs
-
-pypypy n = filter check (fff n)
-
-pythagoras n = map (\x -> if (n == (sum x)) then (product x) else (-1)) (fff n)
-
-pythagor1 n = filter (\x -> x /= (-1)) (pythagoras n)
-
-pythagor :: Integer -> Integer
-pythagor n = if ((pythagor1 n) == []) then (-1) else (head (pythagor1 n))
 
 readInteger :: IO Integer
 readInteger = getLine >>= (\s -> return (read s :: Integer))
