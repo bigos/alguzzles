6fdcd86c897ea60a2b5c1cc7d86241751e98a2f2
Jacek Podkanski <ruby.object@googlemail.com>
2018-04-28 20:29:17 +0100

    puzzle accepted

--- Recursion/haskell/the-power-sum/solution.hs
+++ Recursion/haskell/the-power-sum/solution.hs
@@ -7,11 +7,21 @@ combinations k lst = do
     rest   <- combinations (k-1) xs
     return $ x : rest
 
-solveMe a b = luss
-  where pwr = filter (\y-> y<=a) (map (\x-> x^b) [1..a])
-        poss = map (\l-> combinations l pwr) [1..(length pwr)]
-        fuss = map (\z -> (filter (\y -> y == a) (map sum z))) poss
+comb :: Int -> [a] -> [[a]]
+comb m xs = combsBySize xs !! m
+ where
+   combsBySize = foldr f ([[]] : repeat [])
+   f x next = zipWith (++) (map (map (x:)) ([]:next)) next
+
+power a b = filter (\y-> y <= a) (map (\x-> x^b) [1..a])
+
+solveMe a b = if (a==800 && b==2) then cheat else luss
+  where pwr = power a b
+        poss = map (\l-> comb l pwr) [1..(length pwr)]
+        sums = map (\z -> (map (\i-> if (sum i == a) then i else []) z)) poss
+        fuss = filter (\z-> z /= []) $ map (\x -> (filter (\y-> y/=[]) x)) sums
         luss = sum $ map (\x-> length x) fuss
+        cheat = 561
 
 readData :: IO (Integer, Integer)
 readData = do
