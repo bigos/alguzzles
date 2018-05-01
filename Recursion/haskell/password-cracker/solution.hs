3622792a6a749c110857bad07f465a6f131c49f2
Jacek Podkanski <ruby.object@googlemail.com>
2018-05-02 00:08:19 +0100

    unfinished puzzle

--- Recursion/haskell/password-cracker/solution.hs
+++ Recursion/haskell/password-cracker/solution.hs
@@ -1,4 +1,29 @@
 -- password cracker
 
-main =  do
-  print "finish me"
+import Control.Monad (replicateM)
+
+comb :: Int -> [a] -> [[a]]
+comb m xs = combsBySize xs !! m
+ where
+   combsBySize = foldr f ([[]] : repeat [])
+   f x next = zipWith (++) (map (map (x:)) ([]:next)) next
+
+-- try it with
+-- solve 3 ["ab", "abcd", "cd"] "abcd"
+-- I need to find a recursive way of solving the problem
+solve n ns la = replicateM n ns
+
+testCases :: IO Integer
+testCases = getLine >>= (\x -> return (read x))
+
+readTestCase :: IO (Int, [String], String)
+readTestCase = do
+  n <- getLine
+  ns <- getLine
+  la <- getLine
+  return ((read n), words ns, la)
+
+main = do
+  t <- testCases
+  mapM_ (\_ -> (readTestCase >>= (\(n,ns,la) -> putStrLn (show (solve n ns la)))))
+    [1..t]
