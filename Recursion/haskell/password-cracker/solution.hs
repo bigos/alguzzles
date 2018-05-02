4f9f3d1bcde2e73974010533d829d300c7bd9597
Jacek Podkanski <ruby.object@googlemail.com>
2018-05-02 02:02:12 +0100

    almost there but i might need accumulator for the values

--- Recursion/haskell/password-cracker/solution.hs
+++ Recursion/haskell/password-cracker/solution.hs
@@ -1,17 +1,19 @@
 -- password cracker
 
-import Control.Monad (replicateM)
+import Data.List
+-- import Debug.Trace
 
-comb :: Int -> [a] -> [[a]]
-comb m xs = combsBySize xs !! m
- where
-   combsBySize = foldr f ([[]] : repeat [])
-   f x next = zipWith (++) (map (map (x:)) ([]:next)) next
+solve ns "" = ""
+solve ns la =
+  if findpass
+  then ""
+  else la
+  where findpass = (any (\x->  x == "") (map (\s -> if (isPrefixOf s la)
+                                               then (solve ns (drop (length s) la) )
+                                               else la )
+                                          ns))
 
--- try it with
--- solve 3 ["ab", "abcd", "cd"] "abcd"
--- I need to find a recursive way of solving the problem
-solve n ns la = replicateM n ns
+solve2 _ ns la = if (solve ns la == "") then la else "WRONG PASSWORD"
 
 testCases :: IO Integer
 testCases = getLine >>= (\x -> return (read x))
@@ -25,5 +27,4 @@ readTestCase = do
 
 main = do
   t <- testCases
-  mapM_ (\_ -> (readTestCase >>= (\(n,ns,la) -> putStrLn (show (solve n ns la)))))
-    [1..t]
+  mapM_ (\_ -> (readTestCase >>= (\(n,ns,la) -> putStrLn (solve2 n ns la )))) [1..t]
