8642a72971336bb275f8636ec2b9ec04fa1cb6a3
Jacek Podkanski <ruby.object@googlemail.com>
2018-05-02 17:01:42 +0100

    oho

--- Recursion/haskell/password-cracker/solution.hs
+++ Recursion/haskell/password-cracker/solution.hs
@@ -4,11 +4,16 @@ import Data.List
 
 solve :: [String] -> [String] -> String -> [String] -> [String]
 solve _ _  "" acc = reverse acc
-solve _ [] _  _   = ["WRONG","PASSWORD"]
+solve _ [] _  _   = []
 solve ngo (n:ns) la acc  =
   if (isPrefixOf n la)
   then solve ngo ngo (drop (length n) la) (n:acc)
   else solve ngo ns la acc
+
+flt a = (filter (\x-> x /= []) a)
+
+runner []     la a = if (null (flt a)) then ["WRONG","PASSWORD"] else  (head (flt a))
+runner (n:ns) la a = runner ns la ((solve (n:ns) (n:ns) la []) : a)
 
 testCases :: IO Integer
 testCases = getLine >>= (\x -> return (read x))
@@ -23,4 +28,4 @@ readTestCase = do
 main :: IO ()
 main = do
   t <- testCases
-  mapM_ (\_ -> (readTestCase >>= (\(_, ns, la) -> putStrLn  (unwords(solve ns ns la []))))) [1..t]
+  mapM_ (\_ -> (readTestCase >>= (\(_, ns, la) -> putStrLn (unwords (runner  ns la []))))) [1..t]
