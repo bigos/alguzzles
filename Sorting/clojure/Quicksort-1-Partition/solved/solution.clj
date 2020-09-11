2aadb10314ec21d12d77b54abe3cdd7e711ebd04
Jacek Podkanski <ruby.object@googlemail.com>
2020-09-12 00:01:57 +0100

    first clojure solution with deps.edn

--- Sorting/clojure/Quicksort-1-Partition/solved/solution.clj
+++ Sorting/clojure/Quicksort-1-Partition/solved/solution.clj
@@ -2,9 +2,21 @@
 
 ;;; ========= copy below ============
 
+(defn quickSort [arr]
+  (sort arr))
+
+
+(def n (Integer/parseInt (clojure.string/trim (read-line))))
+(def arr (vec (map #(Integer/parseInt %) (clojure.string/split (read-line) #" "))))
+
+(def result (quickSort arr))
+
+(def fptr (get (System/getenv) "OUTPUT_PATH"))
+(spit fptr (clojure.string/join " " result) :append true)
+(spit fptr "\n" :append true)
 
 ;;; ========= copy above ============
 
 (defn -main []
   (println
-   (str "Helloo")))
+   (str (quickSort [5 4 3 2 1]))))
