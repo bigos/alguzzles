fe101ab00127a2a13d953320b57a388a4a460650
Jacek Podkanski <ruby.object@googlemail.com>
2020-09-12 01:35:58 +0100

    figured out how to read the data locally

--- Sorting/clojure/Find-the-Median/solved/solution.clj
+++ Sorting/clojure/Find-the-Median/solved/solution.clj
@@ -8,18 +8,25 @@
         i      (quot l 2)]
     (nth sorted i)))
 
-(def fptr (get (System/getenv) "OUTPUT_PATH"))
+;; (def fptr (get (System/getenv) "OUTPUT_PATH"))
 
-(def n (Integer/parseInt (clojure.string/trim (read-line))))
+;; (def n (Integer/parseInt (clojure.string/trim (read-line))))
 
-(def arr (vec (map #(Integer/parseInt %) (clojure.string/split (read-line) #" "))))
+;; (def arr (vec (map #(Integer/parseInt %) (clojure.string/split (read-line) #" "))))
 
-(def result (findMedian arr))
+;; (def result (findMedian arr))
 
-(spit fptr (str result "\n") :append true)
+;; (spit fptr (str result "\n") :append true)
 
 ;;; ========= copy above ============
 
+(defn read-data-lines []
+  (clojure.string/split (slurp "input0.txt") #"\n"))
+
 (defn -main []
-  (println
-   (str (findMedian [0 1 2 4 6 5 3]))))
+  (let [lines (read-data-lines)
+        n (Integer/parseInt (nth lines 0))
+        arr (vec (map #(Integer/parseInt %)
+                        (clojure.string/split (nth lines 1) #" ")))]
+    (println
+     (str (findMedian arr)))))
