5139f5c834720326064f005fb8b97a71ae87b8d0
Jacek Podkanski <ruby.object@googlemail.com>
2020-09-20 14:50:50 +0100

    prepared lisp puzzle

---
+++ Strings/lisp/separate-the-numbers/solution.lisp
@@ -0,0 +1,33 @@
+(defun solve-me (s)
+   (format t "~A ~%" s))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((sl (parse-integer (read-line stream))))
+    (loop for x from 1 to sl do
+      (solve-me
+       (read-line stream)))))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
