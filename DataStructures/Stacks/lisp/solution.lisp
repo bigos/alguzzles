3c6279c86893ca72625fcfbb21b40e4860d63690
Jacek Podkanski <ruby.object@googlemail.com>
2020-10-09 15:38:39 +0100

    started work on the puzzle

---
+++ DataStructures/Stacks/lisp/solution.lisp
@@ -0,0 +1,50 @@
+(defparameter opening-brackets "({[")
+(defparameter closing-brackets ")}]")
+
+(defun balanced? (str &optional stack)
+  (format t "solving ~A - ~A~%" str stack)
+  (if (zerop (length str))
+      T
+      (balanced? (subseq str 1)
+                 (cons (car str)
+                       stack ))))
+
+(defun solve-me (str)
+  (format t "~A~%" (if (balanced? str)
+                       "YES"
+                       "NO")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+
+(defun string-to-characters (string)
+  (loop for c across string
+     collect c))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((n (parse-integer (read-line stream))))
+    (dotimes (x n)
+      (solve-me (string-to-characters (read-line stream))))))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
