3753629ae296d43cac40ce3060f77c13265b8f70
Jacek Podkanski <ruby.object@googlemail.com>
2020-10-10 02:31:29 +0100

    prepared haskell version

--- DataStructures/Stacks/balanced-brackets/haskell/solution.hs
+++ DataStructures/Stacks/balanced-brackets/haskell/solution.hs
@@ -5,18 +5,36 @@
 module Main where
 
 import Control.Monad
-import Data.Array
-import Data.Bits
-import Data.List
-import Data.List.Split
-import Data.Set
-import Debug.Trace
 import System.Environment
 import System.IO
-import System.IO.Unsafe
+
+cb :: String
+cb = ")}]"
+
+mb :: Char -> String -> Bool
+mb c stack =
+  (c == ')' && '(' == head stack) ||
+  (c == '}' && '{' == head stack) ||
+  (c == ']' && '[' == head stack)
+
+isEmptyStack :: String -> Bool
+isEmptyStack stack = words stack == []
+
+balanced :: String -> String -> Bool
+balanced str stack =
+  if str == ""
+  then isEmptyStack stack
+  else
+    balanced (tail str) ns
+    where
+      fc = head str
+      c = fc == ')' ||  fc == '}' || fc == ']'
+      f = c && mb fc stack
+      ns = if f then tail stack else fc : stack
 
 -- Complete the isBalanced function below.
-isBalanced s = do
+isBalanced s =
+  if balanced s [] then "YES" else "NO"
 
 main :: IO()
 main = do
