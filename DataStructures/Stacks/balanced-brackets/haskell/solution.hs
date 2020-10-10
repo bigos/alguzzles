d352b60238e597c88c26b4cdfb9c8c99cf56c13e
Jacek Podkanski <ruby.object@googlemail.com>
2020-10-10 01:37:50 +0100

    preparing Haskell puzzle

---
+++ DataStructures/Stacks/balanced-brackets/haskell/solution.hs
@@ -0,0 +1,36 @@
+-- solution
+
+{-# LANGUAGE FlexibleInstances, UndecidableInstances, DuplicateRecordFields #-}
+
+module Main where
+
+import Control.Monad
+import Data.Array
+import Data.Bits
+import Data.List
+import Data.List.Split
+import Data.Set
+import Debug.Trace
+import System.Environment
+import System.IO
+import System.IO.Unsafe
+
+-- Complete the isBalanced function below.
+isBalanced s = do
+
+main :: IO()
+main = do
+    stdout <- getEnv "OUTPUT_PATH"
+    fptr <- openFile stdout WriteMode
+
+    t <- readLn :: IO Int
+
+    forM_ [1..t] $ \t_itr -> do
+        s <- getLine
+
+        let result = isBalanced s
+
+        hPutStrLn fptr result
+
+    hFlush fptr
+    hClose fptr
