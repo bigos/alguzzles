605a4ca7690fdd0df6c1e1f8aadba3b29edbb930
Jacek Podkanski <ruby.object@googlemail.com>
2020-10-12 16:37:14 +0100

    prepared puzzle

---
+++ Implementation/lisp/almost-sorted/solution.lisp
@@ -0,0 +1,34 @@
+;;; solution for: https://www.hackerrank.com/challenges/almost-sorted/problem
+
+(defun solve-me (len d)
+  (format t "~A   ~A~%" len d))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun split-by-one-space (string)
+  (loop for i = 0 then (1+ j)
+     as j = (position #\Space string :start i)
+     collect (subseq string i j)
+     while j))
+
+(defun split-and-parse (string)
+  (map 'list
+       (lambda (x) (parse-integer x))
+       (split-by-one-space string)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun solution (&optional stream)
+  (let ((len (parse-integer (read-line stream)))
+        (d (split-and-parse (read-line stream))))
+    (solve-me len d)))
+
+;; (solution) ; uncomment this when running on hacker-rank
+
+(defun main ()
+  (with-open-file (s (make-pathname
+                      :directory
+                      (pathname-directory
+                       (parse-namestring *load-pathname*))
+                      :name "input0" :type "txt"))
+    (solution s)))
+
+(main)
