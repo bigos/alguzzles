7aebc9d48ced0ffb6866fc2ec616a41f42c4bda1
Jacek Podkanski <ruby.object@googlemail.com>
2020-10-26 19:11:23 +0000

    solved the puzzle thanks to trace

--- Implementation/haskell/h-----een-sale/solution.hs
+++ Implementation/haskell/h-----een-sale/solution.hs
@@ -1,23 +1,23 @@
 module Main where
 
+import Debug.Trace
 import System.IO
 
 solve2 :: Int -> Int -> Int -> Int  -> Int -> Int
 solve2    p      d      m      s       acc =
-  if endCond then retVal else again
+  trace ("===== pdmsacc " ++ show (p, d, m, s, acc)) $
+  if endCond then acc else solve2 newp d m (s-p) (1 + acc)
   where
     endCond   = s < p
-    retVal = if acc > 0 then acc - 1 else 0
-    again = solve2 newp d m (s-p) (1 + acc)
-    priceDesc = p > m
-    newp = if priceDesc then p - d else p
-
+    newp = max m (p - d)
 
 solve :: [Int] -> Int
 solve (p:d:m:s:[]) = solve2 p d m s 0
 
 
-try = solve2 20 3 6 80 0
+try1 = solve2 20 3 6 80 0
+try2 = solve2 20 3 6 85 0
+
 
 main :: IO ()
 main = do
