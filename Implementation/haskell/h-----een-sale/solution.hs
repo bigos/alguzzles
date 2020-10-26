d552c654d8ea02f8ee6e647b19bcb5b7048306ae
Jacek Podkanski <ruby.object@googlemail.com>
2020-10-26 14:04:20 +0000

    some cases fail

--- Implementation/haskell/h-----een-sale/solution.hs
+++ Implementation/haskell/h-----een-sale/solution.hs
@@ -1,16 +1,27 @@
--- solution
+module Main where
 
 import System.IO
 
 solve2 :: Int -> Int -> Int -> Int  -> Int -> Int
-solve2    p      d      m      s       acc = 1 -- finish me
+solve2    p      d      m      s       acc =
+  if endCond then retVal else again
+  where
+    endCond   = s < p
+    retVal = if acc > 0 then acc - 1 else 0
+    again = if priceDesc then descAgain else regAgain
+    priceDesc = p > m
+    descAgain = solve2 (p-d) d m (s-p) (1 + acc)
+    regAgain  = solve2 p d m (s-p) (1 + acc)
 
 solve :: [Int] -> Int
 solve (p:d:m:s:[]) = solve2 p d m s 0
 
+
+try = solve2 20 3 6 80 0
+
 main :: IO ()
 main = do
-  let fromStdin = False --True or False
+  let fromStdin = True --True or False
   handle <- if fromStdin then pure stdin else openFile "./input0.txt" ReadMode
   ln <- hGetLine handle
   let nums = (map (\x -> read x::Int) (words ln))
