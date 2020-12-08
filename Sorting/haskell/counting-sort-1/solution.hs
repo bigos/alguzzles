16fc094bffc6157adeffcf7dc7e85cea636b0935
Jacek Podkanski <ruby.object@googlemail.com>
2020-12-08 02:08:52 +0000

    progress

--- Sorting/haskell/counting-sort-1/solution.hs
+++ Sorting/haskell/counting-sort-1/solution.hs
@@ -1,42 +1,40 @@
-{-# LANGUAGE FlexibleInstances, UndecidableInstances, DuplicateRecordFields #-}
+-- https://www.hackerrank.com/challenges/countingsort1/problem
 
 module Main where
 
-import Control.Monad
 import Data.Array
-import Data.Bits
-import Data.List
-import Data.List.Split
-import Data.Set
-import Debug.Trace
-import System.Environment
 import System.IO
-import System.IO.Unsafe
 
--- Complete the countingSort function below.
-countingSort arr = do
+-- λ> accumArray (+) 0 (0,5) [(1,1),(1,1),(1,1),(1,1),(3,1),(3,1)]
+-- array (0,5) [(0,0),(1,4),(2,0),(3,2),(4,0),(5,0)]
 
-readMultipleLinesAsStringArray :: Int -> IO [String]
-readMultipleLinesAsStringArray 0 = return []
-readMultipleLinesAsStringArray n = do
-    line <- getLine
-    rest <- readMultipleLinesAsStringArray(n - 1)
-    return (line : rest)
+recsol :: Integer -> [Integer] -> Array Integer Integer -> Array Integer Integer
+recsol n d ar =
+  if null d
+  then ar
+  else recsol n (tail d) (ar // [(i, nv)])
+  where
+    i = head d
+    nv = (ar!i) + 1
 
-main :: IO()
+zeroed :: Integer ->  Array Integer Integer
+zeroed n = array (0,n) [(i, 0) | i <- [0..n]]
+
+solution :: Integer -> [Integer] -> String
+solution n d = unwords $ map show (elems res)
+  where
+    res = recsol n d (zeroed n)
+
+-- results are ok but problem with IO in main
+
+main :: IO ()
 main = do
-    stdout <- getEnv "OUTPUT_PATH"
-    fptr <- openFile stdout WriteMode
-
-    n <- readLn :: IO Int
-
-    arrTemp <- getLine
-
-    let arr = Data.List.map (read :: String -> Int) . words $ arrTemp
-
-    let result = countingSort arr
-
-    hPutStrLn fptr $ intercalate " " $ Data.List.map (\x -> show x) $ result
-
-    hFlush fptr
-    hClose fptr
+  let fromStdin = not True --must be True for submission or False for development
+  handle <- if fromStdin then pure stdin else openFile "./input0.txt" ReadMode
+  contents <- hGetContents handle
+  let linez = lines contents
+  let num = read (head linez) :: Integer
+  let dat = map read (words (linez!!1)) :: [Integer]
+  let !res = solution num dat
+  hClose handle
+  print res
