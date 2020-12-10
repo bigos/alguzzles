46293d6ee6e01ae4834dcb57d2855d72668fbc3c
Jacek Podkanski <ruby.object@googlemail.com>
2020-12-10 00:01:53 +0000

    this solution was accepted

--- Sorting/haskell/counting-sort-1/solution.hs
+++ Sorting/haskell/counting-sort-1/solution.hs
@@ -1,23 +1,3 @@
-module Main where
-
-import Data.Array
-import Data.List
-import System.Environment
-import System.IO
-
-main :: IO()
-main = do
-    stdout <- getEnv "OUTPUT_PATH"
-    fptr <- openFile stdout WriteMode
-
-    n <- readLn :: IO Integer
-    arrTemp <- getLine
-    let result1 = elems $ accumArray (+) 0 (0,n-1) (map (\x->(x,1)) (map read (words arrTemp) :: [Integer]))
-    let result = unwords $ map show result1
-    hPutStrLn fptr result
-    hFlush fptr
-    hClose fptr
-
 -- import Data.Array
 -- import qualified Data.ByteString.Char8 as BS
 -- import Data.Maybe
@@ -35,18 +15,18 @@ main = do
 
 
 
--- import Control.Monad
--- import Control.Applicative
--- import Data.List
--- import Data.Ord
--- import Data.Char
--- import Data.Maybe
--- import Data.Array
--- import qualified Data.ByteString as B
--- import qualified Data.ByteString.Char8 as C
+import Control.Monad
+import Control.Applicative
+import Data.List
+import Data.Ord
+import Data.Char
+import Data.Maybe
+import Data.Array
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
 
--- main = do
---             getLine
---             s<-map read.words <$> getContents::IO [Int]
---             let arra= accumArray (+) 0 (0,99) (zip s (repeat 1))
---             putStrLn $ intercalate " " $ map show $ elems arra
+main = do
+            getLine
+            s<-map read.words <$> getContents::IO [Int]
+            let arra= accumArray (+) 0 (0,99) (zip s (repeat 1))
+            putStrLn $ intercalate " " $ map show $ elems arra
