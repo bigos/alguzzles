5885e6ea5478f0c604ffe3ad2999c138b82f6fc9
Jacek Podkanski <ruby.object@googlemail.com>
2020-12-12 15:47:19 +0000

    solved clojure puzzle

--- Search/clojure/missing-numbers/solution.clj
+++ Search/clojure/missing-numbers/solution.clj
@@ -1,8 +1,14 @@
 ;; Complete the missingNumbers function below.
 
-(defn missingNumbers [arr brr] (list "hello"))
-
-(def fptr (get (System/getenv) "OUTPUT_PATH"))
+(defn missingNumbers [arr brr]
+  (let [ag (frequencies arr)
+        bg (frequencies brr)
+        an (sort (distinct (concat (keys ag) (keys bg))))]
+    (for [n     an
+          :let  [ac (get ag n)
+                bc (get bg n)]
+          :when (not= ac bc)]
+      n)))
 
 (def n (Integer/parseInt (clojure.string/trim (read-line))))
 
@@ -12,7 +18,20 @@
 
 (def brr (vec (map #(Integer/parseInt %) (clojure.string/split (read-line) #" "))))
 
-(def result (missingNumbers arr brr))
+;;; comment is out when submitting
+;; (def a1 [203 204 205 206 207 208 203 204 205 206])
+;; (def b1 [203 204 204 205 206 207 205 208 203 206 205 206 204])
+;; (defn huh []
+;;   (missingNumbers a1 b1))
 
-(spit fptr (clojure.string/join " " result) :append true)
-(spit fptr "\n" :append true)
+(defn solve []
+  (let [fptr (get (System/getenv) "OUTPUT_PATH")
+        ;; uncomment when deploying
+        result (missingNumbers arr brr)
+        ;; result (missingNumbers a1 b1)
+        ]
+
+    (spit fptr (clojure.string/join " " result) :append true)
+    (spit fptr "\n" :append true)))
+
+(solve)
