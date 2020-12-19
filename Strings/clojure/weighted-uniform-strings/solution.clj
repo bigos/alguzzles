6eb6a60c26ad28fafa11ab026df068eec4662b7d
Jacek Podkanski <ruby.object@googlemail.com>
2020-12-19 00:36:27 +0000

    this time all puzzles have passed

--- Strings/clojure/weighted-uniform-strings/solution.clj
+++ Strings/clojure/weighted-uniform-strings/solution.clj
@@ -1,10 +1,45 @@
 ;;; solution
 
+(def inputsabc "abccddde")
+(def inputs123 [1 3 12 5 9 10])
+;;; end of development code
+
+(defn mults [n nn acc]
+  ;; (print n nn acc "\n")
+  (if (empty? nn)
+    acc
+    (recur n (rest nn) (cons (+ n (first acc)) acc))))
+
+(defn group-same [seq last sames acc]
+  ;; (print seq last sames " ---- " acc "\n")
+  (if (= (first seq) :end)
+    (rest (reverse  acc))
+    (let [nv (if (empty? seq)
+               (list :end)
+               (rest seq))]
+      (if (= last (first seq))
+        (recur nv
+               (first seq)
+               (cons (first seq) sames)
+               acc)
+        (recur nv
+               (first seq)
+               []
+               (if (empty? sames)
+                 (concat (list last) acc)
+                 (concat (mults last sames [last]) acc)))))))
+
+(def weights (zipmap (map #(char (+ % 96))
+                           (range 1 27))
+                     (range 1 27)))
+
+;; REPL (weightedUniformStrings inputsabc inputs123)
 ;; Complete the weightedUniformStrings function below.
 (defn weightedUniformStrings [s queries]
-
-
-  )
+  (let [letter-weights (set (group-same
+                             (map (fn [c] (get weights c) ) s)
+                             nil [] []))]
+    (map (fn [q] (if (contains? letter-weights q) "Yes" "No")) queries)))
 
 
 (def s (read-line))
@@ -14,8 +49,7 @@
 (def queries [])
 
 (doseq [_ (range queries-count)]
-  (def queries (conj queries (Integer/parseInt (read-line))))
-  )
+  (def queries (conj queries (Integer/parseInt (read-line)))))
 
 (def result (weightedUniformStrings s queries))
 
