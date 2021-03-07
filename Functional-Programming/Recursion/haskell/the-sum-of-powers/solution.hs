0afc36a3a7b71b403e874b677da964d397a45e25
Jacek Podkanski <ruby.object@googlemail.com>
2021-03-07 01:38:51 +0000

    one failing testcase

--- Functional-Programming/Recursion/haskell/the-sum-of-powers/solution.hs
+++ Functional-Programming/Recursion/haskell/the-sum-of-powers/solution.hs
@@ -1,12 +1,26 @@
--- solution
+module Main where
 
-solve :: Integer -> Integer -> Integer
-solve x n = 1
+import Data.List
+
+pows :: Integer -> Integer -> [Integer]
+pows n p = takeWhile (\x -> (x^p) <= n) [1..]
+
+powcomb :: Integer -> Integer -> [[Integer]]
+powcomb n p = subsequences (pows n p)
+
+powr :: Integer -> Integer -> [[Integer]]
+powr n p = map (map (^p)) (powcomb n p)
+
+findRootCeil :: Integer -> Integer -> Int
+findRootCeil n p = length (filter (\a -> sum a == n) (powr n p))
+
+solve :: Integer -> Integer -> Int
+solve = findRootCeil
 
 main = do
-  --contents <- getContents
-  let contents = "10\n2"
+  contents <- getContents
+  --let contents = "10\n2"
 
   let x = read ((lines contents)!!0)::Integer
   let n = read ((lines contents)!!1)::Integer
-  print  (show (solve x n))
+  putStrLn  (show (solve x n))
