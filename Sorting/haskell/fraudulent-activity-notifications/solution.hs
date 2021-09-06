9034910e888ac18d66a7eb1d4e91280b8f9875fd
Jacek Podkanski <ruby.object@googlemail.com>
2021-09-07 00:54:34 +0100

    hopelessly stuck with type problem

--- Sorting/haskell/fraudulent-activity-notifications/solution.hs
+++ Sorting/haskell/fraudulent-activity-notifications/solution.hs
@@ -4,33 +4,28 @@ module Main where
 
 import Data.List
 
-getMiddle [] = 0
-getMiddle xs = (a' + b') `div` 2
-    where a' = head $ drop a xs
-          b' = head $ drop b xs
-          a = (n `div` 2)
-          b = n' - 1
-          n' = n `div` 2
-          n = length xs
+medianFromSorted :: Fractional a => [a] -> Maybe a
+medianFromSorted [] = Nothing
+medianFromSorted [a] = Just a
+medianFromSorted [a,b] = Just ((a + b) / 2)
+medianFromSorted (a:xs) = medianFromSorted (init xs) -- init is not efficient
 
-median :: [Integer] -> Integer
-median [] = 0
-median xs = result
-    where result = if (n `mod` 2 == 0)
-                    then getMiddle sorted
-                    else head $ drop a sorted
-          a = (n - 1) `div` 2
-          n = length xs
-          sorted = sort xs
+median :: Ord a => Fractional a => [a] -> Maybe a
+median = medianFromSorted . sort
 
-nc hr h2 count = if hr >= (median h2) then count + 1 else count
+nc :: Integer -> [Integer] -> Integer -> Integer
+nc hr h2 count =
+  case mv of
+    Nothing -> count
+    Just x  -> if hr >= x then (count + 1) else count
+  where mv = median (map fromIntegral h2)
 
 recurse :: Integer -> Integer -> [Integer] -> [Integer] -> Integer -> Integer
-recurse n d [] remainingE count = recurse n d (take 1 remainingE) (drop 1 remainingE) count
-recurse n d seenE [] count = count
+recurse n d [] remainingE count = recurse    n d [] (drop 1 remainingE) count
+recurse n d seenE [] count = nc 0 [] count
 recurse n d seenE remainingE count = recurse n d h2 (drop 1 remainingE) (nc hr h2 count)
   where hr = head remainingE
-        lastSeen = take (fromIntegral d) seenE
+        lastSeen = take (fromIntegral (d)) seenE
         h2 = (hr : lastSeen)
 
 
@@ -52,5 +47,5 @@ solve a = show (solve2 n d e)
 dat = "5 4\n1 2 3 4 4"
 
 main = interact solve
-       -- unfinished puzzle
+-- unfinished puzzle
 -- https://www.hackerrank.com/challenges/fraudulent-activity-notifications/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
